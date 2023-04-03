7fcb11ca0bf57ae850de36faa73b9722c31b6521
niehusst <liamniehusstaab@gmail.com>
2023-04-02 21:43:16 -0600

    unbork db state

---
+++ apps/core/eave_alembic/versions/2023_04_02_1439-c88077c78b60_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c88077c78b60
+Revises: 7a7f21394cb7, cb5b68318224
+Create Date: 2023-04-02 14:39:52.856373
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "c88077c78b60"
+down_revision = ("7a7f21394cb7", "cb5b68318224")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
