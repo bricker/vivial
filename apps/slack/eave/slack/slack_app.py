c027c5654311ecb9f2e68f5452506ff48290cfdf
niehusst <liamniehusstaab@gmail.com>
2023-04-10 12:24:29 -0600

    feat: remove hard-coded token from slack app

--- apps/slack/eave/slack/slack_app.py
+++ apps/slack/eave/slack/slack_app.py
@@ -9,17 +9,15 @@ from slack_bolt.authorization import AuthorizeResult
 
 from .config import app_config
 
+
 async def authorize(enterprise_id: str, team_id: str, logger: Any) -> AuthorizeResult:
     input = eave.stdlib.core_api.operations.GetSlackSource.RequestBody(
         slack_source=eave.stdlib.core_api.operations.SlackSourceInput(slack_team_id=team_id),
     )
     data = await eave_client.get_slack_source(input=input)
+    # TODO: return optional instead??
     assert data is not None
     assert data.slack_source is not None
-    # TODO: do we care about enterprise stuff yet?? what even is it for
-    # enterprise_id doesn't exist for some teams
-    # is_valid_enterprise = ("enterprise_id" not in data) or (enterprise_id == data["enterprise_id"])
-    # assert ((is_valid_enterprise == True) and (team["team_id"] == team_id)):
     return AuthorizeResult(
         enterprise_id=enterprise_id,
         team_id=team_id,
@@ -27,13 +25,11 @@ async def authorize(enterprise_id: str, team_id: str, logger: Any) -> AuthorizeR
         bot_id=data.slack_source.bot_id,
         bot_user_id=data.slack_source.bot_user_id,
     )
-    
 
-token = app_config.eave_slack_bot_token
+
 signing_secret = app_config.eave_slack_app_signing_secret
 
 app = AsyncApp(
-    token=token,
     signing_secret=signing_secret,
     url_verification_enabled=True,
     ssl_check_enabled=True,
