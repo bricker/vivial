3f6358619d23cbc8c9a78a4e111ec06892b48545
niehusst <liamniehusstaab@gmail.com>
2023-04-17 20:55:26 -0600

    fix: validate signature for slack_source

--- apps/core/eave/core/public/requests/slack_sources.py
+++ apps/core/eave/core/public/requests/slack_sources.py
@@ -3,11 +3,17 @@ import eave.core.internal.orm as eave_orm
 import eave.stdlib.core_api.models as models
 import eave.stdlib.core_api.operations as eave_ops
 import fastapi
+from eave.stdlib import logger
+
+from . import util as eave_request_util
 
 
 async def query(
     input: eave_ops.GetSlackSource.RequestBody, request: fastapi.Request, response: fastapi.Response
 ) -> eave_ops.GetSlackSource.ResponseBody:
+    logger.debug("subscriptions.delete_subscription")
+    await eave_request_util.validate_signature_or_fail(request=request)
+
     async with await eave_db.get_session() as session:
         slack_source = await eave_orm.SlackSource.one_or_none_by_slack_team_id(
             session=session,
