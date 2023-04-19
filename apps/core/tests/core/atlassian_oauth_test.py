3df56c4059a76d6be64cab7f479632d00702ee35
Bryan Ricker <978899+bricker@users.noreply.github.com>
2023-04-18 22:31:22 -0700

    skip tests on deployment

--- apps/core/tests/core/atlassian_oauth_test.py
+++ apps/core/tests/core/atlassian_oauth_test.py
@@ -29,65 +29,64 @@ class TestAtlassianOAuth(BaseTestCase):
         self.assertTrue(response.headers["location"], "https://auth.atlassian.com/authorize")
         self.assertIsNotNone(response.cookies.get("eave-oauth-state-atlassian"))
 
-    async def test_atlassian_callback_endpoint(self) -> None:
-        self.skipTest("Atlassian oauth team ID not yet implemented")
-        # mockito.when2(eave.core.internal.oauth.atlassian.AtlassianOAuthSession.get_available_resources).thenReturn(
-        #     [
-        #         eave.core.internal.oauth.atlassian.AtlassianAvailableResource(
-        #             id=self.anystring("atlassian_cloud_id"),
-        #             url=self.anystring("confluence_document_response._links.base"),
-        #             avatarUrl=self.anystring("atlassianresourceavatar"),
-        #             name=self.anystring("atlassianresourcename"),
-        #             scopes=[],
-        #         )
-        #     ]
-        # )
+    # async def test_atlassian_callback_endpoint(self) -> None:
+    #     mockito.when2(eave.core.internal.oauth.atlassian.AtlassianOAuthSession.get_available_resources).thenReturn(
+    #         [
+    #             eave.core.internal.oauth.atlassian.AtlassianAvailableResource(
+    #                 id=self.anystring("atlassian_cloud_id"),
+    #                 url=self.anystring("confluence_document_response._links.base"),
+    #                 avatarUrl=self.anystring("atlassianresourceavatar"),
+    #                 name=self.anystring("atlassianresourcename"),
+    #                 scopes=[],
+    #             )
+    #         ]
+    #     )
 
-        # fake_token = {
-        #     "access_token": self.anystring("oauth_access_token"),
-        #     "expires_in": self.anyint("oauth_expires_in"),
-        #     "scope": self.anystring("oauth_scope"),
-        # }
+    #     fake_token = {
+    #         "access_token": self.anystring("oauth_access_token"),
+    #         "expires_in": self.anyint("oauth_expires_in"),
+    #         "scope": self.anystring("oauth_scope"),
+    #     }
 
-        # mockito.when2(
-        #     eave.core.internal.oauth.atlassian.AtlassianOAuthSession.fetch_token, code=self.anystring("oauthcode")
-        # ).thenReturn(fake_token)
+    #     mockito.when2(
+    #         eave.core.internal.oauth.atlassian.AtlassianOAuthSession.fetch_token, code=self.anystring("oauthcode")
+    #     ).thenReturn(fake_token)
 
-        # # Get the state cookie
-        # authorize_response = await self.make_request(
-        #     "/oauth/atlassian/authorize",
-        #     method="GET",
-        #     follow_redirects=False,
-        # )
+    #     # Get the state cookie
+    #     authorize_response = await self.make_request(
+    #         "/oauth/atlassian/authorize",
+    #         method="GET",
+    #         follow_redirects=False,
+    #     )
 
-        # self.assertEqual(authorize_response.status_code, HTTPStatus.TEMPORARY_REDIRECT)
-        # state = authorize_response.cookies.get("eave-oauth-state-atlassian")
-        # self.assertIsNotNone(state)
+    #     self.assertEqual(authorize_response.status_code, HTTPStatus.TEMPORARY_REDIRECT)
+    #     state = authorize_response.cookies.get("eave-oauth-state-atlassian")
+    #     self.assertIsNotNone(state)
 
-        # async def get_installation() -> eave_orm.AtlassianInstallationOrm | None:
-        #     installation = await eave_orm.AtlassianInstallationOrm.one_or_none_by_atlassian_cloud_id(
-        #         session=self.dbsession,
-        #         atlassian_cloud_id=self.anystring("atlassian_cloud_id"),
-        #     )
-        #     return installation
+    #     async def get_installation() -> eave_orm.AtlassianInstallationOrm | None:
+    #         installation = await eave_orm.AtlassianInstallationOrm.one_or_none_by_atlassian_cloud_id(
+    #             session=self.dbsession,
+    #             atlassian_cloud_id=self.anystring("atlassian_cloud_id"),
+    #         )
+    #         return installation
 
-        # installation = await get_installation()
-        # self.assertIsNone(installation)
+    #     installation = await get_installation()
+    #     self.assertIsNone(installation)
 
-        # callback_response = await self.make_request(
-        #     "/oauth/atlassian/callback",
-        #     method="GET",
-        #     payload={
-        #         "state": state,
-        #         "code": self.anystring("oauthcode"),
-        #     },
-        # )
+    #     callback_response = await self.make_request(
+    #         "/oauth/atlassian/callback",
+    #         method="GET",
+    #         payload={
+    #             "state": state,
+    #             "code": self.anystring("oauthcode"),
+    #         },
+    #     )
 
-        # self.assertEqual(callback_response.status_code, HTTPStatus.OK)
+    #     self.assertEqual(callback_response.status_code, HTTPStatus.OK)
 
-        # installation = await get_installation()
-        # assert installation is not None
-        # self.assertEqual(installation.atlassian_cloud_id, self.anystring("atlassian_cloud_id"))
-        # self.assertEqual(installation.oauth_token_encoded, json.dumps(fake_token))
-        # # Test cookie was deleted
-        # self.assertIsNone(callback_response.cookies.get("eave-oauth-state-atlassian"))
+    #     installation = await get_installation()
+    #     assert installation is not None
+    #     self.assertEqual(installation.atlassian_cloud_id, self.anystring("atlassian_cloud_id"))
+    #     self.assertEqual(installation.oauth_token_encoded, json.dumps(fake_token))
+    #     # Test cookie was deleted
+    #     self.assertIsNone(callback_response.cookies.get("eave-oauth-state-atlassian"))
