acd448b26cbd001accf16ad5d5d1f253603b55b6
Bryan Ricker <978899+bricker@users.noreply.github.com>
2023-05-21 18:10:52 -0700

    deployment updates

--- apps/github/pm2.config.cjs
+++ apps/github/pm2.config.cjs
@@ -1,9 +1,12 @@
+const path = require('node:path');
+
 module.exports = {
   apps: [{
     name: 'github',
     script: './src/app.ts',
     out_file: '/dev/stdout',
     error_file: '/dev/stderr',
+    interpreter: path.join(__dirname, 'node_modules/.bin/ts-node'),
     interpreter_args: '--swc',
   }],
 };
