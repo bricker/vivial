e1037c4a88dc55961bb092be28abf1b1f6f7a48e
Bryan Ricker <978899+bricker@users.noreply.github.com>
2023-06-10 15:05:26 -0700

    eslint config updates

--- develop/javascript/es-config/eslint/typescript.js
+++ develop/javascript/es-config/eslint/typescript.js
@@ -1,22 +1,30 @@
 const config = {
-  parserOptions: {
-    project: './tsconfig.json',
-  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       extends: ['plugin:@typescript-eslint/recommended'],
-      plugins: ['@typescript-eslint'],
+      plugins: [
+        '@typescript-eslint',
+        'unused-imports',
+      ],
       parser: '@typescript-eslint/parser',
       rules: {
-        '@typescript-eslint/no-floating-promises': 'error',
-        '@typescript-eslint/no-non-null-assertion': 'off',
-        '@typescript-eslint/no-shadow': 'error',
+        '@typescript-eslint/no-floating-promises': 'warn', // A genuine source of bugs
+        '@typescript-eslint/no-non-null-assertion': 'off', // Useful language feature
+        '@typescript-eslint/no-shadow': 'warn',
+        'no-shadow': 'off', // There is a bug with this rule for typescript files, replaced by above
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-empty-interface': 'off',
         '@typescript-eslint/no-empty-function': 'off',
-        '@typescript-eslint/no-unused-vars': 'off', // TODO: Add back no-unused-vars
-        'no-shadow': 'off', // There is a bug with this rule for typescript files
+        '@typescript-eslint/no-unused-vars': 'off', // This is replaced by the unused-imports plugin
+        'unused-imports/no-unused-imports': 'warn',
+        'unused-imports/no-unused-vars': [
+          'warn',
+          {
+            varsIgnorePattern: '^_',
+            argsIgnorePattern: '^_',
+          },
+        ],
       },
     },
   ],
