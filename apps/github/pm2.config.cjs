1b3267bf7d1af8a6949fa28dc02f22a07b073ec4
Bryan Ricker <978899+bricker@users.noreply.github.com>
2023-06-13 13:47:28 -0700

    update github app to server/app modules

--- apps/github/pm2.config.cjs
+++ apps/github/pm2.config.cjs
@@ -3,7 +3,7 @@ const path = require('node:path');
 module.exports = {
   apps: [{
     name: 'github',
-    script: './src/app.ts',
+    script: './server.ts',
     out_file: '/dev/stdout',
     error_file: '/dev/stderr',
     interpreter: path.join(__dirname, 'node_modules/.bin/ts-node'),
--- apps/github/src/app.ts
+++ apps/github/src/app.ts
@@ -3,9 +3,9 @@ import eaveLogger from '@eave-fyi/eave-stdlib-ts/src/logging.js';
 import { applyCommonRequestMiddlewares, applyCommonResponseMiddlewares, applyInternalApiMiddlewares, helmetMiddleware } from '@eave-fyi/eave-stdlib-ts/src/middleware/common-middlewares.js';
 import { GAELifecycleRouter, StatusRouter } from '@eave-fyi/eave-stdlib-ts/src/api-util.js';
 import { WebhookRouter, applyWebhookMiddlewares } from './events/routes.js';
-import { InternalApiRouter } from './requests/routes.js';
+import { InternalApiRouter } from './api/routes.js';
 
-const app = express();
+export const app = express();
 app.use(helmetMiddleware());
 applyCommonRequestMiddlewares({ app });
 applyInternalApiMiddlewares({ app, path: '/github/api' });
@@ -20,10 +20,3 @@ rootRouter.use('/events', WebhookRouter());
 rootRouter.use('/api', InternalApiRouter());
 
 applyCommonResponseMiddlewares({ app });
-
-const PORT = parseInt(process.env['PORT'] || '5300', 10);
-app.listen(PORT, '0.0.0.0', () => {
-  eaveLogger.info(`App listening on port ${PORT}`);
-});
-
-export default app;
