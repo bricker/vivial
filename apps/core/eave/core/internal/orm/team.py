19798e5b45c1b820c0fdef3d3cbf92c6a9e4057c
niehusst <liamniehusstaab@gmail.com>
2023-06-14 17:04:05 -0600

    refactor for flexibility and assumption verification

--- apps/core/eave/core/internal/orm/team.py
+++ apps/core/eave/core/internal/orm/team.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Optional, Self, Tuple, TypedDict, Unpack
+from typing import Any, Optional, Self, Sequence, Tuple, TypedDict, Unpack
 from uuid import UUID
 import sqlalchemy.sql
 from eave.core.internal.document_client import DocumentClient
@@ -7,7 +7,7 @@ from eave.core.internal.orm.atlassian_installation import AtlassianInstallationO
 from eave.core.internal.orm.confluence_destination import ConfluenceDestinationOrm
 from eave.core.internal.orm.connect_installation import ConnectInstallationOrm
 import eave.stdlib
-from sqlalchemy import Select, false, func, select
+from sqlalchemy import Row, Select, false, func, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from eave.stdlib.core_api.models.connect import AtlassianProduct
@@ -109,53 +109,68 @@ class TeamOrm(Base):
         return result
 
     async def get_integrations(self, session: AsyncSession) -> Integrations:
-        import time  # debug
-        start = time.perf_counter_ns()
-
-        # slack_installation = await SlackInstallationOrm.one_or_none(session=session, team_id=self.id)
-        # github_installation = await GithubInstallationOrm.one_or_none(session=session, team_id=self.id)
-        # atlassian_installation = await AtlassianInstallationOrm.one_or_none(session=session, team_id=self.id)
-        # confluence_installation = await ConnectInstallationOrm.one_or_none(
-        #     session=session, team_id=self.id, product=AtlassianProduct.confluence
-        # )
-        # jira_installation = await ConnectInstallationOrm.one_or_none(
-        #     session=session, team_id=self.id, product=AtlassianProduct.jira
-        # )
-
-        s = sqlalchemy.sql.select(TeamOrm, SlackInstallationOrm, GithubInstallationOrm, AtlassianInstallationOrm, ConnectInstallationOrm) \
-            .join(SlackInstallationOrm, TeamOrm.id == SlackInstallationOrm.team_id, isouter=True) \
-            .join(GithubInstallationOrm, TeamOrm.id == GithubInstallationOrm.team_id, isouter=True) \
-            .join(AtlassianInstallationOrm, TeamOrm.id == AtlassianInstallationOrm.team_id, isouter=True) \
-            .join(ConnectInstallationOrm, TeamOrm.id == ConnectInstallationOrm.team_id, isouter=True) \
+        query = (
+            sqlalchemy.sql.select(
+                TeamOrm, SlackInstallationOrm, GithubInstallationOrm, AtlassianInstallationOrm, ConnectInstallationOrm
+            )
+            .join(SlackInstallationOrm, TeamOrm.id == SlackInstallationOrm.team_id, isouter=True)
+            .join(GithubInstallationOrm, TeamOrm.id == GithubInstallationOrm.team_id, isouter=True)
+            .join(AtlassianInstallationOrm, TeamOrm.id == AtlassianInstallationOrm.team_id, isouter=True)
+            .join(ConnectInstallationOrm, TeamOrm.id == ConnectInstallationOrm.team_id, isouter=True)
             .filter(TeamOrm.id == self.id)
-        
-        # returns upto 2 rows: 1 w/ jira connectinstall and other w/ confluence connectinstall (other col data is same)
-        query_res = (await session.execute(s)).all()
+        )
 
-        end = time.perf_counter_ns()
-        print(f"\n\n\n elapcsed: {(end - start) / 1e9}\n\n\n") # 0.2 to 0.4 seconds in my empty ass db and also in db w/ 1000 rows??
-
+        # contains up to 2 rows:
+        # 1 w/ jira connectinstall and other w/ confluence connectinstall (other column data is same)
+        query_res: Sequence[
+            Row[
+                Tuple[
+                    TeamOrm,
+                    SlackInstallationOrm,
+                    GithubInstallationOrm,
+                    AtlassianInstallationOrm,
+                    ConnectInstallationOrm,
+                ]
+            ]
+        ] = (await session.execute(query)).all()
         assert len(query_res) <= 2, "Expected 2 or fewer rows of results from joined installations table"
 
-        # TODO: clean up this garbage and use some dang types
-        slack_install = github_install = atlassian_install = confluence_install = jira_install = None
-        if len(query_res) > 0:
-            # we dont care about TeamOrm that is first in unpack, so we ignore with _
-            # we dont know whether the ConnectInstallationOrm is for jira or confluence yet, so also ignore
-            _, slack_install, github_install, atlassian_install, _ = query_res[0]
-            
-            if query_res[0][-1]:
-                match query_res[0][-1].product:
+        slack_install: Optional[SlackInstallationOrm] = None
+        github_install: Optional[GithubInstallationOrm] = None
+        atlassian_install: Optional[AtlassianInstallationOrm] = None
+        confluence_install: Optional[ConnectInstallationOrm] = None
+        jira_install: Optional[ConnectInstallationOrm] = None
+
+        def validate_data(curr: Any, next: Any) -> None:
+            """
+            Helper function to validate data consistency assumptions
+            """
+            if curr:
+                assert (
+                    curr == next
+                ), "Violation of assumption that integrations join table columns have the same values across rows"
+
+        for results_row in query_res:
+            # we dont care about TeamOrm that is first in unpacking, so we ignore with _
+            _, slack_tmp, github_tmp, atlassian_tmp, connect_tmp = results_row
+
+            validate_data(slack_install, slack_tmp)
+            slack_install = slack_tmp
+
+            validate_data(github_install, github_tmp)
+            github_install = github_tmp
+
+            validate_data(atlassian_install, atlassian_tmp)
+            atlassian_install = atlassian_tmp
+
+            if connect_tmp:
+                match connect_tmp.product:
                     case AtlassianProduct.confluence:
-                        confluence_install = query_res[0][-1]
+                        validate_data(confluence_install, connect_tmp)
+                        confluence_install = connect_tmp
                     case AtlassianProduct.jira:
-                        jira_install = query_res[0][-1]
-        if len(query_res) > 1 and query_res[1][-1]:
-            match query_res[1][-1].product:
-                case AtlassianProduct.confluence:
-                    confluence_install = query_res[1][-1]
-                case AtlassianProduct.jira:
-                    jira_install = query_res[1][-1]
+                        validate_data(jira_install, connect_tmp)
+                        jira_install = connect_tmp
 
         return Integrations(
             slack_integration=slack_install.api_model_peek if slack_install else None,
