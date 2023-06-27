84aa8203b8581df0bc2997235769299d1ae4d2ee
Bryan Ricker <978899+bricker@users.noreply.github.com>
2023-06-26 17:55:44 -0700

    fix whitelist

--- apps/marketing/eave/marketing/app.py
+++ apps/marketing/eave/marketing/app.py
@@ -154,15 +154,15 @@ def _clean_response(eave_response: account.GetAuthenticatedAccountTeamIntegratio
     access_token = eave_response.account.access_token
     del eave_response.account.access_token
 
+    if shared_config.eave_beta_whitelist_disabled:
+        eave_response.team.beta_whitelisted = True
+
     response = _json_response(body=eave_response.json())
 
     eave.stdlib.cookies.set_auth_cookies(
         response=response,
         access_token=access_token,  # In case the access token was refreshed
     )
-
-    if shared_config.eave_beta_whitelist_disabled:
-        eave_response.team.beta_whitelisted = True
 
     # TODO: Forward cookies from server to client
     return response
