ccc2f875069615d6ddd8c08835f97264de95c178
Bryan Ricker <978899+bricker@users.noreply.github.com>
2023-06-29 11:43:46 -0700

    check for analytics enabled in ts analytics

--- libs/eave-stdlib-ts/src/analytics.ts
+++ libs/eave-stdlib-ts/src/analytics.ts
@@ -2,6 +2,7 @@ import { PubSub, Encodings } from '@google-cloud/pubsub';
 import * as schemas from '@eave-fyi/eave-pubsub-schemas/src/generated/eave_event.js';
 import eaveLogger, { LogContext } from './logging.js';
 import { sharedConfig } from './config.js';
+import { JsonObject } from './types.js';
 
 const EVENT_TOPIC_ID = 'eave_event_topic';
 const pubSubClient = new PubSub();
@@ -27,6 +28,12 @@ export async function logEvent(event: schemas.EaveEvent, ctx?: LogContext) {
   event.event_ts = new Date().getTime();
 
   const protoMessage = schemas.EaveEvent.encode(event).finish();
-  const messageId = await topic.publishMessage({ data: protoMessage });
-  eaveLogger.debug(`published message to pubsub ${messageId}`, ctx);
+
+  if (sharedConfig.analyticsEnabled) {
+    const messageId = await topic.publishMessage({ data: protoMessage });
+    eaveLogger.debug(`published message to pubsub ${messageId}`, ctx);
+  } else {
+    const serializedEvent = <JsonObject>schemas.EaveEvent.toJSON(event);
+    eaveLogger.warning('Analytics disabled', { event: serializedEvent }, ctx);
+  }
 }
