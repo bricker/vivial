5b57746a8549c32f0567db5cee4a6dfb6b5d8f30
niehusst <liamniehusstaab@gmail.com>
2023-07-11 17:41:12 -0600

    switch to gql for gh api reqs

--- apps/github/src/events/pull-request.ts
+++ apps/github/src/events/pull-request.ts
@@ -2,14 +2,11 @@ import { PullRequestEvent } from '@octokit/webhooks-types';
 import { GitHubOperationsContext } from '../types.js';
 import eaveLogger from '@eave-fyi/eave-stdlib-ts/src/logging.js';
 import OpenAIClient, { OpenAIModel } from '@eave-fyi/eave-stdlib-ts/src/openai.js';
+import { Query, Scalars, Blob, Repository, PullRequest, PullRequestChangedFileConnection, PullRequestChangedFile, PatchStatus } from '@octokit/graphql-schema';
+import * as GraphQLUtil from '../lib/graphql-util.js';
+
 import { appConfig } from '../config.js';
 import { getGithubInstallation } from '@eave-fyi/eave-stdlib-ts/src/core-api/operations/github.js';
-
-// TODO: move elsewhere
-type GithubFile = {
-  filename: String,
-  contents_url: String,
-}
 
 
 /**
@@ -25,50 +22,99 @@ export default async function handler(event: PullRequestEvent, context: GitHubOp
   if (event.action !== 'closed' || !event.pull_request.merged) {
     return;
   }
+  
+  const repoOwner = event.repository.owner.name;
+  const repoName = event.repository.name;
 
   const { ctx, octokit } = context;
   eaveLogger.debug('Processing push', ctx);
 
   // find files changed in PR commits
-  let page = 1;
-  const pageSize = 50;
   let keepPaginating = true;
-  let files: Array<GithubFile> = [];
+  let filePaths: Array<string> = [];
+  const filesQuery = await GraphQLUtil.loadQuery('listFilesInPullRequest');
+  const filesQueryVariables: {
+    repoOwner: Scalars['String'],
+    repoName: Scalars['String'],
+    prNumber: Scalars['Int'], 
+    batchSize: Scalars['Int'],
+    after?: Scalars['String'],
+  } = {
+    repoOwner,
+    repoName,
+    prNumber: event.pull_request.number,
+    batchSize: 50, // max 100
+  };
 
+  // paginate to collect all files
   while (keepPaginating) {
-    // fetch paginated list of files
-    // Note: Responses include a maximum of 3000 files. 
-    // https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28#list-pull-requests-files
-    let { data: filesResp } = await octokit.rest.pulls.listFiles({
-      per_page: pageSize,
-      page,
-      owner: event.repository.owner.name,
-      repo: event.repository.name,
-      pull_number: event.pull_request.number,
-    });
+    const queryResp = await octokit.graphql<{ repository: Query['repository'] }>(filesQuery, filesQueryVariables);
+    const prRepo = <Repository>queryResp.repository;
+    const pr = <PullRequest>prRepo?.pullRequest;
+    const prFilesConnection = <PullRequestChangedFileConnection>pr?.files;
+    const prFileNodes = <Array<PullRequestChangedFile>>prFilesConnection?.nodes;
 
-    // extract files that were changed
-    const documentableFiles = filesResp.filter((f) => f.status === 'added' || f.status === 'changed' || f.status === 'modified');
+    if (!prFileNodes) {
+      eaveLogger.error('Failed to acquire file list from PR while processing PR merge event');
+      return;
+    }
 
-    // TODO: extract files that we want comments in
+    // extract files that new docs updates
+    const documentableFiles = prFileNodes.filter((f) => f.changeType === 'ADDED' || f.changeType === 'MODIFIED');
 
-    // convert api resp to GithubFile type (?)
-    files = files.concat(documentableFiles);
+    // TODO: extract files that we want inline comments in
+
+
+    filePaths = filePaths.concat(documentableFiles.map((f) => f.path));
 
     // continue while files fill page size
-    keepPaginating = filesResp.length === pageSize;
+    keepPaginating = prFilesConnection.pageInfo.hasNextPage;
+    // assign query vars `after` field so that next query will continue paginating
+    // files where the previous request left off
+    filesQueryVariables.after = prFilesConnection.pageInfo.endCursor;
   }
 
 
   // update docs in each file
   const openaiClient = await OpenAIClient.getAuthedClient();
+  const contentsQuery = await GraphQLUtil.loadQuery('getFileContentsByPath');
+
+  await Promise.all(filePaths.map(async (fpath) => {
+    const contentsQueryVariables: {
+      repoOwner: Scalars['String'],
+      repoName: Scalars['String'],
+      expression: Scalars['String'],
+    } = {
+      repoOwner: event.repository.owner.name,
+      repoName: event.repository.name,
+      expression: `${event.pull_request.base.ref}:${fpath}`, // branch:fpath
+    };
+
+    const response = await octokit.graphql<{ repository: Query['repository'] }>(contentsQuery, contentsQueryVariables);
+    const objectRepository = <Repository>response.repository;
+    const gitObject = <Blob>objectRepository?.object;
+    const fileContent = gitObject?.text;
+    if (!fileContent) {
+      eaveLogger.error(`Error fetching file content for ${fpath}`, ctx); // TODO: is it ok to log file name/path? is that too sensitive?
+      return;
+    }
+    
+    // see if existing docs
+
+    // write new docs
+
+  }));
 
 
-  // branch off event.pull_request.base branch
+  // branch off event.pull_request.base.ref 
+  // https://stackoverflow.com/a/9513594/9718199
 
   // commit changes
+  // https://stackoverflow.com/a/46760154/9718199
+  // https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#create-or-update-file-contents
 
-  // open PR against event.pull_request.base branch
+  // open PR against event.pull_request.base.ref
+  // https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28#create-a-pull-request
 
 
 }
