f21b36f080fda1e5b7cad4623c1a39fc3b364402
Liam Niehus-Staab <34754390+niehusst@users.noreply.github.com>
2023-07-20 15:53:20 -0600

    docs: automated update [ci skip]

--- apps/github/src/events/routes.ts
+++ apps/github/src/events/routes.ts
@@ -1,3 +1,27 @@
+In shell scripting, comments are typically written using the hash symbol (#). However, multi-line comments are not directly supported. You can use multiple single-line comments instead. Here's how you can convert the given text to comments in shell:
+
+```shell
+# The preceding code file is designed to manage webhooks within an Express application. 
+# It is primarily responsible for setting up middleware to handle raw JSON data and 
+# creating a router to process incoming POST requests. 
+# The file plays a crucial role in enabling the application to receive and 
+# correctly process incoming webhook events, particularly from GitHub.
+```
+
+/**
+* This code file is primarily responsible for setting up and managing webhooks in an Express application.
+* It exports two main functions: applyWebhookMiddlewares and WebhookRouter.
+*
+* The applyWebhookMiddlewares function is used to apply middleware to the Express application, specifically
+* for handling raw JSON data. This is necessary for correctly processing GitHub signature verifications.
+*
+* The WebhookRouter function creates and returns a new Express Router. This router is configured to handle
+* POST requests at its root path. Upon receiving a request, it dispatches the request and response objects
+* to a separate function for further processing.
+*
+* Overall, this file is crucial for enabling the application to receive and correctly process incoming webhook events.
+*/
+
 import { NextFunction, Request, Response, Router, raw, Express } from 'express';
 import dispatch from '../dispatch.js';
 
