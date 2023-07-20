e14d4ccc86e082545f6f3fcf1a3b5bef3154bf02
Liam Niehus-Staab <34754390+niehusst@users.noreply.github.com>
2023-07-20 16:22:17 -0600

    docs: automated update [ci skip]

--- apps/github/src/events/routes.ts
+++ apps/github/src/events/routes.ts
@@ -1,3 +1,17 @@
+```shell
+: '
+This code file serves as a crucial configuration and routing module for handling webhooks in an Express.js application. It exports two main functions: `applyWebhookMiddlewares` and `WebhookRouter`.
+
+The `applyWebhookMiddlewares` function is used to apply middleware to the Express application, specifically for parsing incoming raw JSON data from webhooks. This is a necessary step for accurate GitHub signature verification, ensuring the correct processing of this data.
+
+The `WebhookRouter` function is responsible for creating and returning a new Express Router. This router is configured to handle incoming POST requests at the root ("/") of the application. Upon receiving a request, it uses the `dispatch` function to handle these requests and includes error handling logic, dispatching the request and response objects to a separate function for further processing.
+
+Overall, the purpose of this file is to set up and manage the routing and data handling for incoming webhook events within the application, enabling the application to receive and correctly process these events.
+'
+```
+Please note that Shell scripts do not have a specific syntax for multi-line comments. The above approach is a common workaround using a no-op command `:` and a multi-line string.
+
+
 import { NextFunction, Request, Response, Router, raw, Express } from 'express';
 import dispatch from '../dispatch.js';
 
