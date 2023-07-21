134968c6a7e9c5dcb44b277ed41151d941089a0e
Liam Niehus-Staab <34754390+niehusst@users.noreply.github.com>
2023-07-20 18:53:53 -0600

    docs: automated update [ci skip]

--- apps/github/src/events/routes.ts
+++ apps/github/src/events/routes.ts
@@ -1,3 +1,19 @@
+/**
+ * This code file is primarily designed to manage and set up webhooks in an Express.js application.
+ * It exports two main functions: applyWebhookMiddlewares and WebhookRouter.
+ * 
+ * The applyWebhookMiddlewares function is used to apply middleware to the application, specifically for handling raw JSON data.
+ * This is crucial for correctly processing GitHub's signature verification system and ensuring data integrity.
+ * 
+ * The WebhookRouter function, on the other hand, creates and returns a new Express Router that is configured to handle POST requests at its root path.
+ * Upon receiving a request, it dispatches the request and response objects to a separate function for further processing.
+ * 
+ * The main purpose of this file is to receive, process, and route incoming webhook requests to the appropriate handlers within the application,
+ * making it a vital component for the application's ability to correctly process incoming webhook events.
+ */
+
+
+
 import { NextFunction, Request, Response, Router, raw, Express } from 'express';
 import dispatch from '../dispatch.js';
 
