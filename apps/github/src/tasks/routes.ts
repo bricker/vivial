59d046082e7a87330472cd92dc8ea4f7bdeb826c
Bryan Ricker <978899+bricker@users.noreply.github.com>
2023-08-07 15:58:09 -0700

    checkpoint

---
+++ apps/github/src/tasks/routes.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response, Router } from 'express';
+import { inlineCodeDocs } from './inline-code-docs.js';
+
+export function InternalApiRouter(): Router {
+  const router = Router();
+
+  router.post('/inline-code-docs', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await inlineCodeDocs(req, res);
+      res.end(); // safety
+    } catch (e: unknown) {
+      next(e);
+    }
+  });
+
+  return router;
+}
