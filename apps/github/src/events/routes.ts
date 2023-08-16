c32902655257563a6d5113ce81487791ae8c9bb7
Liam Niehus-Staab <34754390+niehusst@users.noreply.github.com>
2023-08-16 15:00:20 -0600

    docs: automated update

--- apps/github/src/events/routes.ts
+++ apps/github/src/events/routes.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response, Router, raw, Express } from 'express';
 import dispatch from '../dispatch.js';
 
+It seems like you've provided an incomplete input. Could you please provide more details or context? I'm here to help with TypeScript doc comments.
 export function applyWebhookMiddlewares({ app, path }:{ app: Express, path: string }) {
   /*
   Using raw parsing rather than express.json() parser because of GitHub signature verification.
@@ -9,6 +10,15 @@ export function applyWebhookMiddlewares({ app, path }:{ app: Express, path: stri
   app.use(path, raw({ type: 'application/json', limit: '5mb' }));
 }
 
+It seems like you're trying to start a TypeScript doc comment. Here's an example of how you can do it:
+
+```typescript
+/**
+ * This is a TypeScript doc comment.
+ */
+```
+
+In TypeScript, doc comments are used to provide detailed explanations about the code, which can include descriptions of functions, classes, interfaces, etc. They are written inside `/** */` tags.
 export function WebhookRouter(): Router {
   const router = Router();
 
