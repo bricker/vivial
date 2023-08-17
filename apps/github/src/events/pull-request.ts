a0b5bfa19e92e884c2b1bb7ec2aa8f41261aa182
niehusst <liamniehusstaab@gmail.com>
2023-08-17 14:38:03 -0700

    refactor: move documentation creation function to ts stdlib

--- apps/github/src/events/pull-request.ts
+++ apps/github/src/events/pull-request.ts
@@ -1,6 +1,6 @@
 import { PullRequestEvent } from '@octokit/webhooks-types';
 import path from 'path';
-import eaveLogger, { LogContext } from '@eave-fyi/eave-stdlib-ts/src/logging.js';
+import eaveLogger from '@eave-fyi/eave-stdlib-ts/src/logging.js';
 import OpenAIClient, { OpenAIModel, formatprompt } from '@eave-fyi/eave-stdlib-ts/src/transformer-ai/openai.js';
 import {
   Query,
@@ -16,10 +16,9 @@ import {
   CommittableBranch,
 } from '@octokit/graphql-schema';
 import { Octokit } from 'octokit';
-import * as AIUtil from '@eave-fyi/eave-stdlib-ts/src/transformer-ai/util.js';
-import { getExtensionMap, isSupportedProgrammingLanguage } from '@eave-fyi/eave-stdlib-ts/src/language-mapping.js';
+import { isSupportedProgrammingLanguage } from '@eave-fyi/eave-stdlib-ts/src/language-mapping.js';
+import { updateDocumentation } from '@eave-fyi/eave-stdlib-ts/src/function-documenting.js';
 import { logEvent } from '@eave-fyi/eave-stdlib-ts/src/analytics.js';
-import { writeUpdatedCommentsIntoFileString, parseFunctionsAndComments } from '@eave-fyi/eave-stdlib-ts/src/parsing/function-parsing.js';
 import { GitHubOperationsContext } from '../types.js';
 import * as GraphQLUtil from '../lib/graphql-util.js';
 import { appConfig } from '../config.js';
@@ -262,106 +261,4 @@ async function deleteBranch(octokit: Octokit, branchNodeId: string) {
     refNodeId: branchNodeId,
   };
   await octokit.graphql<{ resp: Mutation['deleteRef'] }>(query, params);
-}
-
-/**
- * Given the current content of a file, returns the same file
- * content but with the documentation updated to reflect any code changes.
- *
- * @param currContent a file's content in plaintext
- * @param filePath file path correlated with `currContent` file content
- * @param openaiClient
- * @param ctx extra context for more detailed logs
- * @returns the same code content as `currContent` but with doc strings updated, or null if unable to create updated document
- */
-async function updateDocumentation(currContent: string, filePath: string, openaiClient: OpenAIClient, ctx: LogContext): Promise<string | null> {
-  // load language from file extension map file
-  const extName = `${path.extname(filePath).toLowerCase()}`;
-  const flang = (await getExtensionMap())[extName];
-  if (!flang) {
-    // file extension not found in the map file, which makes it impossible for us to
-    // put docs in a syntactially valid comment; exit early
-    eaveLogger.error(`No matching language found for file extension: "${extName}"`);
-    return null;
-  }
-
-  const parsedData = parseFunctionsAndComments(currContent, extName, flang);
-  if (parsedData.length === 0) {
-    eaveLogger.error(`Unable to parse ${flang} from ${extName} file`);
-    return null;
-  }
-
-  // update parsedData objects in place w/ updatedCommentStrings
-  await Promise.all(parsedData.map(async (funcData) => {
-    // convert long function strings to a summary for docs writing to prevent AI from getting overwhelmed by
-    // implementation details in raw code file (and to account for functions longer than model context)
-    const summarizedFunction = await AIUtil.rollingSummary(openaiClient, funcData.func);
-
-    // update docs, or write new ones if currDocs is empty/undefined
-    // TODO: retest w/ summarized function
-    // TODO: experiment performance qulaity on dif types of comments:
-    //      (1. update own comment 2. write from scratch 3. update existing detailed docs 4. fix slightly incorrect docs)
-    const docsPrompt = formatprompt(
-      `Write a ${flang} doc comment for the following function.\n`,
-      '===',
-      summarizedFunction,
-      '===',
-    );
-    const newDocsResponse = await openaiClient.createChatCompletion({
-      parameters: {
-        messages: [
-          {
-            role: 'system',
-            content: `You must respond with only a valid ${flang} doc comment.`,
-          },
-          {
-            role: 'user',
-            content: docsPrompt,
-          },
-        ],
-        model: OpenAIModel.GPT4,
-        temperature: 0,
-      },
-      ctx,
-    });
-
-    // if there were already existing docs, update them using newly written docs
-    // TODO: how to handle comment merging...
-    let updatedDocs = newDocsResponse;
-    if (funcData.comment) {
-      updatedDocs = await openaiClient.createChatCompletion({
-        parameters: {
-          messages: [
-            {
-              role: 'system',
-              content: `You must respond with only a valid ${flang} doc comment.`,
-            },
-            {
-              role: 'user',
-              content: formatprompt(
-                `Merge these two ${flang} doc comments, maintaining the important information.`,
-                `If there are any conflicts of content, prefer the new documentation. Return only the ${flang} doc comment.\n`,
-                'Old documentation:',
-                '===',
-                funcData.comment,
-                '===\n',
-                'New documentation:',
-                '===',
-                newDocsResponse,
-                '===',
-              ),
-            },
-          ],
-          model: OpenAIModel.GPT4,
-          temperature: 0,
-        },
-        ctx,
-      });
-    }
-
-    funcData.updatedComment = updatedDocs;
-  }));
-
-  // write `updatedComment` data back into currContent string
-  return writeUpdatedCommentsIntoFileString(currContent, parsedData);
 }
