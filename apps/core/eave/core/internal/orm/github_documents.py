f664eca141551765d0623ad648dd7f57c4e6e5b5
niehusst <liamniehusstaab@gmail.com>
2023-09-05 17:23:36 -0700

    fix: make pr number optional

--- apps/core/eave/core/internal/orm/github_documents.py
+++ apps/core/eave/core/internal/orm/github_documents.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from enum import StrEnum
-from typing import Optional, Self
+from pydoc import Doc
+from typing import NotRequired, Optional, Self, Sequence, TypedDict, Unpack, Tuple
 from uuid import UUID
 
-from sqlalchemy import ForeignKeyConstraint, PrimaryKeyConstraint, Index
+from sqlalchemy import ForeignKeyConstraint, PrimaryKeyConstraint, Index, Select
 from sqlalchemy import func, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column
@@ -45,11 +46,9 @@ class GithubDocumentsOrm(Base):
     id: Mapped[UUID] = mapped_column(server_default=UUID_DEFAULT_EXPR)
     external_repo_id: Mapped[str] = mapped_column()
     """Github API node_id for this repo"""
-    pull_request_number: Mapped[int] = mapped_column()
+    pull_request_number: Mapped[Optional[int]] = mapped_column(server_default=None)
     """Number of the most recent PR opened for this document"""
-    status: Mapped[Status] = mapped_column(
-        server_default=Status.PROCESSING.value
-    )  # TODO: should default up-to-date instead??
+    status: Mapped[Status] = mapped_column(server_default=Status.PROCESSING.value)
     """Current state of API documentation for this repo. options: processing, under-review, up-to-date"""
     status_updated: Mapped[Optional[datetime]] = mapped_column(server_default=None)
     """Last time the `status` column was updated."""
@@ -65,3 +64,49 @@ class GithubDocumentsOrm(Base):
     @property
     def api_model(self) -> GithubDocument:
         return GithubDocument.from_orm(self)
+
+    class QueryParams(TypedDict):
+        id: NotRequired[UUID | str]
+        team_id: NotRequired[UUID | str]
+        external_repo_id: NotRequired[str]
+
+    @classmethod
+    def _build_query(cls, **kwargs: Unpack[QueryParams]) -> Select[Tuple[Self]]:
+        lookup = select(cls)
+
+        if id := kwargs.get("id"):
+            lookup.where(cls.id == id)
+        if team_id := kwargs.get("team_id"):
+            lookup.where(cls.team_id == team_id)
+        if external_repo_id := kwargs.get("external_repo_id"):
+            lookup.where(cls.external_repo_id == external_repo_id)
+
+        return lookup
+    
+    @classmethod
+    async def query(cls, session: AsyncSession, **kwargs: Unpack[QueryParams]) -> Sequence[Self]:
+        stmt = cls._build_query(**kwargs)
+        results = (await session.scalars(stmt)).all()
+        return results
+
+    @classmethod
+    async def create(
+        cls,
+        session: AsyncSession,
+        team_id: UUID,
+        external_repo_id: str,
+        pull_request_number: Optional[int],
+        status: Status,
+        status_updated: datetime,
+        file_path: str,
+        api_name: str,
+        type: DocumentType,
+    ) -> Self:
+        obj = cls(
+            team_id=team_id,
+            external_repo_id=external_repo_id,
+
+        )
+        session.add(obj)
+        await session.flush()
+        return obj
