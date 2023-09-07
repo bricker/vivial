b6554c633d64d9cdce51f70915214ff2d2501ef5
Bryan Ricker <git@bryanricker.com>
2023-09-07 12:59:39 -0700

    vscode python tools extensions
    https://github.com/microsoft/vscode-python/wiki/Migration-to-Python-Tools-Extensions

--- apps/core/tests/core/__init__.py
+++ apps/core/tests/core/__init__.py
@@ -10,6 +10,9 @@ eave.stdlib.time.set_utc()
 
 dotenv.load_dotenv(dotenv_path=os.path.join(os.environ["EAVE_HOME"], ".env.test"), override=True)
 
+os.environ["EAVE_MONITORING_DISABLED"] = "1"
+os.environ["EAVE_ANALYTICS_DISABLED"] = "1"
+
 os.environ["EAVE_PUBLIC_API_BASE"] = "https://api.eave.tests"
 os.environ["EAVE_PUBLIC_APPS_BASE"] = "https://apps.eave.tests"
 os.environ["EAVE_PUBLIC_WWW_BASE"] = "https://www.eave.tests"
