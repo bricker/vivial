5f522bac229a98e8d27b62818e80178508b5c4c9
niehusst <liamniehusstaab@gmail.com>
2023-09-08 14:53:12 -0700

    feat: add ts stdlib translation

---
+++ libs/eave-stdlib-ts/src/core-api/models/github-documents.ts
@@ -0,0 +1,50 @@
+export enum Status {
+  PROCESSING = 'processing',
+  PR_OPENED = 'pr_opened',
+  PR_MERGED = 'pr_merged',
+}
+
+export enum DocumentType {
+  API_DOCUMENT = 'api_document',
+  ARCHITECTURE_DOCUMENT = 'architecture_document',
+}
+
+export type GithubDocument = {
+  team_id: string;
+  external_repo_id: string;
+  pull_request_number?: number;
+  status: Status;
+  status_updated?: Date;
+  file_path: string;
+  api_name: string;
+  type: DocumentType;
+}
+
+export type GithubDocumentsQueryInput = {
+  external_repo_id?: string;
+  type?: DocumentType;
+}
+
+export type GithubDocumentCreateInput = {
+  external_repo_id: string;
+  file_path: string;
+  api_name: string;
+  type: DocumentType;
+  pull_request_number?: number;
+}
+
+export type GithubDocumentValuesInput = {
+  pull_request_number?: number;
+  status?: Status;
+  file_path?: string;
+  api_name?: string;
+}
+
+export type GithubDocumentUpdateInput = {
+  external_repo_id: string;
+  new_values: GithubDocumentValuesInput;
+}
+
+export type GithubDocumentDeleteInput = {
+  external_repo_id: string;
+}
---
+++ libs/eave-stdlib-ts/src/core-api/operations/github-documents.ts
@@ -0,0 +1,82 @@
+import { sharedConfig } from '../../config.js';
+import { EaveService } from '../../eave-origins.js';
+import { makeRequest, RequestArgsAuthedRequest } from '../../requests.js';
+import {
+  GithubDocument,
+  GithubDocumentCreateInput,
+  GithubDocumentDeleteInput,
+  GithubDocumentsQueryInput,
+  GithubDocumentUpdateInput,
+} from '../models/github-documents.js';
+
+const baseUrl = sharedConfig.eaveInternalServiceBase(EaveService.api);
+
+export type GetGithubDocumentsRequestBody = {
+  query_params: GithubDocumentsQueryInput;
+}
+
+export type GetGithubDocumentsResponseBody = {
+  documents: Array<GithubDocument>;
+}
+
+export async function getGithubDocuments(
+  args: RequestArgsAuthedRequest & { input: GetGithubDocumentsRequestBody },
+): Promise<GetGithubDocumentsResponseBody> {
+  const resp = await makeRequest({
+    url: `${baseUrl}/github-documents/query`,
+    ...args,
+  });
+  const responseData = <GetGithubDocumentsResponseBody>(await resp.json());
+  return responseData;
+}
+
+export type CreateGithubDocumentRequestBody = {
+  document: GithubDocumentCreateInput;
+}
+
+export type CreateGithubDocumentResponseBody = {
+  document: GithubDocument;
+}
+
+export async function createGithubDocuments(
+  args: RequestArgsAuthedRequest & { input: CreateGithubDocumentRequestBody },
+): Promise<CreateGithubDocumentResponseBody> {
+  const resp = await makeRequest({
+    url: `${baseUrl}/github-documents/create`,
+    ...args,
+  });
+  const responseData = <CreateGithubDocumentResponseBody>(await resp.json());
+  return responseData;
+}
+
+export type UpdateGithubDocumentRequestBody = {
+  document: GithubDocumentUpdateInput;
+}
+
+export type UpdateGithubDocumentResponseBody = {
+  document: GithubDocument;
+}
+
+export async function updateGithubDocuments(
+  args: RequestArgsAuthedRequest & { input: UpdateGithubDocumentRequestBody },
+): Promise<UpdateGithubDocumentResponseBody> {
+  const resp = await makeRequest({
+    url: `${baseUrl}/github-documents/update`,
+    ...args,
+  });
+  const responseData = <UpdateGithubDocumentResponseBody>(await resp.json());
+  return responseData;
+}
+
+export type DeleteGithubDocumentsRequestBody = {
+  documents: Array<GithubDocumentDeleteInput>;
+}
+
+export async function deleteGithubDocuments(
+  args: RequestArgsAuthedRequest & { input: DeleteGithubDocumentsRequestBody },
+): Promise<void> {
+  await makeRequest({
+    url: `${baseUrl}/github-documents/delete`,
+    ...args,
+  });
+}
