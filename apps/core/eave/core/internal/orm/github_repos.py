0cc97da704ded8cc93ac4aa201b81c040e75cc8a
Liam Niehus-Staab <34754390+niehusst@users.noreply.github.com>
2023-09-08 16:58:12 -0600

    feat(EAVE-308): github_repos ORM queries (#133)
    Ticket link:
    https://eave-fyi.atlassian.net/browse/EAVE-308
    
    Implements all ORM methods/sql junk necessary for GithubRepos API
    endpoints
    
    Did you run?:
    - [ ] unit tests
    - [ ] lint

--- apps/core/eave/core/internal/orm/github_repos.py
+++ apps/core/eave/core/internal/orm/github_repos.py
@@ -1,11 +1,14 @@
 from datetime import datetime
-from enum import StrEnum
-from typing import Optional
+from typing import NotRequired, Optional, Self, Sequence, TypedDict, Unpack, Tuple
 from uuid import UUID
 
-from sqlalchemy import PrimaryKeyConstraint
-from sqlalchemy import func
+from sqlalchemy import PrimaryKeyConstraint, Index, Select
+from sqlalchemy import func, select, delete
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column
+
+import eave.stdlib.util
+from eave.stdlib.core_api.models.repos import GithubRepo, State, Feature
 
 from .base import Base
 from .util import make_team_fk
@@ -19,12 +22,13 @@ class GithubRepoOrm(Base):
             "external_repo_id",
         ),
         make_team_fk(),
+        Index(
+            None,
+            "team_id",
+            "external_repo_id",
+            unique=True,
+        ),
     )
-
-    class State(StrEnum):
-        DISABLED = "disabled"
-        ENABLED = "enabled"
-        PAUSED = "paused"
 
     team_id: Mapped[UUID] = mapped_column()
     external_repo_id: Mapped[str] = mapped_column(unique=True)
@@ -38,6 +42,107 @@ class GithubRepoOrm(Base):
     created: Mapped[datetime] = mapped_column(server_default=func.current_timestamp())
     updated: Mapped[Optional[datetime]] = mapped_column(server_default=None, onupdate=func.current_timestamp())
 
-    # @property
-    # def api_model(self) -> GithubRepo:
-    #     return GithubRepo.from_orm(self)
+    @property
+    def api_model(self) -> GithubRepo:
+        return GithubRepo.from_orm(self)
+
+    class QueryParams(TypedDict):
+        team_id: UUID | str
+        external_repo_id: NotRequired[str]
+        api_documentation_state: NotRequired[State]
+        inline_code_documentation_state: NotRequired[State]
+        architecture_documentation_state: NotRequired[State]
+
+    @classmethod
+    def _build_query(cls, **kwargs: Unpack[QueryParams]) -> Select[Tuple[Self]]:
+        team_id = eave.stdlib.util.ensure_uuid(kwargs["team_id"])
+        lookup = select(cls).where(cls.team_id == team_id)
+
+        if external_repo_id := kwargs.get("external_repo_id"):
+            lookup = lookup.where(cls.external_repo_id == external_repo_id)
+
+        if api_documentation_state := kwargs.get("api_documentation_state"):
+            lookup = lookup.where(cls.api_documentation_state == api_documentation_state)
+
+        if inline_code_documentation_state := kwargs.get("inline_code_documentation_state"):
+            lookup = lookup.where(cls.inline_code_documentation_state == inline_code_documentation_state)
+
+        if architecture_documentation_state := kwargs.get("architecture_documentation_state"):
+            lookup = lookup.where(cls.architecture_documentation_state == architecture_documentation_state)
+
+        return lookup
+
+    @classmethod
+    async def create(
+        cls,
+        session: AsyncSession,
+        team_id: UUID,
+        external_repo_id: str,
+        api_documentation_state: State = State.DISABLED,
+        inline_code_documentation_state: State = State.DISABLED,
+        architecture_documentation_state: State = State.DISABLED,
+    ) -> Self:
+        obj = cls(
+            team_id=team_id,
+            external_repo_id=external_repo_id,
+            api_documentation_state=api_documentation_state.value,
+            inline_code_documentation_state=inline_code_documentation_state.value,
+            architecture_documentation_state=architecture_documentation_state.value,
+        )
+        session.add(obj)
+        await session.flush()
+        return obj
+
+    @classmethod
+    async def list_all(cls, team_id: UUID, session: AsyncSession) -> Sequence[Self]:
+        stmt = cls._build_query(team_id=team_id)
+        result = (await session.scalars(stmt)).all()
+        return result
+
+    @classmethod
+    async def one_or_exception(cls, team_id: UUID, external_repo_id: str, session: AsyncSession) -> Self:
+        stmt = cls._build_query(team_id=team_id, external_repo_id=external_repo_id).limit(1)
+        result = (await session.scalars(stmt)).one()
+        return result
+
+    @classmethod
+    async def one_or_none(cls, team_id: UUID, external_repo_id: str, session: AsyncSession) -> Self | None:
+        stmt = cls._build_query(team_id=team_id, external_repo_id=external_repo_id).limit(1)
+        result = await session.scalar(stmt)
+        return result
+
+    @classmethod
+    async def delete_by_repo_ids(cls, team_id: UUID, external_repo_ids: list[str], session: AsyncSession) -> None:
+        if len(external_repo_ids) < 1:
+            # no work to be done (also dont delete ALL entries for team_id)
+            return
+
+        stmt = delete(cls).where(cls.team_id == team_id).where(cls.external_repo_id.in_(external_repo_ids))
+        await session.execute(stmt)
+
+    @classmethod
+    async def all_repos_match_feature_state(
+        cls, team_id: UUID, feature: Feature, state: State, session: AsyncSession
+    ) -> bool:
+        """
+        Check if for a given `team_id` all their repos have the specified `state` for a `feature`.
+
+        This query will make use of the composite index for the team_id where clause, which should filter out
+        most entries. However, a linear scan will be used for the feature state comparisons. Hopefully it will
+        not be too expensive of a query to scan all the repos of a single team.
+        """
+        stmt = cls._build_query(team_id=team_id)
+
+        # we find all entries for feature status NOT matching the provided one.
+        # if there are 0 matches, that means all rows have the same status
+        # for `feature` (or there are 0 rows)
+        match feature:
+            case Feature.INLINE_CODE_DOCUMENTATION:
+                stmt = stmt.where(cls.inline_code_documentation_state != state.value)
+            case Feature.API_DOCUMENTATION:
+                stmt = stmt.where(cls.api_documentation_state != state.value)
+            case Feature.ARCHITECTURE_DOCUMENTATION:
+                stmt = stmt.where(cls.architecture_documentation_state != state.value)
+
+        result = (await session.scalars(stmt)).all()
+        return len(result) == 0
