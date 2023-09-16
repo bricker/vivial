649eaf91b42b36d4be115e2398f6242569353f46
Bryan Ricker <978899+bricker@users.noreply.github.com>
2023-09-16 14:59:12 -0700

    vscode python tools extensions (#136)
    https://github.com/microsoft/vscode-python/wiki/Migration-to-Python-Tools-Extensions
    
    Ticket link:
    <!-- here -->
    
    Did you run?:
    - [ ] unit tests
    - [ ] lint
    
    Co-authored-by: Liam Niehus-Staab <34754390+niehusst@users.noreply.github.com>

--- apps/core/tests/core/__init__.py
+++ apps/core/tests/core/__init__.py
@@ -10,6 +10,9 @@ eave.stdlib.time.set_utc()
 
 dotenv.load_dotenv(dotenv_path=os.path.join(os.environ["EAVE_HOME"], ".env.test"), override=True)
 
+os.environ["EAVE_MONITORING_DISABLED"] = "1"
+os.environ["EAVE_ANALYTICS_DISABLED"] = "1"
+
 os.environ["EAVE_API_BASE_PUBLIC"] = "https://api.eave.tests"
 os.environ["EAVE_APPS_BASE_PUBLIC"] = "https://apps.eave.tests"
 os.environ["EAVE_WWW_BASE_PUBLIC"] = "https://www.eave.tests"
