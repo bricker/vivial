2f011ec4fd55bad724744e2db28b630a48db1bcc
Bryan Ricker <git@bryanricker.com>
2023-09-17 15:31:59 -0700

    formatting

--- apps/github/src/app.ts
+++ apps/github/src/app.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-import { commonRequestMiddlewares, commonResponseMiddlewares, helmetMiddleware } from '@eave-fyi/eave-stdlib-ts/src/middleware/common-middlewares.js';
-import { GAELifecycleRouter, StatusRouter } from '@eave-fyi/eave-stdlib-ts/src/api-util.js';
-import { WebhookOfflineTaskRouter, WebhookRouter } from './events/routes.js';
-import { InternalApiRouter } from './api/routes.js';
-import { OfflineTaskRouter } from './tasks/routes.js';
+import { GAELifecycleRouter, StatusRouter } from "@eave-fyi/eave-stdlib-ts/src/api-util.js";
+import { commonRequestMiddlewares, commonResponseMiddlewares, helmetMiddleware } from "@eave-fyi/eave-stdlib-ts/src/middleware/common-middlewares.js";
+import express from "express";
+import { InternalApiRouter } from "./api/routes.js";
+import { WebhookOfflineTaskRouter, WebhookRouter } from "./events/routes.js";
+import { OfflineTaskRouter } from "./tasks/routes.js";
 
 export const app = express();
 app.use(helmetMiddleware());
