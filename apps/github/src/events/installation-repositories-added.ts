b558ae456210abe7c25ff2ffa789c159ddb120a3
niehusst <liamniehusstaab@gmail.com>
2023-09-18 18:34:27 -0700

    feat: create repo on webhook event

---
+++ apps/github/src/events/installation-repositories-added.ts
@@ -0,0 +1,68 @@
+import { InstallationRepositoriesAddedEvent } from '@octokit/webhooks-types';
+import { createGithubRepo, getGithubRepos, GetGithubReposRequestBody, queryGithubReposFeatureState } from '@eave-fyi/eave-stdlib-ts/src/core-api/operations/github-repos.js'
+import { GitHubOperationsContext } from '../types.js';
+import { EaveApp } from '@eave-fyi/eave-stdlib-ts/src/eave-origins.js';
+import { Feature, State } from '@eave-fyi/eave-stdlib-ts/src/core-api/models/github-repos.js';
+import { enumCases } from '@eave-fyi/eave-stdlib-ts/src/util.js';
+
+/**
+ * Receives github webhook pull_request events.
+ * https://docs.github.com/en/webhooks/webhook-events-and-payloads#installation_repositories
+ *
+ * Features:
+ * Inserts rows into the github_repos db table with feature activation states matching
+ * the other rows in the table whenever it is given access to a new repo in GitHub.
+ */
+export default async function handler(event: InstallationRepositoriesAddedEvent, context: GitHubOperationsContext) {
+  if (event.action !== 'added') {
+    return;
+  }
+  const { ctx } = context;
+
+  const sharedReqInput = {
+    teamId: ctx.eave_team_id,
+    origin: EaveApp.eave_github_app,
+  };
+
+  const res = await getGithubRepos({
+    ...sharedReqInput,
+    input: {}
+  });
+  if (res.repos.length < 1) {
+    // exit early; website app handles adding first repo to the db
+    return;
+  }
+
+  for (const repo of event.repositories_added) {
+    const featureStates: { [key: string]: State } = {}
+
+    for (const feat of enumCases(Feature)) {
+      // if all the team's repos have the ENABLED state for this feature,
+      // the new repo will also get the ENABLED state to match the default
+      featureStates[Feature.API_DOCUMENTATION] = (await queryGithubReposFeatureState({
+        ...sharedReqInput,
+        input: {
+          query_params: {
+            feature: feat,
+            state: State.ENABLED,
+          }
+        }
+      })).states_match ? State.ENABLED : State.DISABLED;
+    }
+
+    await createGithubRepo({
+      teamId: ctx.eave_team_id,
+      origin: EaveApp.eave_confluence_app,
+      input: {
+        repo: {
+          external_repo_id: repo.node_id,
+          api_documentation_state: featureStates[Feature.API_DOCUMENTATION] || State.DISABLED,
+          inline_code_documentation_state: featureStates[Feature.INLINE_CODE_DOCUMENTATION] || State.DISABLED,
+          architecture_documentation_state: featureStates[Feature.ARCHITECTURE_DOCUMENTATION] || State.DISABLED,
+        }
+      },
+    });
+  }
+
+ 
+}
--- libs/eave-stdlib-ts/src/util.ts
+++ libs/eave-stdlib-ts/src/util.ts
@@ -10,3 +10,13 @@ export function redact(str: string | undefined): string | undefined {
 
   return `${str.slice(0, 4)}[redacted ${strlen - 8} chars]${str.slice(-4)}`;
 }
+
+export function enumCases<O extends object>(obj: O): Array<NonNullable<O[keyof O]>> {
+  return Object.keys(obj).reduce((acc, key, _) => {
+    const candidateCase = obj[key as keyof typeof obj];
+    if (candidateCase && Number.isNaN(parseInt(key, 10))) {
+      acc.push(candidateCase);
+    }
+    return acc;
+  }, Array<NonNullable<O[keyof O]>>());
+}
