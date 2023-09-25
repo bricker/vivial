516f0d446809c3dd2b47d882f69af40af105cf9d
Bryan Ricker <git@bryanricker.com>
2023-09-24 18:45:09 -0700

    wip on api doc task

--- apps/github/src/tasks/run-api-documentation.ts
+++ apps/github/src/tasks/run-api-documentation.ts
@@ -1,7 +1,19 @@
+import { RunApiDocumentationTaskRequestBody } from "@eave-fyi/eave-stdlib-ts/src/github-api/operations/run-api-documentation-task.js";
 import { LogContext } from "@eave-fyi/eave-stdlib-ts/src/logging.js";
 import Express from "express";
 
-export async function runApiDocumentationTaskHandler(_req: Express.Request, res: Express.Response): Promise<void> {
+export async function runApiDocumentationTaskHandler(req: Express.Request, res: Express.Response): Promise<void> {
   const ctx = LogContext.load(res);
-  // TODO
+  const input = <RunApiDocumentationTaskRequestBody>req.body;
+
+  /**
+   * TODO:
+   * 1. Get the team given by eave-team-id header
+   * 1. Get the full GithubRepo object from Core API using external_repo_id (body) and eave-team-id (header)
+   * 1. Verify that the feature is enabled
+   * 1. Create container GithubDocument with default state
+   * 1. Run the API documentation process
+   * 1. Open a pull request
+   * 1. Update the GithubDocument object with the PR number
+   */
 }
