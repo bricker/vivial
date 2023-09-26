8ba4e30988c10053ceeb6dcf04a3669f4a76aa53
niehusst <liamniehusstaab@gmail.com>
2023-09-25 17:04:03 -0700

    chore: add ts translation for gh install delete op

--- libs/eave-stdlib-ts/src/core-api/operations/github.ts
+++ libs/eave-stdlib-ts/src/core-api/operations/github.ts
@@ -1,4 +1,4 @@
-import { RequestArgsOrigin, makeRequest } from "../../requests.js";
+import { RequestArgsOrigin, RequestArgsTeamId, makeRequest } from "../../requests.js";
 import { GithubInstallation, GithubInstallationInput } from "../models/github.js";
 import { Team } from "../models/team.js";
 import { CoreApiEndpointConfiguration } from "./shared.js";
@@ -22,5 +22,20 @@ export class GetGithubInstallationOperation {
     });
     const responseData = <GetGithubInstallationResponseBody>await resp.json();
     return responseData;
+  }
+}
+
+export type DeleteGithubInstallationRequestBody = {
+  github_integration: GithubInstallationInput;
+};
+
+export class DeleteGithubInstallationOperation {
+  static config = new CoreApiEndpointConfiguration({ path: "/integrations/github/delete" });
+
+  static async perform(args: RequestArgsTeamId & { input: DeleteGithubInstallationRequestBody }): Promise<void> {
+    await makeRequest({
+      config: this.config,
+      ...args,
+    });
   }
 }
