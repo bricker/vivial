c70dc29a382380ab94aedd7e758cc1d629f46d85
Bryan Ricker <git@bryanricker.com>
2023-09-25 18:07:06 -0700

    eslint config for jsx

--- develop/javascript/es-config/eslint/index.js
+++ develop/javascript/es-config/eslint/index.js
@@ -1,5 +1,4 @@
-/* eslint-disable quote-props */
-
+/** @type {import("eslint").Config} */
 const config = {
   extends: ["eslint:recommended", "prettier"],
   plugins: [],
@@ -12,6 +11,7 @@ const config = {
   parserOptions: {
     ecmaVersion: "latest",
     impliedStrict: true,
+    sourceType: "module",
   },
   rules: {
     // Rules that enforce consistent code style, but may not be fixable by a formatter. These could also indicate a bug.
