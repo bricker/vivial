9fbdda73d12627bdbe346dab7e221e52fb4832a4
Bryan Ricker <git@bryanricker.com>
2023-09-27 12:41:41 -0700

    fix team_id type in github_repos

--- apps/core/eave/core/internal/orm/github_repos.py
+++ apps/core/eave/core/internal/orm/github_repos.py
@@ -51,7 +51,7 @@ class GithubRepoOrm(Base):
 
     @classmethod
     def _build_query(cls, **kwargs: Unpack[QueryParams]) -> Select[Tuple[Self]]:
-        team_id = eave.stdlib.util.ensure_str_or_none(kwargs["team_id"])
+        team_id = eave.stdlib.util.ensure_uuid(kwargs["team_id"])
         lookup = select(cls).where(cls.team_id == team_id)
 
         if external_repo_id := kwargs.get("external_repo_id"):
