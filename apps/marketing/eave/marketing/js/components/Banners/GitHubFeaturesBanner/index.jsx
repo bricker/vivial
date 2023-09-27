78f160748ac1e8aa4e047a598cdb083407fd437a
niehusst <liamniehusstaab@gmail.com>
2023-09-27 12:24:26 -0700

    feat: css magic to make feature cards stretchy

--- apps/marketing/eave/marketing/js/components/Banners/GitHubFeaturesBanner/index.jsx
+++ apps/marketing/eave/marketing/js/components/Banners/GitHubFeaturesBanner/index.jsx
@@ -6,12 +6,9 @@ import Copy from "../../Copy/index.jsx";
 import PageSection from "../../PageSection/index.jsx";
 
 const makeClasses = makeStyles((theme) => ({
-  // section: {
-  //   padding: `54px 40px calc(${theme.footer.height}px + 108px)`,
-  // },
   wrapper: {
     display: "flex",
-    alignItems: "center",
+    alignItems: "flex-start",
     flexDirection: "column",
     justifyContent: "space-between",
     [theme.breakpoints.up("md")]: {
@@ -25,9 +22,42 @@ const makeClasses = makeStyles((theme) => ({
     },
   },
   featureContainer: {
-    flex: 1,
+    flexGrow: 1,
+    flexBasis: 150,
+    marginRight: 80,
   },
-  featureImage: {},
+  featureSubtitle: {
+    marginBottom: 50,
+  },
+  featureImage: {
+    // widthFn = (maxWidth) => (maxWidth - PageSection.padding*2 - featureContainer.margin*2) / numberOfFeatures
+    // min( widthFn(viewportWidth), widthFn(PageSection.maxWidth) )
+    maxWidth: "min((100vw - 108px - 160px) / 3, 282px)",
+    marginBottom: 100,
+    minWidth: 150,
+    [theme.breakpoints.down("sm")]: {
+      minWidth: 300,
+    },
+  },
+  titleCopy: {},
+  titleImage: {
+    maxWidth: 355,
+    [theme.breakpoints.down("sm")]: {
+      order: -1,
+      marginBottom: 50,
+    },
+  },
+  titleContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "spacing-between",
+    marginBottom: 100,
+    [theme.breakpoints.up("md")]: {
+      flexDirection: "row",
+      alignItems: "flex-end",
+    },
+  },
 }));
 
 const GitHubFeaturesBanner = ({ title, features }) => {
@@ -35,13 +65,22 @@ const GitHubFeaturesBanner = ({ title, features }) => {
 
   return (
     <PageSection>
-      <Copy variant="h1">{title}</Copy>
+      <div className={classes.titleContainer}>
+        <Copy variant="h1" className={classes.titleCopy}>
+          {title}
+        </Copy>
+        <img className={classes.titleImage} src={imageUrl("eave-github-logos-3x.png")} alt="Eave and GitHub logos side-by-side" />
+      </div>
 
       <div className={classes.wrapper}>
         {features.map((feature, i) => (
           <div key={i} className={classes.featureContainer}>
-            <Copy variant="h2">{feature.title}</Copy>
-            <Copy variant="p">{feature.subtitle}</Copy>
+            <Copy variant="h2" bold={true}>
+              {feature.title}
+            </Copy>
+            <Copy variant="p" className={classes.featureSubtitle}>
+              {feature.subtitle}
+            </Copy>
             {/* image isn't important for a11y, so use empty alt text to show that */}
             <img className={classes.featureImage} src={imageUrl(feature.image)} alt="" />
           </div>
