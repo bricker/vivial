d035c201bb4a9aaaa40c69f1981c1b022e809ce5
Bryan Ricker <git@bryanricker.com>
2023-09-27 14:54:20 -0700

    formatting: misc

--- develop/javascript/es-config/ava/test-setup.js
+++ develop/javascript/es-config/ava/test-setup.js
@@ -4,7 +4,10 @@ import path from "node:path";
 dotenv.config({ path: path.join(process.env["EAVE_HOME"], ".env") });
 
 try {
-  dotenv.config({ path: path.join(process.env["EAVE_HOME"], ".env.test"), override: true });
+  dotenv.config({
+    path: path.join(process.env["EAVE_HOME"], ".env.test"),
+    override: true,
+  });
 } catch (e) {
   console.warn(".env.test file not found");
 }
