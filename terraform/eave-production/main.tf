0f84447eb94253fe3607d72b258f4f22706060a9
Bryan Ricker <git@bryanricker.com>
2023-10-08 21:24:34 -0700

    terraform updates

--- terraform/eave-production/main.tf
+++ terraform/eave-production/main.tf
@@ -1,11 +1,17 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs
 
+variable "cron_shared_secret" {
+  type = string
+  sensitive = true
+}
+
 locals {
   project_id      = "eave-production"
   region          = "us-central1"
   zone            = "us-central1-c"
   billing_account = "013F5E-137CB0-B6AA2A"
   org_id          = "482990375115"
+  eave_domain_apex = "eave.fyi"
 }
 
 terraform {
@@ -38,6 +44,26 @@ module "gcp_cloud_tasks" {
 
 module "gcp_cloud_scheduler" {
   source     = "../modules/gcp/cloud_scheduler"
+  project_id = local.project_id
+  region     = local.region
+  cron_shared_secret = var.cron_shared_secret
+}
+
+module "gcp_memorystore" {
+  source     = "../modules/gcp/memorystore"
+  project_id = local.project_id
+  region     = local.region
+}
+
+module "gcp_monitoring" {
+  source     = "../modules/gcp/monitoring"
+  project_id = local.project_id
+  region     = local.region
+  eave_domain_apex = local.eave_domain_apex
+}
+
+module "gcp_pubsub" {
+  source     = "../modules/gcp/pubsub"
   project_id = local.project_id
   region     = local.region
 }
--- terraform/modules/gcp/pubsub/topics.tf
+++ terraform/modules/gcp/pubsub/topics.tf
@@ -17,7 +17,6 @@ resource "google_pubsub_topic" "eave_event_pubsub_topic" {
   message_retention_duration = null
   name                       = "eave_event"
   project                    = var.project_id
-  depends_on                 = [google_pubsub_schema.eave_event_pubsub_schema]
   schema_settings {
     encoding = "BINARY"
     schema   = "projects/${var.project_id}/schemas/eave_event"
@@ -35,28 +34,9 @@ resource "google_pubsub_topic" "gpt_request_event_pubsub_topic" {
   message_retention_duration = null
   name                       = "gpt_request_event"
   project                    = var.project_id
-  depends_on                 = [google_pubsub_schema.gpt_request_event_pubsub_schema]
   schema_settings {
     encoding = "BINARY"
     schema   = "projects/${var.project_id}/schemas/gpt_request_event"
-  }
-  timeouts {
-    create = null
-    delete = null
-    update = null
-  }
-}
-
-resource "google_pubsub_topic" "eave_event_topic_pubsub_topic" {
-  kms_key_name               = null
-  labels                     = {}
-  message_retention_duration = "604800s"
-  name                       = "eave_event_topic"
-  project                    = var.project_id
-  depends_on                 = [google_pubsub_schema.eave_event_schema_pubsub_schema]
-  schema_settings {
-    encoding = "BINARY"
-    schema   = "projects/${var.project_id}/schemas/eave_event_schema"
   }
   timeouts {
     create = null
