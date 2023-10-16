ddcc95465f59d3e853f1394f8085b8b3d68a8650
niehusst <liamniehusstaab@gmail.com>
2023-10-16 14:37:19 -0700

    fix: stage deploy failures

--- apps/marketing/eave/marketing/config.py
+++ apps/marketing/eave/marketing/config.py
@@ -12,13 +12,5 @@ class AppConfig(eave.stdlib.config.EaveConfig):
     def asset_base(self) -> str:
         return os.getenv("EAVE_ASSET_BASE", "/static")
 
-    @cached_property
-    def eave_web_session_encryption_key(self) -> str:
-        key = "EAVE_WEB_SESSION_ENCRYPTION_KEY"
-        if self.is_development:
-            return os.getenv(key, "dev-encryption-key")
-        else:
-            return self.get_secret(key)
-
 
 app_config = AppConfig()
