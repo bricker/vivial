de24046839e31e7f7f15a47e1a94dc35ab134968
Bryan Ricker <git@bryanricker.com>
2023-10-19 00:53:11 -0700

    formatting

--- apps/github/bin/start-dev.cjs
+++ apps/github/bin/start-dev.cjs
@@ -2,19 +2,24 @@
 
 const dotenv = require("dotenv");
 const path = require("node:path");
-const nodemon = require('../../../node_modules/nodemon/lib/');
+const nodemon = require("../../../node_modules/nodemon/lib/");
 
-const EAVE_HOME = process.env["EAVE_HOME"]
+const EAVE_HOME = process.env["EAVE_HOME"];
 
-dotenv.config({ path: path.join(EAVE_HOME, "develop/shared/share.env"), override: true });
+dotenv.config({
+  path: path.join(EAVE_HOME, "develop/shared/share.env"),
+  override: true,
+});
 dotenv.config({ path: path.join(EAVE_HOME, ".env"), override: true });
 
 try {
-  dotenv.config({ path: path.join(EAVE_HOME, ".eavefyi-dev.env"), override: true });
+  dotenv.config({
+    path: path.join(EAVE_HOME, ".eavefyi-dev.env"),
+    override: true,
+  });
 } catch (e) {
   console.warn(".eavefyi-dev.env file not found");
 }
-
 
 dotenv.populate(
   process.env,
@@ -25,10 +30,12 @@ dotenv.populate(
   { override: true },
 );
 
-nodemon([
-  "--exec", `${EAVE_HOME}/node_modules/.bin/tsx`,
-  "--watch", `./src`,
-  "--watch", `${EAVE_HOME}/libs/eave-stdlib-ts`,
-  "--delay", "5",
-  "./server.ts"
-].join(" "));
+nodemon(
+  [
+    `--exec ${EAVE_HOME}/node_modules/.bin/tsx`,
+    "--watch ./src",
+    `--watch ${EAVE_HOME}/libs/eave-stdlib-ts`,
+    "--delay 5",
+    "./server.ts",
+  ].join(" "),
+);
