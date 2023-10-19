b6fc2f11f480921b1c8b9586ac6cbef6fb9defb1
Bryan Ricker <git@bryanricker.com>
2023-10-19 00:41:11 -0700

    Move dev server into scripts

---
+++ apps/github/bin/start-dev.cjs
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const dotenv = require("dotenv");
+const path = require("node:path");
+const nodemon = require('../../../node_modules/nodemon/lib/');
+
+const EAVE_HOME = process.env["EAVE_HOME"]
+
+dotenv.config({ path: path.join(EAVE_HOME, "develop/shared/share.env"), override: true });
+dotenv.config({ path: path.join(EAVE_HOME, ".env"), override: true });
+
+try {
+  dotenv.config({ path: path.join(EAVE_HOME, ".eavefyi-dev.env"), override: true });
+} catch (e) {
+  console.warn(".eavefyi-dev.env file not found");
+}
+
+
+dotenv.populate(
+  process.env,
+  {
+    GAE_SERVICE: "github",
+    PORT: "5300",
+  },
+  { override: true },
+);
+
+nodemon([
+  "--exec", `${EAVE_HOME}/node_modules/.bin/tsx`,
+  "--watch", `./src`,
+  "--watch", `${EAVE_HOME}/libs/eave-stdlib-ts`,
+  "--delay", "5",
+  "./server.ts"
+].join(" "));
