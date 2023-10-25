7c275c93bcffc08196149a10b8cdf3a5e5b69976
Bryan Ricker <978899+bricker@users.noreply.github.com>
2023-10-25 00:12:07 -0700

    script updates (#259)
    - Remove unused EAVE_HOMEs
    - Fix watch/ignore files
    - update dotenv loading for tests
    - disable python bytecode
    - add EAVE_HOME constant
    - update bin/clean script
    
    Ticket link:
    <!-- here -->
    
    Did you run?:
    - [ ] unit tests
    - [ ] lint

---
+++ apps/core/bin/src/create-db-migration.py
@@ -0,0 +1,25 @@
+import sys
+import click
+from eave.dev_tooling.dotenv_loader import load_standard_dotenv_files
+import alembic
+import alembic.config
+import alembic.command
+
+sys.path.append(".")
+
+alembic_config = alembic.config.Config("alembic.ini")
+load_standard_dotenv_files()
+
+
+@click.command()
+@click.option("-m", "--message", required=True)
+def create_revision(message: str) -> None:
+    alembic.command.revision(
+        config=alembic_config,
+        message=message,
+        autogenerate=True,
+    )
+
+
+if __name__ == "__main__":
+    create_revision()
---
+++ apps/github/bin/src/start-dev-nodemon.cjs
@@ -0,0 +1,23 @@
+// @ts-check
+const nodemon = require("nodemon/lib");
+const {
+  loadStandardDotenvFiles,
+  populateEnv,
+} = require("../../../../develop/javascript/dotenv-loader.cjs");
+const { EAVE_HOME } = require("../../../../develop/javascript/constants.cjs");
+
+loadStandardDotenvFiles();
+populateEnv({
+  GAE_SERVICE: "github",
+  PORT: "5300",
+});
+
+nodemon(
+  [
+    `--exec ${EAVE_HOME}/node_modules/.bin/tsx`,
+    "--watch ./src",
+    `--watch ${EAVE_HOME}/libs/eave-stdlib-ts`,
+    "--delay 5",
+    "./server.ts",
+  ].join(" "),
+);
