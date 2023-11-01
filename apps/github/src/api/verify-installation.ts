1bcc306c363280426b4d7c95b650480066974a25
niehusst <liamniehusstaab@gmail.com>
2023-11-01 11:40:31 -0700

    help debug

--- apps/github/src/api/verify-installation.ts
+++ apps/github/src/api/verify-installation.ts
@@ -19,71 +19,84 @@ export async function verifyInstallation(
   req: Request,
   res: Response,
 ): Promise<void> {
-  const ctx = LogContext.load(res);
+  // TODO: debuging
+  try {
+    const ctx = LogContext.load(res);
 
-  // validate input
-  const input = <VerifyInstallationRequestBody>req.body;
-  if (!(input.code && input.installation_id)) {
-    eaveLogger.error("Invalid input", ctx);
-    res.sendStatus(400);
-    return;
-  }
+    // validate input
+    const input = <VerifyInstallationRequestBody>req.body;
+    if (!(input.code && input.installation_id)) {
+      eaveLogger.error("Invalid input", ctx);
+      res.sendStatus(400);
+      return;
+    }
+    eaveLogger.debug("validated input");
 
-  // TODO: is there vulnerability in assuming this install id is ok to use for api auth??
-  const octokit = await createOctokitClient(
-    parseInt(input.installation_id, 10),
-  );
-
-  // TODO: test this result, no idea what the typing is on it
-  // exchange code for an access token
-  // https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/generating-a-user-access-token-for-a-github-app#generating-a-user-access-token-when-a-user-installs-your-app
-  const {
-    data: { accessToken },
-  } = await octokit.request("POST /login/oauth/access_token", {
-    client_id: appConfig.eaveGithubAppClientId,
-    client_secret: appConfig.eaveGithubAppClientSecret,
-    code: input.code,
-    headers: {
-      "X-GitHub-Api-Version": "2022-11-28",
-    },
-  });
-
-  // use access token to find list of installations the user has explicit access to
-  // https://docs.github.com/en/rest/apps/installations?apiVersion=2022-11-28#list-app-installations-accessible-to-the-user-access-token
-  const userOctokit = new Octokit({
-    auth: accessToken,
-  });
-
-  const {
-    data: { installations: accessibleInstallations },
-  } = await userOctokit.request("GET /user/installations", {
-    headers: {
-      "X-GitHub-Api-Version": "2022-11-28",
-    },
-  });
-
-  // verify the provided installation is in that list
-  const installation = accessibleInstallations.find(
-    (install) => install.id.toString() === input.installation_id,
-  );
-  if (installation === undefined) {
-    eaveLogger.warning(
-      "Failed to find installation_id in list of accessible GitHub app installations",
-      ctx,
+    // TODO: is there vulnerability in assuming this install id is ok to use for api auth??
+    const octokit = await createOctokitClient(
+      parseInt(input.installation_id, 10),
     );
-    res.sendStatus(401);
-    return;
-  }
+    eaveLogger.debug(`created octokit ${octokit}`);
 
-  // verify the installation is an Eave app installation
-  if (installation.app_id.toString() !== (await appConfig.eaveGithubAppId)) {
-    eaveLogger.warning(
-      "The installation_id was not an Eave GitHub app ID",
-      ctx,
+    // TODO: test this result, no idea what the typing is on it
+    // exchange code for an access token
+    // https://docs.github.com/en/apps/creating-github-apps/authenticating-with-a-github-app/generating-a-user-access-token-for-a-github-app#generating-a-user-access-token-when-a-user-installs-your-app
+    const {
+      data, //: { accessToken },
+    } = await octokit.request("POST /login/oauth/access_token", {
+      client_id: appConfig.eaveGithubAppClientId,
+      client_secret: appConfig.eaveGithubAppClientSecret,
+      code: input.code,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
+    eaveLogger.debug(`got access token respo: ${JSON.stringify(data)}`);
+
+    const { accessToken } = data;
+
+    // use access token to find list of installations the user has explicit access to
+    // https://docs.github.com/en/rest/apps/installations?apiVersion=2022-11-28#list-app-installations-accessible-to-the-user-access-token
+    const userOctokit = new Octokit({
+      auth: accessToken,
+    });
+    eaveLogger.debug(`succes build new user octokit ${userOctokit}`);
+
+    const {
+      data: { installations: accessibleInstallations },
+    } = await userOctokit.request("GET /user/installations", {
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
+    eaveLogger.debug(`got back installations list: ${accessibleInstallations}`);
+
+    // verify the provided installation is in that list
+    const installation = accessibleInstallations.find(
+      (install) => install.id.toString() === input.installation_id,
     );
-    res.send(401);
-    return;
-  }
+    if (installation === undefined) {
+      eaveLogger.warning(
+        "Failed to find installation_id in list of accessible GitHub app installations",
+        ctx,
+      );
+      res.sendStatus(401);
+      return;
+    }
 
-  res.sendStatus(200);
+    // verify the installation is an Eave app installation
+    if (installation.app_id.toString() !== (await appConfig.eaveGithubAppId)) {
+      eaveLogger.warning(
+        "The installation_id was not an Eave GitHub app ID",
+        ctx,
+      );
+      res.send(401);
+      return;
+    }
+
+    res.sendStatus(200);
+  } catch (e: unknown) {
+    eaveLogger.error(<Error>e);
+    throw e;
+  }
 }
