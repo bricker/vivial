d37b05969b0444a8099f44f544f242b20d28b6f3
Bryan Ricker <git@bryanricker.com>
2023-11-07 13:59:12 -0800

    prod configs for jira/confluence/slack apps

--- terraform/eavefyi-dev/main.tf
+++ terraform/eavefyi-dev/main.tf
@@ -1,11 +1,17 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs
 
+variable "cron_shared_secret" {
+  type      = string
+  sensitive = true
+}
+
 locals {
   project_id      = "eavefyi-dev"
   region          = "us-central1"
   zone            = "us-central1-c"
   billing_account = "013F5E-137CB0-B6AA2A"
   org_id          = "482990375115"
+  eave_domain_apex = "eave.dev"
 }
 
 terraform {
@@ -40,4 +46,9 @@ module "gcp_cloud_scheduler" {
   source     = "../modules/gcp/cloud_scheduler"
   project_id = local.project_id
   region     = local.region
+  cron_shared_secret = var.cron_shared_secret
+}
+
+module "gcp_secret_manager" {
+  source     = "../modules/gcp/secret_manager"
 }
