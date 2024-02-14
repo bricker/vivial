27794b4a58b745a09b0f1eceec06b356801cffd4
niehusst <liamniehusstaab@gmail.com>
2024-02-13 16:23:45 -0800

    complete draft sql for data gen

--- libs/eave-monitoring/src/eave/monitoring/postgres/monkey_test/test_agent.py
+++ libs/eave-monitoring/src/eave/monitoring/postgres/monkey_test/test_agent.py
@@ -1,7 +1,9 @@
+from enum import StrEnum
 import asyncio
 import argparse
 import subprocess
 import sys
+from attr import field
 import urllib3 as ul
 import os
 import random
@@ -16,20 +18,32 @@ NUM_ROW_OPS = 100
 NUM_TABLES = 5
 NUM_COLS_MIN = 2
 NUM_COLS_MAX = 10
-OP_LIST = ["INSERT", "UPDATE", "DELETE"]
+
+
+class Operation(StrEnum):
+    INSERT = "INSERT"
+    UPDATE = "UPDATE"
+    DELETE = "DELETE"
+
+
+OP_LIST = [Operation.INSERT, Operation.UPDATE, Operation.DELETE]
+
 
 class Column:
     def __init__(self, name: str, type: str):
         self.name = name
         self.type = type
 
-    def formated(self) -> str:
-        return f"{self.name} {self.type}"
-    
+    def formated(self) -> sql.Composed:
+        return sql.SQL("{name} {type}").format(
+            name=sql.Identifier(self.name),
+            type=sql.Identifier(self.type),
+        )
+
     @staticmethod
     def randName() -> str:
-        return WORD_LIST[random.randint(0, len(WORD_LIST)-1)]
-    
+        return WORD_LIST[random.randint(0, len(WORD_LIST) - 1)]
+
     @staticmethod
     def randType() -> str:
         # TODO: vary the types? then we'll need to update rand insert generation logic. probably make a type enum to switch generation function on
@@ -39,16 +53,22 @@ class Column:
 class Table:
     def __init__(self, name: str, cols: list[Column]):
         self.name = name
-        self.columns = cols
+        # ensure there's a row_id column for later use in tests
+        self._columns = [Column(name="row_id", type="int")] + cols
 
-    def formatCols(self) -> str:
+    @property
+    def columns(self) -> list[Column]:
+        # exclude the row_id col; we dont need to mess with it most of the time when using other columns
+        return self._columns[1:]
+
+    def formatCols(self) -> sql.Composed:
         """return cols list as a string of comma separated column names + types (for use in SQL queries)"""
-        return ", ".join(map(lambda c: c.formated(), self.columns))
+        return sql.SQL(", ").join(map(lambda c: c.formated(), self.columns))
 
     @staticmethod
     def randName() -> str:
-        return WORD_LIST[random.randint(0, len(WORD_LIST)-1)]
-    
+        return WORD_LIST[random.randint(0, len(WORD_LIST) - 1)]
+
 
 async def build_tables(session: psycopg.AsyncConnection) -> list[Table]:
     """returns list of created table structs"""
@@ -56,24 +76,27 @@ async def build_tables(session: psycopg.AsyncConnection) -> list[Table]:
 
     n_cols = random.randint(NUM_COLS_MIN, NUM_COLS_MAX)
 
+    # gen table shape
     for _ in range(NUM_TABLES):
         tables.append(
             Table(
                 name=Table.randName(),
-                cols=[Column(
-                    name=Column.randName(),
-                    type=Column.randType(),
-                ) for _ in range(n_cols)]
+                cols=[
+                    Column(
+                        name=Column.randName(),
+                        type=Column.randType(),
+                    )
+                    for _ in range(n_cols)
+                ],
             )
         )
 
+    # create tables in db
     async with session.cursor() as curs:
         for table in tables:
             await curs.execute(
-                sql.SQL(
-                    "CREATE TABLE {table} ({cols})"
-                ).format(
-                    table=table.name,
+                sql.SQL("CREATE TABLE {table} ({cols})").format(
+                    table=sql.Identifier(table.name),
                     cols=table.formatCols(),
                 )
             )
@@ -91,25 +114,65 @@ async def drop_tables(session: psycopg.AsyncConnection, tables: list[Table]) -> 
             )
 
 
+def gen_value(type: str) -> str:
+    # TODO: improve?
+    rand_len = random.randint(1, 4)
+    return "-".join([WORD_LIST[random.randint(0, len(WORD_LIST)-1)] for _ in range(rand_len)])
+
+
 async def populate_tables(session: psycopg.AsyncConnection, tables: list[Table]) -> None:
     for op in OP_LIST:
-        for i in range(NUM_ROW_OPS):
-            # async w/ inside so that each op is executed separately rather than in 1 commit
-            # since real usage wont be in big single commits; they'll be mostly 1-off operations
-            async with session.cursor() as curs:
-                # TODO: but the ops all have diff command structure??
-                await curs.execute(
-                    sql.SQL("{op} ").format(
-                        op=op,
-                    )
-                )
+        for table in tables:
+            for i in range(NUM_ROW_OPS):
+                # curs start inside so that each op is executed separately rather than in 1 commit
+                # since real usage wont be in big single commits; they'll be mostly 1-off operations
+                async with session.cursor() as curs:
+                    match op:
+                        case Operation.INSERT:
+                            await curs.execute(
+                                # separately include row_id value non-randomly so we can isolate later UPDATE and DELETE ops
+                                sql.SQL("INSERT INTO {name} VALUES ({id_val}, {other_vals})").format(
+                                    name=sql.Identifier(table.name),
+                                    id_val=sql.Identifier(str(i)),
+                                    other_vals=sql.SQL(", ").join(
+                                        [sql.Identifier(gen_value(col.type)) for col in table.columns]
+                                    ),
+                                )
+                            )
+                        case Operation.UPDATE:
+                            await curs.execute(
+                                sql.SQL("UPDATE {name} SET {values} WHERE row_id = {id}").format(
+                                    name=sql.Identifier(table.name),
+                                    values=sql.SQL(", ").join(
+                                        [
+                                            sql.SQL("{col_name} = {new_val}").format(
+                                                col_name=sql.Identifier(col.name),
+                                                new_val=sql.Identifier(gen_value(col.type)),
+                                            )
+                                            for col in table.columns
+                                        ]
+                                    ),
+                                    id=sql.Identifier(str(i)),
+                                )
+                            )
+                        case Operation.DELETE:
+                            await curs.execute(
+                                sql.SQL("DELETE FROM {name} WHERE row_id = {id}").format(
+                                    name=sql.Identifier(table.name),
+                                    id=sql.Identifier(str(i)),
+                                )
+                            )
+                        case _:
+                            print(f"ERROR! Unhandled op case: {op}")
+                            sys.exit(1)
 
 
-def print_stats() -> None:
+def print_stats(tables: list[Table]) -> None:
     """prints expected data creation stats from agent (good enough to verify all worked)"""
     # TODO: aaa
     for op in OP_LIST:
-        print(f"Peformed {NUM_ROW_OPS} {op} operations")
+        for table in tables:
+            print(f"Peformed {NUM_ROW_OPS} {op} operations on {table.name}")
 
 
 def launch_agent(
@@ -173,7 +236,7 @@ async def main(
 
         proc.kill()
 
-        print_stats()
+        print_stats(tables)
 
         answer = input(print(f"Proceed to drop the created junk tables in the {db} database? (Y/n) "))
         if answer != "Y":
