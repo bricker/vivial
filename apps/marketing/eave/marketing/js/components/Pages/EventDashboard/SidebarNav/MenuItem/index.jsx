bae47ba75a845dc80de6cfc5ad7e05dad92007a5
Liam Niehus-Staab <34754390+niehusst@users.noreply.github.com>
2024-02-26 15:27:24 -0700

    feat(EAV-8): Glossary Dashboard UI (#332)
    Ticket link:
    https://eave-fyi.atlassian.net/browse/EAV-8
    
    Adds UI for the dashboard w/o networking functionality (currently
    populated with dummy data.
    The UI element has been hacked on as the root route for the marketing
    website React app; we'll want to change that eventually.
    
    
    ![eave-dash-screenie](https://github.com/eave-fyi/eave-monorepo/assets/34754390/6a0168b0-f685-4af4-afa0-0158447c2b1f)
    
    
    Did you run?:
    - [ ] unit tests
    - [x] lint

---
+++ apps/marketing/eave/marketing/js/components/Pages/EventDashboard/SidebarNav/MenuItem/index.jsx
@@ -0,0 +1,54 @@
+// @ts-check
+import { makeStyles } from "@material-ui/styles";
+import classNames from "classnames";
+import React from "react";
+import * as Types from "../../../../../types.js"; // eslint-disable-line no-unused-vars
+
+const makeClasses = makeStyles((/** @type {Types.Theme} */ theme) => ({
+  selectedBackground: {
+    backgroundColor: "#0d81d9",
+    color: "white",
+  },
+  unselectedBackground: {
+    backgroundColor: "transparent",
+  },
+  listItem: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+    fontSize: 24,
+    padding: "24px 30px",
+    border: 'none',
+    "&:hover": {
+      backgroundColor: "#0d81d988",
+      cursor: "pointer",
+    },
+  },
+}));
+
+const MenuItem = ({
+  /** @type {React.ReactNode} */ children,
+  /** @type {string} */ label,
+  /** @type {function} */ onClick,
+  /** @type {boolean} */ selected = false,
+  /** @type {boolean} */ expanded = false,
+}) => {
+  const classes = makeClasses();
+
+  const background = selected
+    ? classes.selectedBackground
+    : classes.unselectedBackground;
+  return (
+    <button
+      className={classNames(classes.listItem, background)}
+      aria-label={label}
+      onClick={onClick}
+    >
+      {children}
+      {expanded && <p>{label}</p>}
+    </button>
+  );
+};
+
+export default MenuItem;
