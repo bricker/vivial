7146e3e2e881534148649a289679308c369456e5
niehusst <liamniehusstaab@gmail.com>
2024-03-14 11:49:11 -0700

    new mb temp f

---
+++ apps/core/eave/core/public/requests/oauth/metabase_temp.py
@@ -0,0 +1,81 @@
+from typing import cast
+import time
+import hmac
+import json
+from eave.stdlib import analytics, utm_cookies
+from eave.stdlib.config import SHARED_CONFIG
+from eave.stdlib.core_api.models.account import AuthProvider
+
+import google.oauth2.credentials
+import google.oauth2.id_token
+from starlette.requests import Request
+from starlette.responses import RedirectResponse, Response
+
+import eave.core.internal.oauth.google
+from eave.core.internal.oauth import state_cookies as oauth_cookies
+from eave.stdlib.exceptions import MissingOAuthCredentialsError
+from eave.stdlib.config import SHARED_CONFIG
+from eave.stdlib.http_endpoint import HTTPEndpoint
+from eave.stdlib.logging import LogContext
+from eave.stdlib.jwt import TEMP_create_jwt_for_metabase, JWTPurpose
+from . import base, shared
+
+class TEMP_MetabaseOAuthCallback(base.BaseOAuthCallback):
+    async def get(self, request: Request) -> Response:
+        await super().get(request=request)
+        ctx = LogContext.wrap(scope=request.scope)
+
+        if not self._check_valid_callback():
+            return self.response
+
+        await analytics.log_event(
+            event_name="oauth_flow_completed",
+            event_description="A user completed the Oauth flow (login/register)",
+            event_source="google oauth authorize endpoint",
+            opaque_params={
+                "auth_provider": _AUTH_PROVIDER,
+            },
+            ctx=ctx,
+        )
+
+        flow = eave.core.internal.oauth.google.build_flow(state=self.state)
+        flow.fetch_token(code=self.code)
+
+        # flow.credentials returns a `google.auth.credentials.Credentials`, which is the base class of
+        # google.oauth2.credentials.Credentials and doesn't contain common oauth properties like refresh_token.
+        # The `cast` here gives us type hints, autocomplete, etc. for `flow.credentials`
+        credentials = cast(google.oauth2.credentials.Credentials, flow.credentials)
+        if credentials.id_token is None:
+            raise MissingOAuthCredentialsError("google oauth2 credentials")
+
+        google_token = eave.core.internal.oauth.google.decode_id_token(id_token=credentials.id_token)
+        eave_team_name = f"{google_token.given_name}'s Team" if google_token.given_name else shared.DEFAULT_TEAM_NAME
+
+        account = await shared.get_or_create_eave_account(
+            request=self.request,
+            response=self.response,
+            eave_team_name=eave_team_name,
+            user_email=google_token.email,
+            auth_provider=self.auth_provider,
+            auth_id=google_token.sub,
+            access_token=credentials.token,
+            refresh_token=credentials.refresh_token,
+        )
+        
+        # do metabase stuff
+
+        jwt = TEMP_create_jwt_for_metabase(
+            email=account.email or "",
+            first_name=google_token.given_name or "",
+            last_name=google_token.family_name or "",
+            exp_minutes=10,
+            signing_key="8c1c4cb0017162745f294d4d67a8e249205f1057284809f13c8064f87c796fbf", # TODO make secrt
+            purpose=JWTPurpose.access,
+        )
+
+        return_to = f"{SHARED_CONFIG.eave_public_www_base}/dashboard"
+        shared.set_redirect(
+            response=self.response,
+            location=f"http://metabase.eave.run:3000/auth/sso?jwt={jwt.signature}&return_to={return_to}"
+        )
+        return self.response
