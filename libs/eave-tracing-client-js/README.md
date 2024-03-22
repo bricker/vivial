35900e1477983a64c0b4f92128423cb1383a7a7f
niehusst <liamniehusstaab@gmail.com>
2024-03-22 12:14:13 -0700

    garbo

---
+++ libs/eave-tracing-client-js/README.md
@@ -0,0 +1,3 @@
+# Eave Client Tracing
+
+A JS file to include in any web page to collect user actions on the client.
---
+++ libs/eave-tracing-client-js/eave-client.js
@@ -0,0 +1,11 @@
+/*
+
+singleton init window.Matomo 
+
+addEventListener
+  push it onto dict of listeners per event?
+  should we manually handle the listeners like matomo, or rely on the browser to do it? (not sure if we can manually trigger an event if rely on browser)
+    create own def of addEventListner to allow for crossbrowser compat
+
+batch atom delivery on a 5s timer?
+*/
