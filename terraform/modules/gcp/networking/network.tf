3c7961f2e9c8bf38514a6c86383c721b13e99cb1
Bryan Ricker <git@bryanricker.com>
2024-03-28 17:26:04 -0700

    Thu Mar 28 05:26:04 PM PDT 2024

--- terraform/modules/gcp/networking/network.tf
+++ terraform/modules/gcp/networking/network.tf
@@ -14,23 +14,3 @@ resource "google_compute_network" "default" {
   project                                   = var.project_id
   routing_mode                              = "REGIONAL"
 }
-
-# resource "google_compute_subnetwork" "default" {
-#   description                = null
-#   external_ipv6_prefix       = null
-#   ip_cidr_range              = "10.128.0.0/20"
-#   ipv6_access_type           = null
-#   name                       = "default"
-#   network                    = "https://www.googleapis.com/compute/v1/projects/eave-staging/global/networks/default"
-#   private_ip_google_access   = true
-#   private_ipv6_google_access = "DISABLE_GOOGLE_ACCESS"
-#   project                    = "eave-staging"
-#   purpose                    = "PRIVATE"
-#   region                     = "us-central1"
-#   role                       = null
-#   secondary_ip_range = [{
-#     ip_cidr_range = "10.79.128.0/17"
-#     range_name    = "gke-eave-gke-cluster-pods-46b5475c"
-#   }]
-#   stack_type = "IPV4_ONLY"
-# }
