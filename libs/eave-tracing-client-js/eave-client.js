a5b3be192bc6d798fba49d99a315fb1f3a30f2fc
niehusst <liamniehusstaab@gmail.com>
2024-04-01 14:48:36 -0700

    minor update to license comment

--- libs/eave-tracing-client-js/eave-client.js
+++ libs/eave-tracing-client-js/eave-client.js
@@ -5,8 +5,7 @@
  *TODO update links
  * @link https://piwik.org
  * @source https://github.com/matomo-org/matomo/blob/master/js/piwik.js
- * @license https://piwik.org/free-software/bsd/ BSD-3 Clause (also in js/LICENSE.txt)
- * @license magnet:?xt=urn:btih:c80d50af7d3db9be66a4d0a86db0286e4fd33292&dn=bsd-3-clause.txt BSD-3-Clause
+ * @license https://www.gnu.org/licenses/gpl-3.0.en.html#license-text GPLv3 (also in LICENSE.txt)
  */
 
 /*
