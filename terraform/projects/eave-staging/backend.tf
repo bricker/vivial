c30b999be98298f1a4a04fc8a6a747d10c6029e4
Bryan Ricker <git@bryanricker.com>
2024-04-01 16:04:16 -0700

    Mon Apr  1 04:04:16 PM PDT 2024

--- terraform/projects/eave-staging/backend.tf
+++ terraform/projects/eave-staging/backend.tf
@@ -1,6 +1,6 @@
-terraform {
- backend "gcs" {
-   bucket  = google_storage_bucket.tfstate.name
-   prefix  = "terraform/state"
- }
-}
+# terraform {
+#  backend "gcs" {
+#    bucket  = "tfstate"
+#    prefix  = "terraform/state"
+#  }
+# }
--- terraform/projects/eave-staging/main.tf
+++ terraform/projects/eave-staging/main.tf
@@ -67,85 +67,85 @@ resource "google_storage_bucket" "tfstate" {
   ]
 }
 
-# module "gcp_project" {
-#   source          = "../../modules/gcp/project"
-#   project_id = local.project_id
-#   org_id          = local.org_id
-# }
+module "gcp_project" {
+  source          = "../../modules/gcp/project"
+  project_id = local.project_id
+  org_id          = local.org_id
+}
 
-# module "gcp_nat" {
-#   source = "../../modules/gcp/nat"
+module "gcp_nat" {
+  source = "../../modules/gcp/nat"
+  project_id = local.project_id
+  region = local.region
+  network_id = local.default_network_id
+}
+
+module "gcp_gke" {
+  source     = "../../modules/gcp/gke"
+  project_id = local.project_id
+  region     = local.region
+  network_id = local.default_network_id
+  subnetwork_id = local.default_subnetwork_id
+  authorized_networks = local.authorized_networks
+
+  google_service_accounts = {
+    "gsa-metabase-01": {
+      bound_ksa = "metabase/ksa-metabase-01"
+    },
+    "gsa-eave-core": {
+      bound_ksa = "eave/ksa-eave-core"
+    }
+  }
+
+  roles = {
+    "roles/cloudsql.client": {
+      google_service_accounts = [
+        "gsa-metabase-01",
+        "gsa-eave-core",
+      ]
+    },
+    "roles/logging.logWriter": {
+      google_service_accounts = [
+        "gsa-metabase-01",
+        "gsa-eave-core",
+      ]
+    },
+  }
+}
+
+# module "cloudsql_metabase" {
+#   source = "../../modules/gcp/cloud_sql"
 #   project_id = local.project_id
 #   region = local.region
-#   network_id = local.default_network_id
+#   instance_name = "metabase"
+#   instance_tier = ""
+#   instance_disk_type = ""
+#   instance_disk_size = ""
+#   instance_backup_enabled = ""
+#   instance_zone = ""
+#   project_id = ""
 # }
 
-# module "gcp_gke" {
-#   source     = "../../modules/gcp/gke"
+# module "cloudsql_eave_core" {
+#   source = "../../modules/gcp/cloud_sql"
+#   project_id = local.project_id
+# }
+
+# module "metabase_resources" {
+#   source = "../../modules/gcp/metabase_resources"
+#   project_id = local.project_id
+#   metabase_instances = [
+#     "metabase-01" # TODO: This needs to be more dynamic; currently we'll have to update this list for each new customer.
+#   ]
+# }
+
+# module "gcp_iam" {
+#   source     = "../../modules/gcp/iam"
+#   project_id = local.project_id
+# }
+
+# module "gcp_bigquery" {
+#   source     = "../../modules/gcp/bigquery"
 #   project_id = local.project_id
 #   region     = local.region
-#   network_id = local.default_network_id
-#   subnetwork_id = local.default_subnetwork_id
-#   authorized_networks = local.authorized_networks
-
-#   google_service_accounts = {
-#     "gsa-metabase-01": {
-#       bound_ksa = "metabase/ksa-metabase-01"
-#     },
-#     "gsa-eave-core": {
-#       bound_ksa = "eave/ksa-eave-core"
-#     }
-#   }
-
-#   roles = {
-#     "roles/cloudsql.client": {
-#       google_service_accounts = [
-#         "gsa-metabase-01",
-#         "gsa-eave-core",
-#       ]
-#     },
-#     "roles/logging.logWriter": {
-#       google_service_accounts = [
-#         "gsa-metabase-01",
-#         "gsa-eave-core",
-#       ]
-#     },
-#   }
 # }
-
-# # module "cloudsql_metabase" {
-# #   source = "../../modules/gcp/cloud_sql"
-# #   project_id = local.project_id
-# #   region = local.region
-# #   instance_name = "metabase"
-# #   instance_tier = ""
-# #   instance_disk_type = ""
-# #   instance_disk_size = ""
-# #   instance_backup_enabled = ""
-# #   instance_zone = ""
-# #   project_id = ""
-# # }
-
-# # module "cloudsql_eave_core" {
-# #   source = "../../modules/gcp/cloud_sql"
-# #   project_id = local.project_id
-# # }
-
-# # module "metabase_resources" {
-# #   source = "../../modules/gcp/metabase_resources"
-# #   project_id = local.project_id
-# #   metabase_instances = [
-# #     "metabase-01" # TODO: This needs to be more dynamic; currently we'll have to update this list for each new customer.
-# #   ]
-# # }
-
-# # module "gcp_iam" {
-# #   source     = "../../modules/gcp/iam"
-# #   project_id = local.project_id
-# # }
-
-# # module "gcp_bigquery" {
-# #   source     = "../../modules/gcp/bigquery"
-# #   project_id = local.project_id
-# #   region     = local.region
-# # }
