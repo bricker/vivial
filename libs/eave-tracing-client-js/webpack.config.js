f98cec7690f6af83582dd0b04782e90b9811070d
niehusst <liamniehusstaab@gmail.com>
2024-04-03 11:30:00 -0700

    configure linting and try to improve build process to output es5

--- libs/eave-tracing-client-js/webpack.config.js
+++ libs/eave-tracing-client-js/webpack.config.js
@@ -9,4 +9,18 @@ module.exports = {
     filename: 'eave-client.min.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
