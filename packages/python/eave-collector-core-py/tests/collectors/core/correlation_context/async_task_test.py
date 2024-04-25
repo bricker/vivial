9db5543f5ab64e60a790fbe58881742ae43f7ef6
niehusst <liamniehusstaab@gmail.com>
2024-04-25 16:54:38 -0700

    test: corr ctx classes

---
+++ packages/python/eave-collector-core-py/tests/collectors/core/correlation_context/async_task_test.py
@@ -0,0 +1,60 @@
+import unittest
+
+"""
+read from cookeis sets all vlues
+write cookies only sets context values
+"""
+
+
+class AsyncioCorrelationContextTest(unittest.IsolatedAsyncioTestCase):
+    async def test_values_can_be_written_and_read(self) -> None:
+        from eave.collectors.core.correlation_context import AsyncioCorrelationContext
+
+        ctx = AsyncioCorrelationContext()
+        assert ctx.get("key") is None, "Initial value was not None"
+        ctx.set("key", 1)
+        assert ctx.get("key") == 1, "Set value was not read"
+        ctx.set("key", 2)
+        assert ctx.get("key") == 2, "Set value was not overwritten"
+        assert ctx.to_json() == '{"context": {"key": 2}}', "Value was not set in dedicated context dict"
+
+    async def test_empty_state(self) -> None:
+        from eave.collectors.core.correlation_context import AsyncioCorrelationContext
+
+        ctx = AsyncioCorrelationContext()
+        assert ctx.get("key") is None, "Non-existent key value was not None"
+        assert ctx.to_json() == '{"context": {}}', "Empty/default context didnt convert to json as expected"
+
+    async def test_separate_async_task_ctx_cannot_access_each_others_data(self) -> None:
+        async def _helper(keys) -> None:
+            from eave.collectors.core.correlation_context import AsyncioCorrelationContext
+
+            ctx = AsyncioCorrelationContext()
+            for key in keys:
+                ctx.set(key, 1)
+
+            expected = '{"context": {' + ", ".join([f'"{k}": 1' for k in keys]) + "}}"
+            assert ctx.to_json() == expected, "Context did not match expected content"
+
+        await _helper(["k1", "k2", "k3"])
+        await _helper(["k4", "k5", "k6"])
+
+    async def test_child_tasks_inherit_parent_ctx_values(self) -> None:
+        from eave.collectors.core.correlation_context import AsyncioCorrelationContext
+
+        ctx = AsyncioCorrelationContext()
+        # given values exist in parent context
+        ctx.set("parent", 0)
+
+        async def t1() -> None:
+            assert ctx.get("parent") == 0, "Parent value not present in child task t1"
+            ctx.set("t1", 1)
+
+        async def t2() -> None:
+            assert ctx.get("parent") == 0, "Parent value not present in child task t2"
+            ctx.set("t2", 2)
+
+        await t1()  # run async?
+        await t2()
+
+        assert ctx.to_json() == '{"context": {"parent": 0, "t1": 1, "t2": 2}}', "Values set by child tasks not found"
---
+++ packages/python/eave-collector-core-py/tests/collectors/core/correlation_context/thread_test.py
@@ -0,0 +1,71 @@
+import threading
+import unittest
+
+"""
+read from cookeis sets all vlues
+write cookies only sets context values
+"""
+
+
+class ThreadedCorrelationContextTest(unittest.IsolatedAsyncioTestCase):
+    async def test_values_can_be_written_and_read(self) -> None:
+        from eave.collectors.core.correlation_context import ThreadedCorrelationContext
+
+        ctx = ThreadedCorrelationContext()
+        assert ctx.get("key") is None, "Initial value was not None"
+        ctx.set("key", 1)
+        assert ctx.get("key") == 1, "Set value was not read"
+        ctx.set("key", 2)
+        assert ctx.get("key") == 2, "Set value was not overwritten"
+        assert ctx.to_json() == '{"context": {"key": 2}}', "Value was not set in dedicated context dict"
+
+    async def test_empty_state(self) -> None:
+        from eave.collectors.core.correlation_context import ThreadedCorrelationContext
+
+        ctx = ThreadedCorrelationContext()
+        assert ctx.get("key") is None, "Non-existent key value was not None"
+        assert ctx.to_json() == '{"context": {}}', "Empty/default context didnt convert to json as expected"
+
+    async def test_separate_async_thread_ctx_cannot_access_each_others_data(self) -> None:
+        def _helper(keys) -> None:
+            from eave.collectors.core.correlation_context import ThreadedCorrelationContext
+
+            ctx = ThreadedCorrelationContext()
+            for key in keys:
+                ctx.set(key, 1)
+
+            expected = '{"context": {' + ", ".join([f'"{k}": 1' for k in keys]) + "}}"
+            assert ctx.to_json() == expected, "Context did not match expected content"
+
+        t1 = threading.Thread(target=_helper, args=(["k1", "k2", "k3"],))
+        t2 = threading.Thread(target=_helper, args=(["k4", "k5", "k6"],))
+        t1.start()
+        t2.start()
+
+        t1.join()
+        t2.join()
+
+    async def test_child_threads_inherit_parent_ctx_values(self) -> None:
+        from eave.collectors.core.correlation_context import ThreadedCorrelationContext
+
+        ctx = ThreadedCorrelationContext()
+        # given values exist in parent context
+        ctx.set("parent", 0)
+
+        def f1() -> None:
+            assert ctx.get("parent") == 0, "Parent value not present in child thread t1"
+            ctx.set("t1", 1)
+
+        def f2() -> None:
+            assert ctx.get("parent") == 0, "Parent value not present in child thread t2"
+            ctx.set("t2", 2)
+
+        t1 = threading.Thread(target=f1)
+        t2 = threading.Thread(target=f2)
+        t1.start()
+        t2.start()
+
+        t1.join()
+        t2.join()
+
+        assert ctx.to_json() == '{"context": {"parent": 0, "t1": 1, "t2": 2}}', "Values set by child threads not found"
