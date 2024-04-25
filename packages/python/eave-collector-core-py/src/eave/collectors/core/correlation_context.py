4653f0ca0f951114a72f513b62b5bb99d452b377
niehusst <liamniehusstaab@gmail.com>
2024-04-25 14:43:52 -0700

    feat: add asyncio corr ctx impl

--- packages/python/eave-collector-core-py/src/eave/collectors/core/correlation_context.py
+++ packages/python/eave-collector-core-py/src/eave/collectors/core/correlation_context.py
@@ -1,31 +1,67 @@
+import abc
+import contextvars
 import json
 import threading
 import typing
 
-# TODO: doesnt work w/ async dispatch.. but that's tough anywayu bcus we want to persiste across user-space async dispatch
+
+class BaseCorrelationContext(abc.ABC):
+    @abc.abstractmethod
+    def get(self, key: str) -> typing.Any: ...
+
+    @abc.abstractmethod
+    def set(self, key: str, value: typing.Any) -> None: ...
+
+    @abc.abstractmethod
+    def to_json(self) -> str: ...
+
+
 # TODO: customer child threads wont share this storage
-_storage = threading.local()
+_local_thread_storage = threading.local()
 
 
-class CorrelationContext:
-    def __init__(self):
-        self._init_storage()
-
+class ThreadedCorrelationContext(BaseCorrelationContext):
     def _init_storage(self) -> None:
-        if not getattr(_storage, "eave", None):
-            _storage.eave = {"context": {}}
+        if not getattr(_local_thread_storage, "eave", None):
+            _local_thread_storage.eave = {"context": {}}
 
     def get(self, key: str) -> typing.Any:
         self._init_storage()
-        return _storage.eave.get(key, None) or _storage.eave.get("context", {}).get(key, None)
+        return _local_thread_storage.eave.get(key, None) or _local_thread_storage.eave.get("context", {}).get(key, None)
 
     def set(self, key: str, value: typing.Any) -> None:
         self._init_storage()
-        _storage.eave["context"][key] = value
+        _local_thread_storage.eave["context"][key] = value
 
     def to_json(self) -> str:
         self._init_storage()
-        return json.dumps(_storage.eave)
+        return json.dumps(_local_thread_storage.eave)
 
 
-corr_ctx = CorrelationContext()
+_local_async_storage = contextvars.ContextVar("eave_corr_ctx")
+
+
+class AsyncioCorrelationContext(BaseCorrelationContext):
+    def _get_storage(self) -> dict[str, typing.Any]:
+        eave_ctx = contextvars.copy_context().get(_local_async_storage, None)
+        if not eave_ctx:
+            _local_async_storage.set({"context": {}})
+            # refetch ctx vars now that we've set a value
+            eave_ctx = contextvars.copy_context().get(_local_async_storage)
+        return typing.cast(dict[str, typing.Any], eave_ctx)
+
+    def get(self, key: str) -> typing.Any:
+        storage = self._get_storage()
+        return storage.get(key, None) or storage.get("context", {}).get(key, None)
+
+    def set(self, key: str, value: typing.Any) -> None:
+        storage = self._get_storage()
+        storage["context"][key] = value
+
+    def to_json(self) -> str:
+        storage = self._get_storage()
+        return json.dumps(storage)
+
+
+# TODO: figure out which ctx storage type we need at runtime?
+corr_ctx = ThreadedCorrelationContext()
