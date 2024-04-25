2cfccfb9acd061d923273199e3dd675f2a376993
niehusst <liamniehusstaab@gmail.com>
2024-04-25 14:03:19 -0700

    fix: ensure thread local storage is always init on get/set

--- packages/python/eave-collector-core-py/src/eave/collectors/core/correlation_context.py
+++ packages/python/eave-collector-core-py/src/eave/collectors/core/correlation_context.py
@@ -1,18 +1,31 @@
+import json
 import threading
 import typing
+
+# TODO: doesnt work w/ async dispatch.. but that's tough anywayu bcus we want to persiste across user-space async dispatch
+# TODO: customer child threads wont share this storage
+_storage = threading.local()
 
 
 class CorrelationContext:
     def __init__(self):
-        thread_local = threading.local()
-        thread_local.eave = {"context": {}}
-        self._storage = thread_local.eave
+        self._init_storage()
+
+    def _init_storage(self) -> None:
+        if not getattr(_storage, "eave", None):
+            _storage.eave = {"context": {}}
 
     def get(self, key: str) -> typing.Any:
-        return self._storage.get(key, None) or self._storage.get("context", {}).get(key, None)
+        self._init_storage()
+        return _storage.eave.get(key, None) or _storage.eave.get("context", {}).get(key, None)
 
     def set(self, key: str, value: typing.Any) -> None:
-        self._storage["context"][key] = value
+        self._init_storage()
+        _storage.eave["context"][key] = value
+
+    def to_json(self) -> str:
+        self._init_storage()
+        return json.dumps(_storage.eave)
 
 
 corr_ctx = CorrelationContext()
