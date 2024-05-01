1537a26069f02826ee7dcff77af3854fb3c08da8
niehusst <liamniehusstaab@gmail.com>
2024-05-01 14:49:19 -0700

    test: add manual ctx reset

--- packages/python/eave-collector-core-py/tests/collectors/core/correlation_context/thread_test.py
+++ packages/python/eave-collector-core-py/tests/collectors/core/correlation_context/thread_test.py
@@ -1,6 +1,9 @@
 import threading
 import unittest
 
+from eave.collectors.core.correlation_context import ThreadedCorrelationContext
+from eave.collectors.core.correlation_context.thread import _local_thread_storage
+
 """
 read from cookeis sets all vlues
 write cookies only sets context values
@@ -8,9 +11,13 @@ write cookies only sets context values
 
 
 class ThreadedCorrelationContextTest(unittest.IsolatedAsyncioTestCase):
-    async def test_values_can_be_written_and_read(self) -> None:
-        from eave.collectors.core.correlation_context import ThreadedCorrelationContext
+    async def asyncTearDown(self) -> None:
+        super().tearDown()
+        # manually reset private thread storage since asyncio tests are all launched from same thread
+        _local_thread_storage.eave = {}
 
+
+    async def test_values_can_be_written_and_read(self) -> None:
         ctx = ThreadedCorrelationContext()
         assert ctx.get("key") is None, "Initial value was not None"
         ctx.set("key", 1)
@@ -20,16 +27,12 @@ class ThreadedCorrelationContextTest(unittest.IsolatedAsyncioTestCase):
         assert ctx.to_json() == '{"_eave_context": {"key": 2}}', "Value was not set in dedicated context dict"
 
     async def test_empty_state(self) -> None:
-        from eave.collectors.core.correlation_context import ThreadedCorrelationContext
-
         ctx = ThreadedCorrelationContext()
         assert ctx.get("key") is None, "Non-existent key value was not None"
         assert ctx.to_json() == '{"_eave_context": {}}', "Empty/default context didnt convert to json as expected"
 
     async def test_separate_async_thread_ctx_cannot_access_each_others_data(self) -> None:
         def _helper(keys) -> None:
-            from eave.collectors.core.correlation_context import ThreadedCorrelationContext
-
             ctx = ThreadedCorrelationContext()
             for key in keys:
                 ctx.set(key, 1)
@@ -46,8 +49,6 @@ class ThreadedCorrelationContextTest(unittest.IsolatedAsyncioTestCase):
         t2.join()
 
     async def test_child_threads_inherit_parent_ctx_values(self) -> None:
-        from eave.collectors.core.correlation_context import ThreadedCorrelationContext
-
         ctx = ThreadedCorrelationContext()
         # given values exist in parent context
         ctx.set("parent", 0)
