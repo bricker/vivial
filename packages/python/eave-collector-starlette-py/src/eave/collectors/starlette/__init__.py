41584f4c89cc374efe846432b0fdfdc6bfc646ff
niehusst <liamniehusstaab@gmail.com>
2024-05-02 14:54:06 -0700

    feat: make starlette app instance instrumentation public preferred method

--- packages/python/eave-collector-starlette-py/src/eave/collectors/starlette/__init__.py
+++ packages/python/eave-collector-starlette-py/src/eave/collectors/starlette/__init__.py
@@ -1,16 +1,20 @@
 from eave.collectors.starlette.private.collector import StarletteCollector
+from starlette.applications import Starlette
 
 _collector: StarletteCollector | None = None
 
 
 class StarletteCollectorManager:
     @classmethod
-    def start(cls) -> None:
+    def start(cls, app: Starlette | None) -> None:
         global _collector
 
         if not _collector:
             _collector = StarletteCollector()
-            _collector.instrument()
+            if app is not None:
+                _collector.instrument_app(app=app)
+            else:
+                _collector.instrument()
 
     @classmethod
     def stop(cls) -> None:
