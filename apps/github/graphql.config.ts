e6746c5383897f31a56fa612cf94c6b306e8723a
Bryan Ricker <git@bryanricker.com>
2024-05-04 16:10:42 -0700

    Remove github app

--- apps/github/graphql.config.ts
+++
@@ -1,8 +0,0 @@
-import type { IGraphQLConfig } from "graphql-config";
-
-const config: IGraphQLConfig = {
-  schema: "./node_modules/@octokit/graphql-schema/schema.graphql",
-  documents: ["./src/graphql/**/*.graphql"],
-};
-
-export default config;
