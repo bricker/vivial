7633a76284af929ae2cd96a77b5d7d008eccd1b9
niehusst <liamniehusstaab@gmail.com>
2024-05-13 10:18:47 -0700

    more garbo tests

--- apps/playground-djangoapp/eave_django_playground/polls/views.py
+++ apps/playground-djangoapp/eave_django_playground/polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -21,6 +22,12 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -29,7 +36,7 @@ class ResultsView(generic.DetailView):
 def vote(request, question_id):
     question: Question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+        selected_choice = question.choice_set.get(pk=request.POST["choice"]) # type: ignore
     except (KeyError, Choice.DoesNotExist):
         # back to vote form
         return render(
@@ -45,4 +52,4 @@ def vote(request, question_id):
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
         # redirect after dealing w/ post data. Prevents double post if a user hits back button
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,))) # type: ignore
