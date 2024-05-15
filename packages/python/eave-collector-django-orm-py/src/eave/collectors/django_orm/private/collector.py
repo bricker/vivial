46d79c06a07873a46fe65b4ecbdeea39b6a7f512
niehusst <liamniehusstaab@gmail.com>
2024-05-15 15:57:22 -0700

    feat: first pass impl at dj orm listen

--- packages/python/eave-collector-django-orm-py/src/eave/collectors/django_orm/private/collector.py
+++ packages/python/eave-collector-django-orm-py/src/eave/collectors/django_orm/private/collector.py
@@ -1,9 +1,26 @@
-import django
+from typing import Any
+
+from django.db.models import Model
+from django.db.models.signals import post_save
 
 """
 setup hast o be calle dbefore we can access any orm models (do we need to??)
 > django.setup()
 
-something about settings (django.conf.settings[].configured])
+check settings to see if django has been setup yet (django.conf.settings[.configured])
 """
- 
+
+class DjangoOrmCollector:
+    def _model_saved_callback(self, sender: Any, instance: Model, created: bool, raw: bool, using: str, update_fields: Any, *args, **kwargs) -> None:
+        """
+        https://docs.djangoproject.com/en/5.0/ref/signals/#post-save
+        """
+        # TODO: write to writequeue
+        print("model got saved:", sender, instance, created, raw, using, update_fields)
+
+    def instrument(self) -> None:
+        post_save.connect(self._model_saved_callback)
+
+    def uninstrument(self) -> None:
+        """dont need to call this if `Signal.connect` was called with `weak=True` kwarg (the default value)"""
+        post_save.disconnect(self._model_saved_callback)
