168f962ae266d22302fc3323b0f63b341e98c34a
niehusst <liamniehusstaab@gmail.com>
2024-05-15 14:48:15 -0700

    feat: django orm tracing proj structure

---
+++ packages/python/eave-collector-django-orm-py/src/eave/collectors/django_orm/private/collector.py
@@ -0,0 +1,9 @@
+import django
+
+"""
+setup hast o be calle dbefore we can access any orm models (do we need to??)
+> django.setup()
+
+something about settings (django.conf.settings[].configured])
+"""
+ 
