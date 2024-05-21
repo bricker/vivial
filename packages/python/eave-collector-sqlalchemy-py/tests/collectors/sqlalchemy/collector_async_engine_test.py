f5f04246ff328e93a07015bc743cb7e5f7c1b612
niehusst <liamniehusstaab@gmail.com>
2024-05-21 14:21:31 -0700

    test: user id collection tests

--- packages/python/eave-collector-sqlalchemy-py/tests/collectors/sqlalchemy/collector_async_engine_test.py
+++ packages/python/eave-collector-sqlalchemy-py/tests/collectors/sqlalchemy/collector_async_engine_test.py
@@ -5,13 +5,14 @@ from datetime import datetime
 from uuid import UUID
 
 import sqlalchemy
-from sqlalchemy import func, text
+from sqlalchemy import NullPool, func, text
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 from eave.collectors.core.datastructures import DatabaseEventPayload, DatabaseOperation
 from eave.collectors.core.test_util import EphemeralWriteQueue
 from eave.collectors.sqlalchemy.private.collector import SQLAlchemyCollector
+from eave.collectors.core.correlation_context import corr_ctx
 
 db_uri = sqlalchemy.engine.url.URL.create(
     drivername="postgresql+asyncpg",
@@ -25,6 +26,7 @@ db_uri = sqlalchemy.engine.url.URL.create(
 async_engine = create_async_engine(
     db_uri,
     echo=False,
+    poolclass=NullPool,
     connect_args={
         "server_settings": {
             "timezone": "UTC",
@@ -32,21 +34,28 @@ async_engine = create_async_engine(
     },
 )
 
-async_session = async_sessionmaker(async_engine)
+async_session = async_sessionmaker(async_engine, expire_on_commit=False)
 
 
 class OrmBase(DeclarativeBase):
     pass
 
 
-class PeopleOrm(OrmBase):
-    __tablename__ = "people"
+class AccountOrm(OrmBase):
+    __tablename__ = "accounts"
 
     id: Mapped[UUID] = mapped_column(primary_key=True, server_default=text("(gen_random_uuid())"))
     name: Mapped[str]
     created: Mapped[datetime] = mapped_column(server_default=func.current_timestamp())
     updated: Mapped[datetime | None] = mapped_column(server_default=None, onupdate=func.current_timestamp())
 
+class TodoItem(OrmBase):
+    __tablename__ = "todo_items"
+
+    id: Mapped[UUID] = mapped_column(primary_key=True, server_default=text("(gen_random_uuid())"))
+    text: Mapped[str]
+    created: Mapped[datetime] = mapped_column(server_default=func.current_timestamp())
+    updated: Mapped[datetime | None] = mapped_column(server_default=None, onupdate=func.current_timestamp())
 
 class CollectorTestBase(unittest.IsolatedAsyncioTestCase):
     async def asyncSetUp(self) -> None:
@@ -60,31 +69,35 @@ class CollectorTestBase(unittest.IsolatedAsyncioTestCase):
         self._collector = SQLAlchemyCollector(write_queue=self._write_queue)
         await self._collector.start(engine=async_engine)
 
+    async def asyncTearDown(self) -> None:
+        await super().asyncTearDown()
+        corr_ctx.clear()
+
     async def test_after_execute_insert(self) -> None:
         assert len(self._write_queue.queue) == 0
-        person_name = uuid.uuid4().hex
+        account_name = uuid.uuid4().hex
 
         async with async_session.begin() as session:
-            team = PeopleOrm(name=person_name)
-            session.add(team)
+            account = AccountOrm(name=account_name)
+            session.add(account)
 
         assert len(self._write_queue.queue) == 1
         e = self._write_queue.queue[0]
         assert isinstance(e, DatabaseEventPayload)
-        assert e.table_name == "people"
+        assert e.table_name == "accounts"
         assert e.operation == DatabaseOperation.INSERT
         assert e.db_name == db_uri.database
         assert e.parameters is not None
-        assert e.parameters["name"] == person_name
+        assert e.parameters["name"] == account_name
         assert "__primary_key" in e.parameters
 
     async def test_after_execute_update(self) -> None:
         assert len(self._write_queue.queue) == 0
-        person_name = uuid.uuid4().hex
+        account_name = uuid.uuid4().hex
 
         async with async_session.begin() as session:
-            team = PeopleOrm(name=person_name)
-            session.add(team)
+            account = AccountOrm(name=account_name)
+            session.add(account)
 
         assert len(self._write_queue.queue) == 1
         e0 = self._write_queue.queue[0]
@@ -92,15 +105,43 @@ class CollectorTestBase(unittest.IsolatedAsyncioTestCase):
         assert e0.parameters is not None
 
         async with async_session.begin() as session:
-            r = await session.get_one(entity=PeopleOrm, ident=tuple(e0.parameters["__primary_key"]))
-            new_person_name = uuid.uuid4().hex
-            r.name = new_person_name
+            r = await session.get_one(entity=AccountOrm, ident=tuple(e0.parameters["__primary_key"]))
+            new_account_name = uuid.uuid4().hex
+            r.name = new_account_name
 
         assert len(self._write_queue.queue) == 2
         e = self._write_queue.queue[1]
         assert isinstance(e, DatabaseEventPayload)
-        assert e.table_name == "people"
+        assert e.table_name == "accounts"
         assert e.operation == DatabaseOperation.UPDATE
         assert e.db_name == db_uri.database
         assert e.parameters is not None
-        assert e.parameters["name"] == new_person_name
+        assert e.parameters["name"] == new_account_name
+
+    async def test_after_execute_insert_account_table(self) -> None:
+        assert len(self._write_queue.queue) == 0
+
+        async with async_session.begin() as session:
+            account = AccountOrm(name=uuid.uuid4().hex)
+            session.add(account)
+
+        assert len(self._write_queue.queue) == 1
+        e = self._write_queue.queue[0]
+
+        assert corr_ctx.get("user_id") == str(account.id)
+        assert e.context is not None
+        assert e.context.get("user_id") == str(account.id)
+
+    async def test_after_execute_insert_not_account_table(self) -> None:
+        assert len(self._write_queue.queue) == 0
+
+        async with async_session.begin() as session:
+            orm = TodoItem(text=uuid.uuid4().hex)
+            session.add(orm)
+
+        assert len(self._write_queue.queue) == 1
+        e = self._write_queue.queue[0]
+
+        assert corr_ctx.get("user_id") is None
+        assert e.context is not None
+        assert e.context.get("user_id") is None
