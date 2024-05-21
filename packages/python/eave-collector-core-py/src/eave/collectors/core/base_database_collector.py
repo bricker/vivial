afb254ef95f2b897bda7297a881fe6bb4f8eda52
niehusst <liamniehusstaab@gmail.com>
2024-05-21 14:24:51 -0700

    chore: cleanup

--- packages/python/eave-collector-core-py/src/eave/collectors/core/base_database_collector.py
+++ packages/python/eave-collector-core-py/src/eave/collectors/core/base_database_collector.py
@@ -17,6 +17,7 @@ keys_of_interest = {
     "account_id",
 }
 
+
 def save_identification_data(table_name: str, primary_keys: list[Any]) -> None:
     """
     Saves into correlation context global any database values that may be useful for
@@ -33,13 +34,6 @@ def save_identification_data(table_name: str, primary_keys: list[Any]) -> None:
     if table_name.lower() in tables_of_interest:
         user_id = primary_keys[0]
         corr_ctx.set("user_id", str(user_id))
-
-    # for key, value in data.items():
-    #     if key.lower() in keys_of_interest:
-
-    #         # accounts_id = abc123
-    #         corr_ctx.set(f"{table_name}_{key}".lower(), value)
-
 
 
 class BaseDatabaseCollector(BaseCollector):
--- packages/python/eave-collector-core-py/src/eave/collectors/core/correlation_context/__init__.py
+++ packages/python/eave-collector-core-py/src/eave/collectors/core/correlation_context/__init__.py
@@ -1,33 +1,5 @@
-from typing import Any
 from .async_task import AsyncioCorrelationContext as AsyncioCorrelationContext
 from .thread import ThreadedCorrelationContext as ThreadedCorrelationContext
 
 # TODO: figure out which ctx storage type we need at runtime?
 corr_ctx = ThreadedCorrelationContext()
-
-
-# def save_identification_data(table_name: str, data: dict[str, Any]) -> None:
-#     """
-#     Saves into correlation context global any database values that may be useful for
-#     user identification.
-
-#         Parameters:
-#             table_name (str): name of the `data` entry's table
-#             data (dict): key-value map of a table entry's keys and values
-#     """
-tables_of_interest = {
-    "user",
-    "users",
-    "account",
-    "accounts",
-}
-
-
-keys_of_interest = {
-    "id",
-    "uid",
-}
-
-    # for key, value in data.items():
-    #     if key.lower() in keys_of_interest:
-    #         corr_ctx.set(f"{table_name}_{key}".lower(), value)
