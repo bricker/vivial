9764cd4a3729cb92299b973f5b9a1198a8fae133
niehusst <liamniehusstaab@gmail.com>
2024-05-22 16:01:40 -0700

    refactor: add helper functions for data interest abstraction

--- packages/python/eave-collector-core-py/src/eave/collectors/core/base_database_collector.py
+++ packages/python/eave-collector-core-py/src/eave/collectors/core/base_database_collector.py
@@ -2,7 +2,7 @@ from typing import Any
 from .base_collector import BaseCollector
 from .correlation_context import corr_ctx
 
-tables_of_interest = {
+user_table_names = {
     "user",
     "users",
     "account",
@@ -18,20 +18,30 @@ keys_of_interest = {
 }
 
 
+def is_user_table(table_name: str) -> bool:
+    # TODO: what about grouped table names, like in bq? (e.g. "production.core-db.accounts")
+    return table_name.lower() in user_table_names
+
+
+def is_field_of_interest(field_name: str) -> bool:
+    # TODO: "accounts.id"
+    return field_name.lower() in keys_of_interest
+
+
 def save_identification_data(table_name: str, primary_keys: list[Any]) -> None:
     """
     Saves into correlation context global any database values that may be useful for
     user identification.
 
         Parameters:
-            table_name (str): name of the `data` entry's table
-            data (dict): key-value map of a table entry's keys and values
+            table_name (str): name of database table
+            primary_keys (list): primary key value(s) for relevant row(s) in `table_name`
     """
 
     if len(primary_keys) == 0:
         return
 
-    if table_name.lower() in tables_of_interest:
+    if is_user_table(table_name):
         user_id = primary_keys[0]
         corr_ctx.set("user_id", str(user_id))
 
