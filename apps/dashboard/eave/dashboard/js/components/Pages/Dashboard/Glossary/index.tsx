7b396cb38a1f1e8ad8119ecdd485c9447ce6f27c
niehusst <liamniehusstaab@gmail.com>
2024-05-30 10:10:10 -0700

    refactor: extract search bar component from glos to optimize input lag

--- apps/dashboard/eave/dashboard/js/components/Pages/Dashboard/Glossary/index.tsx
+++ apps/dashboard/eave/dashboard/js/components/Pages/Dashboard/Glossary/index.tsx
@@ -1,13 +1,13 @@
 import CloseIcon from "$eave-dashboard/js/components/Icons/CloseIcon";
-import SearchIcon from "$eave-dashboard/js/components/Icons/SearchIcon";
 import SidePanelIcon from "$eave-dashboard/js/components/Icons/SidePanelIcon";
+import SearchBar from "$eave-dashboard/js/components/Pages/Dashboard/Glossary/SearchBar";
 import { AppContext } from "$eave-dashboard/js/context/Provider";
 import useTeam from "$eave-dashboard/js/hooks/useTeam";
 import { theme as eaveTheme } from "$eave-dashboard/js/theme";
 import { VirtualEvent } from "$eave-dashboard/js/types.js";
 import { CircularProgress } from "@mui/material";
 import classNames from "classnames";
-import React, { useCallback, useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { makeStyles } from "tss-react/mui";
 
 const makeClasses = makeStyles<void, "hoverIcon">()((theme, _params, classes) => ({
@@ -20,103 +20,26 @@ const makeClasses = makeStyles<void, "hoverIcon">()((theme, _params, classes) =>
     fontSize: 34,
     fontWeight: 400,
   },
-  searchBar: {
-    maxWidth: 789,
-    minWidth: "calc(100vw / 2)",
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    textAlign: "center",
-    borderRadius: 20,
-    backgroundColor: "#f1f1f1",
-    boxSizing: "border-box",
-    padding: 12,
-    marginTop: 18,
-    "&:focus-within": {
-      outline: "2px solid",
-    },
-    header: {
-      fontSize: 34,
-      fontWeight: 400,
-    },
-    searchBar: {
-      maxWidth: 789,
-      minWidth: "calc(100vw / 2)",
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "center",
-      textAlign: "center",
-      borderRadius: 20,
-      backgroundColor: "#f1f1f1",
-      boxSizing: "border-box",
-      padding: 12,
-      marginTop: 18,
-      "&:focus-within": {
-        outline: "2px solid",
-      },
-    },
-    searchIcon: {
-      position: "relative",
-      top: 3,
-    },
-    searchInput: {
-      backgroundColor: "transparent",
-      outline: "none",
-      fontSize: 16,
-      padding: "1px 10px",
-      border: "none",
-      flexGrow: 1,
-    },
-    table: {
-      borderCollapse: "collapse",
-      width: "100%",
-      maxWidth: 1000,
-      fontSize: 14,
-      marginTop: 60,
-    },
-    tableValue: {
-      textAlign: "left",
-      padding: "12px 24px",
-    },
-    columnWidthLimit: {
-      maxWidth: "calc(100vw / 3)",
-    },
-    tableHeader: {
-      fontWeight: "bold",
-      fontSize: 16,
-    },
-    tableRow: {
-      "&:nth-child(even)": {
-        backgroundColor: "#e5e9f5",
-      },
-    },
-    rowHighlight: {
-      "&:hover": {
-        backgroundColor: "#36363666",
-        cursor: "pointer",
-      },
-      [`&:hover ${classes.hoverIcon}`]: {
-        opacity: 100,
-      },
-    },
-    hoverIcon: {
-      opacity: 0,
-    },
-    root: {
-      display: "flex",
-      flexDirection: "row",
-      flexGrow: 1,
-      wordWrap: "break-word",
-      overflowX: "hidden",
-    },
-    panelContainer: {
-      position: "fixed",
-      right: 0,
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "flex-start",
-      height: "100vh",
-      width: "calc(100vw / 4)",
+  table: {
+    borderCollapse: "collapse",
+    width: "100%",
+    maxWidth: 1000,
+    fontSize: 14,
+    marginTop: 60,
+  },
+  tableValue: {
+    textAlign: "left",
+    padding: "12px 24px",
+  },
+  columnWidthLimit: {
+    maxWidth: "calc(100vw / 3)",
+  },
+  tableHeader: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  tableRow: {
+    "&:nth-child(even)": {
       backgroundColor: "#e5e9f5",
     },
   },
@@ -193,7 +116,6 @@ const makeClasses = makeStyles<void, "hoverIcon">()((theme, _params, classes) =>
 
 const Glossary = () => {
   const { classes } = makeClasses();
-  const [searchValue, setSearchValue] = useState("");
   const [selectedEvent, setSelectedEvent] = useState<VirtualEvent | null>(null);
   const [isOpen, setIsOpen] = useState(false);
   const [usingMobileLayout, setUsingMobileLayout] = useState(false);
@@ -229,36 +151,6 @@ const Glossary = () => {
   } else if (usingMobileLayout) {
     panelClasses.push(classes.panelFullScreen);
   }
-
-  // useCallback to preserve timer variable across renders
-  const debouncedFilterEventsOnType = useCallback(
-    (() => {
-      const halfSecondMs = 500;
-      let timer: NodeJS.Timeout | undefined = undefined;
-
-      return (searchTerm: string) => {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-          if (!searchTerm) {
-            // refresh results w/ all events when search bar is emptied
-            getTeamVirtualEvents(null);
-            return;
-          }
-          if (searchTerm.length < 3) {
-            // wait for min 3 chars to be typed before doing a search
-            return;
-          }
-          getTeamVirtualEvents({ search_term: searchTerm });
-        }, halfSecondMs);
-      };
-    })(),
-    [],
-  );
-
-  // perform search for events
-  useEffect(() => {
-    debouncedFilterEventsOnType(searchValue);
-  }, [searchValue]);
 
   // factored out as it's used in both the row onClick and onKeyPress actions
   const rowClicked = (event: VirtualEvent) => {
@@ -315,11 +207,7 @@ const Glossary = () => {
     );
   } else if (networkState.virtualEventsAreErroring) {
     // show err state
-    component = (
-      <div className={classes.error}>
-        ERROR: Unable to fetch virtual events {searchValue ? `for searched term "${searchValue}"` : ""}
-      </div>
-    );
+    component = <div className={classes.error}>ERROR: Unable to fetch virtual events</div>;
   } else {
     // not loading/erroring and no events found
     component = (
@@ -354,18 +242,7 @@ const Glossary = () => {
         <div className={classNames(glossaryClasses)}>
           <h1 className={classes.header}>Event Glossary</h1>
 
-          <div className={classes.searchBar}>
-            <i className={classes.searchIcon}>
-              <SearchIcon color="#33363f" />
-            </i>
-            <input
-              type="text"
-              className={classes.searchInput}
-              placeholder="Type to search events"
-              value={searchValue}
-              onChange={(elem) => setSearchValue(elem.target.value)}
-            />
-          </div>
+          <SearchBar />
 
           {component}
         </div>
