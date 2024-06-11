1057a925445ccd5d3a2537c1d3c52f264bfb7dc4
niehusst <liamniehusstaab@gmail.com>
2024-06-11 15:58:15 -0700

    fix bg color

--- apps/dashboard/eave/dashboard/js/components/Pages/Dashboard/index.tsx
+++ apps/dashboard/eave/dashboard/js/components/Pages/Dashboard/index.tsx
@@ -22,9 +22,12 @@ import TeamManagement from "./TeamManagement";
 
 const makeClasses = makeStyles()(() => ({
   sharedContainer: {
+    backgroundColor: "white",
     position: "absolute",
     top: 0,
     left: 0,
+    right: 0,
+    bottom: 0,
   },
   desktopContainer: {
     display: "flex",
