198b6139f10909ab690b9b45320e7a6e7e3a3eda
niehusst <liamniehusstaab@gmail.com>
2024-07-05 18:53:04 -0700

    chore: remove core api dep from stdlib tests

--- libs/eave-stdlib-py/tests/stdlib/deidentification_test.py
+++ libs/eave-stdlib-py/tests/stdlib/deidentification_test.py
@@ -1,13 +1,6 @@
 import dataclasses
 from typing import Any
 
-from eave.core.internal.atoms.controllers.browser_events import BrowserEventAtom
-from eave.core.internal.atoms.models.api_payload_types import CurrentPageProperties, TargetProperties
-from eave.core.internal.atoms.models.db_record_fields import (
-    CurrentPageRecordField,
-    MultiScalarTypeKeyValueRecordField,
-    TargetRecordField,
-)
 from eave.stdlib.deidentification import (
     REDACTABLE,
     _flatten_to_dict,
@@ -55,75 +48,39 @@ class BasketWeaver:
 
 class DeidentificationTest(StdlibBaseTestCase):
     def test_object_flattening(self) -> None:
-        obj = BrowserEventAtom(
-            action="Bryan Cameron Ricker is my name!",
-            timestamp=None,
-            session=None,
-            account=None,
-            visitor_id="23d35726-3f73-4041-a724-b788c0b12b97",
-            traffic_source=None,
-            target=TargetRecordField.from_api_resource(
-                TargetProperties.from_api_payload(
-                    {
-                        "type": "x",
-                        "id": "xx",
-                        "content": "xxx",
-                        "attributes": {},
-                    }
-                )
+        obj = BasketWeaver(
+            tools=[Tool(type="knitting needles"), Tool(type="banana")],
+            name="Fred",
+            helpers=[
+                WeavingSpider(genus="bug", favorite_legs=[4, 9], name="Anji"),
+                WeavingSpider(genus="orb weaver", favorite_legs=None, name="Arachne"),
+            ],
+            latest_work=Basket(
+                thread_count=3,
+                woven_underwater=True,
+                price=None,
+                specs={"1": Spec(info="well crafted"), "2": Spec(info=None)},
             ),
-            current_page=CurrentPageRecordField.from_api_resource(
-                CurrentPageProperties.from_api_payload(
-                    {
-                        "url": "https://sensitive_domain.com/your_credit_card_number?ccn=3742-0454-5540-0126",
-                        "title": "Your Credit Card Number",
-                    }
-                )
-            ),
-            device=None,
-            geo=None,
-            extra=MultiScalarTypeKeyValueRecordField.list_from_scalar_dict(
-                {
-                    "password": "sddf89sdfd89dsafh9sd",
-                    "ethnicity": "White",
-                }
-            ),
-            client_ip=None,
         )
 
-        result = _flatten_to_dict(obj)
+        flat = _flatten_to_dict(obj)
 
-        assert result == {
-            "action": "Bryan Cameron Ricker is my name!",
-            "timestamp": None,
-            "session": None,
-            "account": None,
-            "visitor_id": "23d35726-3f73-4041-a724-b788c0b12b97",
-            "traffic_source": None,
-            "target.type": "x",
-            "target.id": "xx",
-            "target.content": "xxx",
-            "target.attributes": None,
-            "current_page.url.raw": "https://sensitive_domain.com/your_credit_card_number?ccn=3742-0454-5540-0126",
-            "current_page.url.protocol": "https",
-            "current_page.url.domain": "sensitive_domain.com",
-            "current_page.url.path": "/your_credit_card_number",
-            "current_page.url.hash": None,
-            "current_page.url.query_params.0.key": "ccn",
-            "current_page.url.query_params.0.value": "3742-0454-5540-0126",
-            "current_page.title": "Your Credit Card Number",
-            "current_page.pageview_id": None,
-            "device": None,
-            "geo": None,
-            "extra.0.key": "password",
-            "extra.0.value.string_value": "sddf89sdfd89dsafh9sd",
-            "extra.0.value.bool_value": None,
-            "extra.0.value.numeric_value": None,
-            "extra.1.value.bool_value": None,
-            "extra.1.value.numeric_value": None,
-            "extra.1.key": "ethnicity",
-            "extra.1.value.string_value": "White",
-            "client_ip": None,
+        assert flat == {
+            "tools.0.type": "knitting needles",
+            "tools.1.type": "banana",
+            "name": "Fred",
+            "helpers.0.genus": "bug",
+            "helpers.0.favorite_legs.0": 4,
+            "helpers.0.favorite_legs.1": 9,
+            "helpers.0.name": "Anji",
+            "helpers.1.genus": "orb weaver",
+            "helpers.1.favorite_legs": None,
+            "helpers.1.name": "Arachne",
+            "latest_work.thread_count": 3,
+            "latest_work.woven_underwater": True,
+            "latest_work.price": None,
+            'latest_work.specs."1".info': "well crafted",
+            'latest_work.specs."2".info': None,
         }
 
     def test_flatten_dict_member(self) -> None:
@@ -136,113 +93,45 @@ class DeidentificationTest(StdlibBaseTestCase):
         assert _flatten_to_dict(f) == {'d."test"': "object", 'd."looking"': "good", 'd."someth"ing"': "janky"}
 
     def test_object_write_back(self) -> None:
-        obj = BrowserEventAtom(
-            action="vote for Jeb! Bush",
-            timestamp=None,
-            session=None,
-            account=None,
-            visitor_id="23d35726-3f73-4045-a724-b788c0b12b97",
-            traffic_source=None,
-            target=TargetRecordField.from_api_resource(
-                TargetProperties.from_api_payload(
-                    {
-                        "type": "AB+",
-                        "id": "grandmamas health record",
-                        "content": "grandma is 79 years old",
-                        "attributes": {"conditions": ["type 1 diabetes", "shingles"]},
-                    }
-                )
+        obj = BasketWeaver(
+            tools=[Tool(type="knitting needles"), Tool(type="banana")],
+            name="Fred",
+            helpers=[
+                WeavingSpider(genus="bug", favorite_legs=[4, 9], name="Anji"),
+                WeavingSpider(genus="orb weaver", favorite_legs=None, name="Arachne"),
+            ],
+            latest_work=Basket(
+                thread_count=3,
+                woven_underwater=True,
+                price=None,
+                specs={"1": Spec(info="well crafted"), "2": Spec(info=None)},
             ),
-            current_page=CurrentPageRecordField.from_api_resource(
-                CurrentPageProperties.from_api_payload(
-                    {
-                        "url": "https://sensitive_domain.com/your_credit_card_number?ccn=3742-0454-5540-0126",
-                        "title": "my Credit Card Number",
-                    }
-                )
-            ),
-            device=None,
-            geo=None,
-            extra=MultiScalarTypeKeyValueRecordField.list_from_scalar_dict(
-                {
-                    "slam-poetry-id": "sddf89sdfd89dsafh9sd",
-                }
-            ),
-            client_ip=None,
         )
 
+        write_data = {
+            "tools.0.type": "[REDACT]",
+            "tools.1.type": "banana",
+            "name": "[NAME]",
+            "helpers.0.genus": "bug",
+            "helpers.0.favorite_legs.0": 4,
+            "helpers.0.favorite_legs.1": 9,
+            "helpers.0.name": "Anji",
+            "helpers.1.genus": "orb weaver",
+            "helpers.1.favorite_legs": None,
+            "helpers.1.name": "Arachne",
+            "latest_work.thread_count": 3,
+            "latest_work.woven_underwater": True,
+            "latest_work.price": None,
+            'latest_work.specs."1".info': "[CREDIT_CARD]",
+            'latest_work.specs."2".info': None,
+        }
         _write_flat_data_to_object(
-            data={
-                "target.attributes": None,
-                "client_ip": None,
-                "user": None,
-                "device": None,
-                "extra.0.value.bool_value": None,
-                "target.content": "grandma is [AGE]",
-                "account": None,
-                "visitor_id": "23d35726-3f73-4045-a724-b788c0b12b97",
-                "target.attributes.0.value.1": "shingles",
-                "current_page.url.query_params.0.value": "3742-0454-5540-0126",
-                "target.id": "grandmamas health record",
-                "action": "vote for [PERSON_NAME]! [PERSON_NAME]",
-                "extra.0.value.numeric_value": None,
-                "current_page.url.protocol": "https",
-                "timestamp": None,
-                "target.attributes.0.key": "conditions",
-                "extra.1.key": None,
-                "current_page.url.hash": None,
-                "extra.1.value.string_value": None,
-                "current_page.url.domain": "sensitive_domain.com",
-                "current_page.pageview_id": None,
-                "geo": None,
-                "extra.0.key": "slam-poetry-id",
-                "current_page.url.query_params": None,
-                "extra.0.value.string_value": "sddf89sdfd89dsafh9sd",
-                "target.attributes.0.value.0": "type 1 diabetes",
-                "extra.1.value.bool_value": None,
-                "current_page.url.query_params.0.key": "ccn",
-                "session": None,
-                "target.type": "AB+",
-                "current_page.url.raw": "https://sensitive_domain.com/your_credit_card_number?ccn=3742-0454-5540-0126",
-                "extra.1.value.numeric_value": None,
-                "traffic_source": None,
-                "current_page.url.path": "/your_credit_card_number",
-                "current_page.title": "my Credit Card Number",
-            },
+            data=write_data,
             obj=obj,
         )
 
         flat_modified_obj = _flatten_to_dict(obj)
-        assert flat_modified_obj == {
-            "action": "vote for [PERSON_NAME]! [PERSON_NAME]",
-            "timestamp": None,
-            "session": None,
-            "traffic_source": None,
-            "device": None,
-            "geo": None,
-            "client_ip": None,
-            "account": None,
-            "visitor_id": "23d35726-3f73-4045-a724-b788c0b12b97",
-            "target.type": "AB+",
-            "target.id": "grandmamas health record",
-            "target.content": "grandma is [AGE]",
-            "current_page.title": "my Credit Card Number",
-            "current_page.pageview_id": None,
-            "current_page.url.raw": "https://sensitive_domain.com/your_credit_card_number?ccn=3742-0454-5540-0126",
-            "current_page.url.protocol": "https",
-            "current_page.url.domain": "sensitive_domain.com",
-            "current_page.url.path": "/your_credit_card_number",
-            "current_page.url.hash": None,
-            "extra.0.key": "slam-poetry-id",
-            "target.attributes.0.key": "conditions",
-            "extra.0.value.string_value": "sddf89sdfd89dsafh9sd",
-            "extra.0.value.numeric_value": None,
-            "extra.0.value.bool_value": None,
-            "target.attributes.0.value.0": "type 1 diabetes",
-            "target.attributes.0.value.1": "shingles",
-            "current_page.url.query_params.0.key": "ccn",
-            "current_page.url.query_params.0.value": "3742-0454-5540-0126",
-        }
+        assert flat_modified_obj == write_data
 
     def test_redactable_field_identification(self) -> None:
         assert sorted(_redactable_fields_matchers(Basket)) == sorted(
@@ -294,74 +183,32 @@ class DeidentificationTest(StdlibBaseTestCase):
 
     async def test_dlp_integration(self) -> None:
         atoms = [
-            BrowserEventAtom(
-                action="Bryan Cameron Ricker is my name!",
-                timestamp=None,
-                session=None,
-                account=None,
-                visitor_id=None,
-                traffic_source=None,
-                target=TargetRecordField.from_api_resource(
-                    TargetProperties.from_api_payload(
-                        {
-                            "type": "xx",
-                            "id": "xx",
-                            "content": "xx",
-                            "attributes": {},
-                        }
-                    )
+            BasketWeaver(
+                tools=[Tool(type="knitting needles"), Tool(type="drill")],
+                name="Fred",
+                helpers=[
+                    WeavingSpider(genus="bug", favorite_legs=[4, 9], name="Anji"),
+                    WeavingSpider(genus="orb weaver", favorite_legs=None, name="Arachne"),
+                ],
+                latest_work=Basket(
+                    thread_count=3,
+                    woven_underwater=True,
+                    price=None,
+                    specs={"ccn": Spec(info="4263982640269299"), "2": Spec(info="10 years old")},
                 ),
-                current_page=CurrentPageRecordField.from_api_resource(
-                    CurrentPageProperties.from_api_payload(
-                        {
-                            "url": "https://sensitive_domain.com/your_credit_card_number/4263982640269299",
-                            "title": "Your Credit Card Number",
-                        }
-                    )
-                ),
-                device=None,
-                geo=None,
-                extra=MultiScalarTypeKeyValueRecordField.list_from_scalar_dict(
-                    {
-                        "password": "my password is password123",
-                        "ethnicity": "caucasian",
-                    }
-                ),
-                client_ip=None,
             ),
-            BrowserEventAtom(
-                action="vote for Jeb! Bush",
-                timestamp=None,
-                session=None,
-                account=None,
-                visitor_id="23d35726-3f73-4045-a724-b788c0b12b97",
-                traffic_source=None,
-                target=TargetRecordField.from_api_resource(
-                    TargetProperties.from_api_payload(
-                        {
-                            "blood-type": "AB+",
-                            "id": "grandmamas health record",
-                            "content": "grandma is 79 years old",
-                            "attributes": {"conditions": ["type 1 diabetes", "shingles"]},
-                        }
-                    )
+            BasketWeaver(
+                tools=[],
+                name="Samantha",
+                helpers=[
+                    WeavingSpider(genus="grasshopper", favorite_legs=[], name="Phil"),
+                ],
+                latest_work=Basket(
+                    thread_count=3,
+                    woven_underwater=False,
+                    price="$10000",
+                    specs={"rookie.mistake": Spec(info="poorly crafted")},
                 ),
-                current_page=CurrentPageRecordField.from_api_resource(
-                    CurrentPageProperties.from_api_payload(
-                        {
-                            "url": "https://sensitive_domain.com/your_credit_card_number?ccn=3742-0454-5540-0126",
-                            "title": "my Credit Card Number",
-                        }
-                    )
-                ),
-                device=None,
-                geo=None,
-                extra=MultiScalarTypeKeyValueRecordField.list_from_scalar_dict(
-                    {
-                        "slam-poetry-id": "sddf89sdfd89dsafh9sd",
-                    }
-                ),
-                client_ip=None,
             ),
         ]
 
@@ -370,65 +217,30 @@ class DeidentificationTest(StdlibBaseTestCase):
 
         assert updated_flat == [
             {
-                "account": None,
-                "action": "Bryan Cameron Ricker is my name!",
-                "client_ip": None,
-                "current_page.pageview_id": None,
-                "current_page.title": "Your Credit Card Number",
-                "current_page.url.domain": "sensitive_domain.com",
-                "current_page.url.hash": None,
-                "current_page.url.path": "/your_credit_card_number/[CREDIT_CARD_NUMBER]",
-                "current_page.url.protocol": "https",
-                "current_page.url.query_params": None,
-                "current_page.url.raw": "https://sensitive_domain.com/your_credit_card_number/[CREDIT_CARD_NUMBER]",
-                "device": None,
-                "extra.0.key": "password",
-                "extra.0.value.bool_value": None,
-                "extra.0.value.numeric_value": None,
-                "extra.0.value.string_value": "my password is password123",
-                "extra.1.key": "ethnicity",
-                "extra.1.value.bool_value": None,
-                "extra.1.value.numeric_value": None,
-                "extra.1.value.string_value": "caucasian",
-                "geo": None,
-                "session": None,
-                "target.attributes": None,
-                "target.content": "xx",
-                "target.id": "xx",
-                "target.type": "xx",
-                "timestamp": None,
-                "traffic_source": None,
-                "visitor_id": None,
+                "tools.0.type": "knitting needles",
+                "tools.1.type": "drill",
+                "name": "[PERSON_NAME]",
+                "helpers.0.genus": "bug",
+                "helpers.0.favorite_legs.0": 4,
+                "helpers.0.favorite_legs.1": 9,
+                "helpers.0.name": "[PERSON_NAME]",
+                "helpers.1.genus": "orb weaver",
+                "helpers.1.favorite_legs": None,
+                "helpers.1.name": "[LOCATION]",
+                "latest_work.thread_count": 3,
+                "latest_work.woven_underwater": True,
+                "latest_work.price": None,
+                'latest_work.specs."ccn".info': "[CREDIT_CARD_NUMBER]",
+                'latest_work.specs."2".info': "[AGE]",
             },
             {
-                "account": None,
-                "action": "vote for Jeb! Bush",
-                "client_ip": None,
-                "current_page.pageview_id": None,
-                "current_page.title": "my Credit Card Number",
-                "current_page.url.domain": "sensitive_domain.com",
-                "current_page.url.hash": None,
-                "current_page.url.path": "/your_credit_card_number",
-                "current_page.url.protocol": "https",
-                "current_page.url.query_params.0.key": "ccn",
-                "current_page.url.query_params.0.value": "3742-0454-5540-0126",
-                "current_page.url.raw": "https://sensitive_domain.com/your_credit_card_number?ccn=3742-0454-5540-0126",
-                "device": None,
-                "extra.0.key": "slam-poetry-id",
-                "extra.0.value.bool_value": None,
-                "extra.0.value.numeric_value": None,
-                "extra.0.value.string_value": "sddf89sdfd89dsafh9sd",
-                "geo": None,
-                "session": None,
-                "target.attributes.0.key": "conditions",
-                "target.attributes.0.value.0": "type 1 diabetes",
-                "target.attributes.0.value.1": "shingles",
-                "target.content": "grandma is [AGE]",
-                "target.id": "grandmamas health record",
-                "target.type": None,
-                "timestamp": None,
-                "traffic_source": None,
-                "visitor_id": "23d35726-3f73-4045-a724-b788c0b12b97",
+                "name": "[PERSON_NAME]",
+                "helpers.0.genus": "grasshopper",
+                "helpers.0.name": "[PERSON_NAME]",
+                "latest_work.thread_count": 3,
+                "latest_work.woven_underwater": False,
+                "latest_work.price": "$10000",
+                'latest_work.specs."rookie.mistake".info': "poorly crafted",
             },
         ]
 
