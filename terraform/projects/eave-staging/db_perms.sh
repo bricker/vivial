70d28f9a8414213ce2756b9a4d9158fb28c01f25
Bryan Ricker <git@bryanricker.com>
2024-07-17 19:34:14 -0700

    shell formatting

--- terraform/projects/eave-staging/db_perms.sh
+++
@@ -1,6 +0,0 @@
-user: postgres
-db: postgres
-
-grant "gsa-app-mb-xxx@eave-staging.iam" to postgres;
-alter database "mb_xxx" owner to "gsa-app-mb-xxx@eave-staging.iam";
-revoke "gsa-app-mb-xxx@eave-staging.iam" from postgres;
