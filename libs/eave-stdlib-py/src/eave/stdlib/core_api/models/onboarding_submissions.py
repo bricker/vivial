d091f42dc083eaec7efc4c41e52241697c0f91d2
niehusst <liamniehusstaab@gmail.com>
2024-07-29 11:22:50 -0700

    chore: update form resp type

--- libs/eave-stdlib-py/src/eave/stdlib/core_api/models/onboarding_submissions.py
+++ libs/eave-stdlib-py/src/eave/stdlib/core_api/models/onboarding_submissions.py
@@ -1,7 +1,5 @@
-from typing import Any
-
 from eave.stdlib.core_api.models import BaseResponseModel
 
 
 class OnboardingSubmission(BaseResponseModel):
-    response_data: dict[str, Any]
+    response_data: dict[str, list[str]]
