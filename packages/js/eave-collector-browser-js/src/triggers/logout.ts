d2a34af142db7b9f3df9c407ef31c4b8c0da60f5
niehusst <liamniehusstaab@gmail.com>
2024-08-05 09:30:43 -0700

    fix: expand signout regex

--- packages/js/eave-collector-browser-js/src/triggers/logout.ts
+++ packages/js/eave-collector-browser-js/src/triggers/logout.ts
@@ -1,20 +1,17 @@
 import { TRAFFIC_SOURCE_COOKIE_NAME, deleteCookie, getAllEaveAccountCookies } from "../cookies";
 import { castEventTargetToHtmlElement } from "../util/type-helpers";
 
-const signOutRegex = new RegExp(/(sign out)|(logout)/i);
+const signOutRegex = new RegExp(/(sign ?out)|(log ?out)/i);
 
 export async function logoutEventHandler(event: MouseEvent) {
-  console.log("handling logout?");
   if (event.target) {
     // TODO: drill deeper?? search for wrapping button around clicked elem?
     const targetElement = castEventTargetToHtmlElement(event.target);
     const nodeName = targetElement?.nodeName.toUpperCase();
-    console.log("got clieked element", nodeName, targetElement);
 
     // delete auth and traffic cookies on signout button click
     const targetContainsSignoutText = !(targetElement?.innerText ?? "").search(signOutRegex);
     if ((nodeName === "A" || nodeName === "BUTTON") && targetContainsSignoutText) {
-      console.log("deleting cookeis!");
       deleteCookie({ name: TRAFFIC_SOURCE_COOKIE_NAME });
       for (const [cookieName, _] of getAllEaveAccountCookies()) {
         deleteCookie({ name: cookieName });
