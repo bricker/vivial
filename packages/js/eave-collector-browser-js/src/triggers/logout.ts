908fb5963bc0afbef01b0828f5a2862d85187515
niehusst <liamniehusstaab@gmail.com>
2024-08-05 09:22:56 -0700

    fix: failing existing tests

--- packages/js/eave-collector-browser-js/src/triggers/logout.ts
+++ packages/js/eave-collector-browser-js/src/triggers/logout.ts
@@ -4,13 +4,17 @@ import { castEventTargetToHtmlElement } from "../util/type-helpers";
 const signOutRegex = new RegExp(/(sign out)|(logout)/i);
 
 export async function logoutEventHandler(event: MouseEvent) {
+  console.log("handling logout?");
   if (event.target) {
-    // TODO: drill deeper??
+    // TODO: drill deeper?? search for wrapping button around clicked elem?
     const targetElement = castEventTargetToHtmlElement(event.target);
     const nodeName = targetElement?.nodeName.toUpperCase();
+    console.log("got clieked element", nodeName, targetElement);
 
     // delete auth and traffic cookies on signout button click
-    if ((nodeName === "A" || nodeName === "BUTTON") && (targetElement?.innerText ?? "").search(signOutRegex)) {
+    const targetContainsSignoutText = !(targetElement?.innerText ?? "").search(signOutRegex);
+    if ((nodeName === "A" || nodeName === "BUTTON") && targetContainsSignoutText) {
+      console.log("deleting cookeis!");
       deleteCookie({ name: TRAFFIC_SOURCE_COOKIE_NAME });
       for (const [cookieName, _] of getAllEaveAccountCookies()) {
         deleteCookie({ name: cookieName });
