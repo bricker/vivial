143b2528ba3ea53839d07e8fa6cf333cfe712d0d
niehusst <liamniehusstaab@gmail.com>
2024-08-22 17:14:59 -0700

    woohoo cloud logging

--- apps/core/eave/core/internal/atoms/controllers/collector_logs.py
+++ apps/core/eave/core/internal/atoms/controllers/collector_logs.py
@@ -1,16 +1,15 @@
 import dataclasses
-from typing import Any, cast
-from eave.stdlib.logging import LOGGER, LogContext
+from typing import Any
+from google.cloud.logging import Client as CloudLogClient
 
+from eave.stdlib.logging import LogContext
 from eave.core.internal.atoms.models.collector_logs import AtomCollectorLogRecord
-from eave.core.internal.lib.bq_client import EAVE_INTERNAL_BIGQUERY_CLIENT
 from .base_atom_controller import BaseAtomController
 
 
 class AtomCollectorLogsController(BaseAtomController):
     async def insert(self, raw_logs: list[dict[str, Any]], ctx: LogContext) -> None:
-        # TODO: convert to send data to cloud logging instead of bq
-        table = self.get_or_create_bq_table(table_def=AtomCollectorLogRecord.table_def(), ctx=ctx)
+        cloud_logger = CloudLogClient().logger("collector_log")
 
         logs = [
             AtomCollectorLogRecord.from_api_payload(payload, decryption_key=self._client.decryption_key)
@@ -20,10 +19,12 @@ class AtomCollectorLogsController(BaseAtomController):
         if len(logs) == 0:
             return
 
-        errors = EAVE_INTERNAL_BIGQUERY_CLIENT.append_rows(
-            table=table,
-            rows=[dataclasses.asdict(log) for log in logs],
-        )
+        for log in logs:
+            cloud_logger.log_text(
+                text=log.msg,
+                # sev levels: https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#logseverity
+                severity=log.level.upper() if log.level else "DEFAULT",
+                # just slap in other fields as labels for now...
+                labels={k: v for k,v in dataclasses.asdict(log).keys() if v and k not in ("msg", "level")}
+            )
 
-        if len(errors) > 0:
-            LOGGER.warning("BigQuery insert errors", {"errors": cast(list, errors)}, ctx)
