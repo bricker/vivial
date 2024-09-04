67cd65c351f4cffaa927f6d3925438029a3f6057
Bryan Ricker <git@bryanricker.com>
2024-09-04 09:58:11 -0700

    Wed Sep  4 09:58:11 AM PDT 2024

--- terraform/apps/playground_todoapp/iam.tf
+++ terraform/apps/playground_todoapp/iam.tf
@@ -25,7 +25,7 @@ resource "google_project_iam_binding" "project_app_role_members" {
 
 module "cloudsql_bastion" {
   # Creates the CloudSQL bastion, firewall rule, and IAM bindings
-  source = "../../modules/cloudsql_bastion"
+  source = "../../modules/bastion_vm"
   app_service_account_id = module.service_accounts.gsa_account_id
   cloudsql_instance_name = var.cloudsql_instance_name
   network_name = var.network_name
