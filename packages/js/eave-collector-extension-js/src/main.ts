3cf6880a115434d45191f5d87311a46e02a433d3
niehusst <liamniehusstaab@gmail.com>
2024-09-20 15:34:26 -0700

    chore: ext collector project structure

---
+++ packages/js/eave-collector-extension-js/src/main.ts
@@ -0,0 +1,23 @@
+function startCollecting(clientId: string, clientSecret: string) {
+  // TODO: save client stuff and add a bunch of event listeners
+  // TODO: add browser ext eslint plugins
+}
+
+async function sendAtom() {
+  try {
+    const response = await fetch(`${eave_ingest_base_url}/public/ingest/extension`, {
+      method: "POST",
+      body: JSON.stringify(),
+      headers: {
+        "eave-client-id": client_id,
+        "eave-client-secret": client_secret,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+  } catch {
+    // TODO: requeue to try batch again later?
+  }
+}
