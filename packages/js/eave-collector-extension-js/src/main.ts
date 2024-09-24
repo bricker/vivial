866afbd2ab9521c205fb62417cb71bac1c7fceb6
niehusst <liamniehusstaab@gmail.com>
2024-09-24 15:36:54 -0700

    chore: test

--- packages/js/eave-collector-extension-js/src/main.ts
+++ packages/js/eave-collector-extension-js/src/main.ts
@@ -1,7 +1,7 @@
 export function startCollecting({ clientId }: { clientId: string }) {
   // TODO: save client stuff and add a bunch of event listeners
   // TODO: add browser ext eslint plugins for vscode
-  console.debug("eave extension collector started");
+  console.debug("eave extension collector started", clientId);
   // chrome.storage.local;
 }
 
