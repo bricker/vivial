1d4321c4d58612fea54914ab7fc179f320557518
niehusst <liamniehusstaab@gmail.com>
2024-09-26 18:32:38 -0700

    test: fix default ingest base url

--- packages/js/eave-collector-browser-js/tests/integration/cypress/support/constants.js
+++ packages/js/eave-collector-browser-js/tests/integration/cypress/support/constants.js
@@ -1,5 +1,4 @@
-export const EAVE_CLIENT_ID = Cypress.env("EAVE_CLIENT_ID");
-export const EAVE_API_BASE_URL_PUBLIC = Cypress.env("EAVE_API_BASE_URL_PUBLIC") || "https://api.eave.fyi";
+export const EAVE_API_BASE_URL_PUBLIC = Cypress.env("EAVE_API_BASE_URL_PUBLIC") || "https://api.eave.dev";
 export const EAVE_ATOM_INGESTION_ENDPOINT = `${EAVE_API_BASE_URL_PUBLIC}/public/ingest/browser`;
 export const ATOM_INTERCEPTION_EVENT_NAME = "atomFired";
 
