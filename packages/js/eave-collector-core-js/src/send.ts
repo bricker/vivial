b774071f0aa234caa7998e131a98177200d23b49
niehusst <liamniehusstaab@gmail.com>
2024-09-26 17:49:38 -0700

    feat: try use sendBeacon if available

--- packages/js/eave-collector-core-js/src/send.ts
+++ packages/js/eave-collector-core-js/src/send.ts
@@ -13,30 +13,42 @@ export function sendWithClientId({
 }) {
   try {
     const json = JSON.stringify(jsonBody);
+    const urlWithAuth = `${url}?clientId=${clientId}`;
+    // Important note: The `type` property here should be `application/x-www-form-urlencoded`, because that mimetype is CORS-safelisted as documented here:
+    // https://fetch.spec.whatwg.org/#cors-safelisted-request-header
+    // If set to a non-safe mimetype (eg application/json), sendBeacon will send a pre-flight CORS request (OPTIONS) to the server, and the server is then responsible
+    // for responding with CORS "access-control-allow-*" headers. That's okay, but it adds unnecessary overhead to both the client and the server.
+    const ctypeHeader = "application/x-www-form-urlencoded; charset=UTF-8";
 
     logger.debug("Sending data", json);
 
-    fetch(`${url}?clientId=${clientId}`, {
-      method: "POST",
-      headers: {
-        // Important note: The Content-Type should be `application/x-www-form-urlencoded`,
-        // because that mimetype is CORS-safelisted as documented here:
-        // https://fetch.spec.whatwg.org/#cors-safelisted-request-header
-        // If set to a non-safe mimetype (eg application/json), fetch will send a pre-flight CORS request (OPTIONS)
-        // to the server, and the server is then responsible for responding with CORS "access-control-allow-*" headers.
-        // That's okay, but it adds unnecessary overhead to both the client and the server.
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-      },
-      body: json,
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`Network response ${response.status}`);
-        }
-      })
-      .catch((error) => {
-        logger.error("Error sending data", error);
+    if (typeof navigator !== "undefined" && navigator.sendBeacon) {
+      const blob = new Blob([json], {
+        type: ctypeHeader,
       });
+
+      const success = navigator.sendBeacon(urlWithAuth, blob);
+      if (!success) {
+        logger.error("Error sending data");
+      }
+    } else {
+      fetch(urlWithAuth, {
+        mode: "no-cors",
+        method: "POST",
+        headers: {
+          "Content-Type": ctypeHeader,
+        },
+        body: json,
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`Network response ${response.status}`);
+          }
+        })
+        .catch((error) => {
+          logger.error("Error sending data", error);
+        });
+    }
   } catch (e) {
     logger.error(e);
     return;
