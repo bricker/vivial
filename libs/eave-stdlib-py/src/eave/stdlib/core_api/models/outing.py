1f4e5460105ad25ad2f7cd4db58433fce24bf232
niehusst <liamniehusstaab@gmail.com>
2024-10-15 13:22:53 -0700

    chore: rm unnecessary models

--- libs/eave-stdlib-py/src/eave/stdlib/core_api/models/outing.py
+++
@@ -1,9 +0,0 @@
-import uuid
-from eave.stdlib.core_api.models import BaseResponseModel
-
-
-class Outing(BaseResponseModel):
-    id: uuid.UUID
-    visitor_id: str
-    account_id: uuid.UUID | None
-    survey_id: uuid.UUID
