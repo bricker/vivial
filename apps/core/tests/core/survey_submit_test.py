1c48e143ae4e0954141f5cedd8ee17502c268e66
niehusst <liamniehusstaab@gmail.com>
2024-10-16 14:24:05 -0700

    test: survey mutation

---
+++ apps/core/tests/core/survey_submit_test.py
@@ -0,0 +1,21 @@
+from http import HTTPStatus
+
+from .base import BaseTestCase
+
+
+class TestSurveySubmissionEndpoint(BaseTestCase):
+    async def test_survey_submit(self) -> None:
+        response = await self.httpclient.post(
+            "/graphql",
+            json={
+                "query": """
+mutation {
+    submitSurveyForPlan(visitorId: "abc124", startTimeIso: "2024-10-16T21:14:41", searchAreaIds: ["us_ca_la"], budget: 1, headcount: 2) {
+        id
+    }
+}
+"""
+            },
+        )
+        assert response.status_code == HTTPStatus.OK
+        assert response.json().get("data").get("submitSurveyForPlan").get("id") is not None
