ca28260f6bef729a21501820122338ca42dd0707
Bryan Ricker <git@bryanricker.com>
2024-10-17 12:21:53 -0700

    move LA geo data into separate file

--- apps/core/eave/core/curator/eventbrite_scraper.py
+++ apps/core/eave/core/curator/eventbrite_scraper.py
@@ -7,25 +7,9 @@ from eave.stdlib.eventbrite.models.event import EventStatus
 from eave.stdlib.eventbrite.models.expansions import Expansion
 from eave.stdlib.eventbrite.models.venue import Venue
 
-from dataclasses import dataclass
+from eave.core.curator.areas.geo_area import GeoArea
+from eave.core.curator.areas.los_angeles import LOS_ANGELES_AREAS
 
-
-@dataclass
-class GeoArea:
-    name: str
-    key: str
-    lat: float
-    lon: float
-    rad: float
-
-LA_AREAS = [
-    GeoArea(name="Central LA & Hollywood", key="us_ca_la_1", lat=34.065730, lon=-118.323769, rad=5.78),
-    GeoArea(name="Downtown Los Angeles", key="us_ca_la_2", lat=34.046422, lon=-118.245325, rad=1.69),
-    GeoArea(name="Pasadena, Glendale, & Northeast LA", key="us_ca_la_3", lat=34.160040, lon=-118.209821, rad=6.49),
-    GeoArea(name="Westside", key="us_ca_la_4", lat=33.965090, lon=-118.557344, rad=10.55),
-    GeoArea(name="South Bay", key="us_ca_la_5", lat=33.856750, lon=-118.354487, rad=9.70),
-    GeoArea(name="San Gabriel Valley", key="us_ca_la_6", lat=34.116746, lon=-118.016725, rad=8.46),
-]
 
 async def get_eventbrite_events() -> None:
     client = EventbriteClient(api_key=os.environ["EVENTBRITE_API_KEY"])
@@ -75,7 +59,7 @@ def find_area_id(venue: Venue) -> GeoArea | None:
     min_distance = math.inf
     closest_area: GeoArea | None = None
 
-    for area in LA_AREAS:
+    for area in LOS_ANGELES_AREAS:
         distance = haversine_distance(float(event_lat), float(event_lon), area.lat, area.lon)
         if distance < min_distance:
             min_distance = distance
