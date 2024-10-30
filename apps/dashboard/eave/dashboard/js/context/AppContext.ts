42098b41cb919fac5ebeeec541e0cbaf10f0ad55
niehusst <liamniehusstaab@gmail.com>
2024-10-30 13:26:49 -0700

    feat: replan_outing networking

--- apps/dashboard/eave/dashboard/js/context/AppContext.ts
+++ apps/dashboard/eave/dashboard/js/context/AppContext.ts
@@ -1,6 +1,7 @@
 import { createContext } from "react";
-import { SurveySubmitCtx } from "../graphql/survey.js";
+import { SurveySubmitCtx } from "../graphql/survey";
+import { ReplanOutingCtx } from "../graphql/outing";
 
-export type AppContextProps = SurveySubmitCtx; // & union other types
+export type AppContextProps = SurveySubmitCtx & ReplanOutingCtx;
 
 export const AppContext = createContext<AppContextProps>({});
