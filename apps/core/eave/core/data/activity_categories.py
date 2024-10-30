3945a8f7ea9f775116ccda61f357b0ac47de58ea
Bryan Ricker <git@bryanricker.com>
2024-10-30 16:23:56 -0700

    Wed Oct 30 04:23:56 PM PDT 2024

--- apps/core/eave/core/data/activity_categories.py
+++ apps/core/eave/core/data/activity_categories.py
@@ -1,13 +1,11 @@
-from dataclasses import dataclass
-import json
 import csv
-from uuid import UUID, uuid4
+from dataclasses import dataclass
+from uuid import uuid4
 
 @dataclass(kw_only=True)
 class ActivityCategory:
     id: str
     name: str
-    eventbrite_category_id: str | None
 
 @dataclass(kw_only=True)
 class ActivitySubcategory:
@@ -16,37 +14,65 @@ class ActivitySubcategory:
     is_default: bool
     is_manually_curated: bool
     category_id: str
-    eventbrite_subcategory_id: str | None
+    eventbrite_subcategory_ids: list[str]
 
-CATEGORIES: list[ActivityCategory] = []
-SUBCATEGORIES: list[ActivitySubcategory] = []
+ACTIVITY_CATEGORIES: list[ActivityCategory] = []
 
-with open("activity_categories.csv", newline="") as f:
+ACTIVITY_SUBCATEGORIES: list[ActivitySubcategory] = []
+
+
+
+with open("act_cats.csv", newline="") as f:
     rowiter = csv.DictReader(f)
     for row in rowiter:
-        category = next((c for c in CATEGORIES if c.name == row["Vivial Category"]), None)
+        category = next((c for c in ACTIVITY_CATEGORIES if c.name == row["Vivial Category"]), None)
 
         if category is None:
             category = ActivityCategory(
                 id=uuid4().hex,
                 name=row["Vivial Category"],
-                eventbrite_category_id=row["EB Category ID"],
             )
-            CATEGORIES.append(category)
+            ACTIVITY_CATEGORIES.append(category)
 
-        SUBCATEGORIES.append(
-            ActivitySubcategory(
+        subcategory = next((c for c in ACTIVITY_SUBCATEGORIES if c.name == row["Vivial Subcategory"] and c.category_id == category.id), None)
+
+        if subcategory is None:
+            subcategory = ActivitySubcategory(
                 id=uuid4().hex,
                 name=row["Vivial Subcategory"],
                 is_default=row["Default?"] == "Yes",
                 is_manually_curated=row["In Vivial Curated DB?"] == "TRUE",
                 category_id=category.id,
-                eventbrite_subcategory_id=row["EB Subcategory ID"],
+                eventbrite_subcategory_ids=[],
             )
-        )
+            ACTIVITY_SUBCATEGORIES.append(subcategory)
 
-with open("activity_categories.json", "w") as f:
-    json.dump([c.__dict__ for c in CATEGORIES], f, indent=2, sort_keys=True)
+        ebscid = row["EB Subcategory ID"]
+        if ebscid and ebscid not in subcategory.eventbrite_subcategory_ids:
+            subcategory.eventbrite_subcategory_ids.append(ebscid)
 
-with open("activity_subcategories.json", "w") as f:
-    json.dump([c.__dict__ for c in SUBCATEGORIES], f, indent=2, sort_keys=True)
+
+with open("activity_category_data.py", "w") as f:
+    f.write("ACTIVITY_CATEGORIES = [\n")
+    for cat in ACTIVITY_CATEGORIES:
+        f.writelines([
+            "    ActivityCategory(\n",
+            f"""        id="{cat.id}",\n""",
+            f"""        name="{cat.name}",\n""",
+            "    ),\n"
+        ])
+    f.write("]\n\n")
+
+    f.write("ACTIVITY_SUBCATEGORIES = [\n")
+    for subcat in ACTIVITY_SUBCATEGORIES:
+        f.writelines([
+            "    ActivitySubcategory(\n",
+            f"""        id="{subcat.id}",\n""",
+            f"""        name="{subcat.name}",\n""",
+            f"""        is_default={subcat.is_default},\n""",
+            f"""        is_manually_curated={subcat.is_manually_curated},\n""",
+            f"""        category_id="{subcat.category_id}",\n""",
+            f"""        eventbrite_subcategory_ids={subcat.eventbrite_subcategory_ids},\n""",
+            "    ),\n"
+        ])
+    f.write("]\n")
