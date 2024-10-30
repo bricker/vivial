16ae422c3996f56af47e4179ffb19ae2a3e8ddf1
Bryan Ricker <git@bryanricker.com>
2024-10-30 16:44:47 -0700

    Wed Oct 30 04:44:47 PM PDT 2024

--- apps/core/eave/core/outing/models/category.py
+++ apps/core/eave/core/outing/models/category.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+
+
 class Category:
     id: str
     subcategory_id: str | None
@@ -5,3 +8,24 @@ class Category:
     def __init__(self, id: str, subcategory_id: str | None = None) -> None:
         self.id = id
         self.subcategory_id = subcategory_id
+
+@dataclass(kw_only=True)
+class ActivityCategory:
+    id: str
+    name: str
+
+@dataclass(kw_only=True)
+class ActivitySubcategory:
+    id: str
+    name: str
+    is_default: bool
+    is_manually_curated: bool
+    category_id: str
+    eventbrite_subcategory_ids: list[str]
+
+@dataclass(kw_only=True)
+class RestaurantCategory:
+    id: str
+    name: str
+    is_default: bool
+    google_category_ids: list[str]
