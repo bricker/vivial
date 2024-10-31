4671f240fc9e0d6784d891944f502117a8cd91ff
Leilenah Mamea <leilenah.mamea@gmail.com>
2024-10-31 16:40:51 -0700

    Switch to official Google Place API Client 🤡 (#475)
    Ticket link:
    <!-- here -->
    
    Did you run?:
    - [ ] unit tests
    - [x] lint

--- libs/eave-stdlib-py/src/eave/stdlib/google/places/models/sub_destination.py
+++
@@ -1,8 +0,0 @@
-from typing import TypedDict
-
-
-class SubDestination(TypedDict, total=False):
-    """https://developers.google.com/maps/documentation/places/web-service/reference/rest/v1/places#SubDestination"""
-
-    name: str
-    id: str
