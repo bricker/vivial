7157838bf9319a4816e39421774df8448e42fdc0
niehusst <liamniehusstaab@gmail.com>
2024-11-01 15:46:47 -0700

    chore: lint

--- apps/dashboard/graphql.config.ts
+++ apps/dashboard/graphql.config.ts
@@ -2,7 +2,7 @@ import type { IGraphQLConfig } from "graphql-config";
 
 const config: IGraphQLConfig = {
   // current gql schema specified by the core API (must be running core api + http-proxy locally!)
-  schema: "https://api.eave.run:8080/graphql",
+  schema: "http://api.eave.run:8080/graphql",
   documents: ["./eave/dashboard/js/graphql/**/*.graphql"],
 };
 
