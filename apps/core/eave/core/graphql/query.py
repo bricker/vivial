9a7893c81c04dfab63ad8594ac87603130e3deff
leilenah <leilenah.mamea@gmail.com>
2024-11-05 13:50:39 -0800

    Test graphql query

--- apps/core/eave/core/graphql/query.py
+++ apps/core/eave/core/graphql/query.py
@@ -4,6 +4,21 @@ from eave.core.graphql.resolvers.viewer import viewer_query
 from eave.core.graphql.types.authentication import Account
 
 
+
+
+
+@strawberry.type
+class Dummy:
+    doggo: str
+
+
+async def dummy_query() -> Dummy:
+    return Dummy(
+        doggo="Suzanne"
+    )
+
+
 @strawberry.type
 class Query:
     viewer: Account = strawberry.field(resolver=viewer_query)
+    dummy: Dummy = strawberry.field(resolver=dummy_query)
