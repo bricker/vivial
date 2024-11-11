a549a140e563692fcac24a2568a784d9b8cccfe1
leilenah <leilenah.mamea@gmail.com>
2024-11-11 11:14:14 -0800

    Start new theme + add Vivial colors.

---
+++ apps/dashboard/eave/dashboard/js/theme/colors.ts
@@ -0,0 +1,23 @@
+/**
+ * These colors map to the colors in the Vivial Figma Design files.
+ * https://www.figma.com/design/lehDvAyzQZLEsWADXrtM4m/Final-Designs?node-id=0-1&t=4o5pYaazPBOC0sIy-1
+ */
+export const colors = {
+  whiteText: "#D9D9D9",
+  vivialYellow: "#E6F025",
+  fieldBackground: "#312B2C",
+  secondaryButtonCTA: "#434343",
+  lightPurpleAccent: "#CCACEC",
+  lightOrangeAccent: "#FCC3A3",
+  lightPinkAccent: "#FF81B5",
+  brightPinkAccent: "#D2286E",
+  darkPurpleAccent: "#6D208C",
+  mediumPurpleAccent: "#AF83FD",
+  brightOrangeAccent: "#D1591B",
+  midGreySecondaryField: "#ABABAB",
+  pureWhite: "#FFFFFF",
+  pureBlack: "#000000",
+  errorRed: "#FF3B3B",
+  passingGreen: "#54FF68",
+  almostBlackBG: "#1E1E1E",
+}
---
+++ apps/dashboard/eave/dashboard/js/theme/index.ts
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material";
+
+import { colors } from "./colors.js";
+
+/**
+ * MaterialUI Custom Theme Overrides.
+ * Default Theme: https://mui.com/material-ui/customization/default-theme/
+ */
+export const theme = createTheme({
+});
