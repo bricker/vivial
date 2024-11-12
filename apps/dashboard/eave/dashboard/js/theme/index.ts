55e63c5d4c8da35f67e0b468f166ae3a98e58bb1
Leilenah Mamea <leilenah@eave.fyi>
2024-11-11 17:33:45 -0800

    Set up global page structure. (#487)
    Ticket link: https://eave-fyi.atlassian.net/browse/VIV-121
    <!-- here -->
    
    Did you run?:
    - [ ] unit tests
    - [x] lint

--- apps/dashboard/eave/dashboard/js/theme/index.ts
+++ apps/dashboard/eave/dashboard/js/theme/index.ts
@@ -1,7 +1,16 @@
 import { createTheme } from "@mui/material";
+import { colors } from "./colors";
 
 /**
  * MaterialUI Custom Theme Overrides.
  * Default Theme: https://mui.com/material-ui/customization/default-theme/
  */
-export const theme = createTheme({});
+export const theme = createTheme({
+  palette: {
+    mode: "dark",
+    background: {
+      paper: colors.almostBlackBG,
+      default: colors.pureBlack,
+    },
+  },
+});
