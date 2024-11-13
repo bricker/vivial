aa94d84f15950924eef5e5d47a3c720aaaef24e8
niehusst <liamniehusstaab@gmail.com>
2024-11-12 17:16:27 -0800

    feat: create sendgrid client

---
+++ libs/eave-stdlib-py/src/eave/stdlib/mail.py
@@ -0,0 +1,28 @@
+from eave.stdlib.config import SHARED_CONFIG
+from eave.stdlib.logging import LOGGER
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail, Email, To, Content
+
+
+class SendgridMailer:
+    def __init__(self, api_key: str) -> None:
+        self.client = SendGridAPIClient(api_key=api_key)
+
+    def send_email(self, to_emails: list[str], subject: str, html_content: str) -> None:
+        """
+        Send an email with Sendgrid
+        https://github.com/sendgrid/sendgrid-python
+        """
+        message = Mail(
+            from_email=Email(email="friends@vivialapp.com", name="Friends @ Vivial"),
+            to_emails=[To(email=email) for email in to_emails],
+            subject=subject,
+            html_content=Content(mime_type="text/html", content=html_content),
+        )
+        try:
+            self.client.send(message=message)
+        except Exception as e:
+            LOGGER.exception(e)
+
+
+MAILER = SendgridMailer(api_key=SHARED_CONFIG.send_grid_api_key)
