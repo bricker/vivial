b1e78a021c94b85f3107196246f4f9e429bb8366
niehusst <liamniehusstaab@gmail.com>
2024-11-15 17:10:25 -0800

    feat: booking confirm email as reference

--- libs/eave-stdlib-py/src/eave/stdlib/mail.py
+++ libs/eave-stdlib-py/src/eave/stdlib/mail.py
@@ -1,8 +1,23 @@
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Content, Email, Mail, To
-
+import dataclasses
 from eave.stdlib.config import SHARED_CONFIG
 from eave.stdlib.logging import LOGGER
+
+
+@dataclasses.dataclass(kw_only=True)
+class EventItem:
+    time: str
+    """time of day event starts. Should be localized to recipient's timezone (e.g. 6:00pm)"""
+    name: str
+
+
+@dataclasses.dataclass(kw_only=True)
+class BookingConfirmationData:
+    booking_date: str
+    """date the outing will occur on (e.g. January 19, 2024)"""
+    activities: list[EventItem]
+    restaurants: list[EventItem]
 
 
 class SendgridMailer:
@@ -26,9 +41,7 @@ class SendgridMailer:
         except Exception as e:
             LOGGER.exception(e)
 
-    def _send_templated_email(
-        self, template_id: str, subject: str, to_emails: list[str], dynamic_data: dict[str, str]
-    ) -> None:
+    def _send_templated_email(self, template_id: str, to_emails: list[str], dynamic_data: dict[str, str]) -> None:
         """
         Send an email that is defined by a dynamic template in the sendgrid dashboard.
         Template IDs and the expected dynamic data for the target template
@@ -38,7 +51,6 @@ class SendgridMailer:
         message = Mail(
             from_email=self.from_email,
             to_emails=[To(email=email) for email in to_emails],
-            subject=subject,
         )
         message.template_id = template_id
         message.dynamic_template_data = dynamic_data
@@ -50,9 +62,15 @@ class SendgridMailer:
     def send_welcome_email(self, to_emails: list[str]) -> None:
         self._send_templated_email(
             to_emails=to_emails,
-            subject="Welcome to Vivial!",
             template_id="d-638ba190b929408aa71a92771a85d817",
             dynamic_data={},
+        )
+
+    def send_booking_confirmation_email(self, to_emails: list[str], data: BookingConfirmationData) -> None:
+        self._send_templated_email(
+            to_emails=to_emails,
+            template_id="d-28726a7952a641408bd7946e2795e54f",
+            dynamic_data=dataclasses.asdict(data),
         )
 
 
