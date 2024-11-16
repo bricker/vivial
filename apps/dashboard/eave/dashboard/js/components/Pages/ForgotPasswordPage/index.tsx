77c94627b9bc5c5190b044a5bf7d59b9aea64dea
Leilenah Mamea <leilenah@eave.fyi>
2024-11-15 18:22:27 -0800

    Forgot password UI (#499)
    Ticket link:
    <!-- here -->
    
    Did you run?:
    - [ ] unit tests
    - [x] lint

--- apps/dashboard/eave/dashboard/js/components/Pages/ForgotPasswordPage/index.tsx
+++ apps/dashboard/eave/dashboard/js/components/Pages/ForgotPasswordPage/index.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { rem } from "$eave-dashboard/js/util/rem";
+import { styled } from "@mui/material";
+import React, { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+
+import Typography from "@mui/material/Typography";
+import BackButton from "../../Buttons/BackButton";
+import ExternalLink from "../../Links/ExternalLink";
+
+const Container = styled("div")(() => ({
+  padding: "24px 16px",
+}));
+
+const CopyContainer = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+  borderRadius: 15,
+  padding: "32px 40px",
+}));
+
+const Subtitle = styled(Typography)(() => ({
+  fontSize: rem("18px"),
+  lineHeight: rem("22px"),
+  marginTop: 16,
+}));
 
 const ForgotPasswordPage = () => {
-  return <div>FORGOT PASSWORD PAGE</div>;
+  const navigate = useNavigate();
+  const handleBack = useCallback(() => {
+    navigate("/login");
+  }, []);
+  return (
+    <Container>
+      <BackButton onClick={handleBack} />
+      <CopyContainer>
+        <Typography variant="h3">Forgot password?</Typography>
+        <Subtitle>
+          Reach out to us at <ExternalLink to="mailto:friends@vivialapp.com">friends@vivialapp.com</ExternalLink> and
+          we’ll help you reset your password.
+        </Subtitle>
+      </CopyContainer>
+    </Container>
+  );
 };
 
 export default ForgotPasswordPage;
