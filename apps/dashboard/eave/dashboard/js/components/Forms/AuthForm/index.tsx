fcac02857803de2565f64e05b3243f535ed5eef5
leilenah <leilenah.mamea@gmail.com>
2024-11-19 12:15:12 -0800

    Sign up UI

--- apps/dashboard/eave/dashboard/js/components/Forms/AuthForm/index.tsx
+++ apps/dashboard/eave/dashboard/js/components/Forms/AuthForm/index.tsx
@@ -1,17 +1,25 @@
+import { getPasswordInfo, passwordIsValid } from "$eave-dashboard/js/util/password";
 import { rem } from "$eave-dashboard/js/util/rem";
 import { styled } from "@mui/material";
 import Typography from "@mui/material/Typography";
+import * as EmailValidator from "email-validator";
 import React, { useCallback, useState } from "react";
 
 import Button from "../../Buttons/Button";
 import Input from "../../Inputs/Input";
+import Error from "../../Inputs/InputError";
+import Requirement from "../../Inputs/InputRequirement";
 import SensitiveInput from "../../Inputs/SensitiveInput";
 import Link from "../../Links/Link";
 
 const FormContainer = styled("div")(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
   borderRadius: 15,
-  padding: "32px 40px",
+  padding: "32px 0",
+}));
+
+const FormContent = styled("div")(() => ({
+  padding: "0 40px",
 }));
 
 const TitleContainer = styled("div")(() => ({
@@ -26,12 +34,8 @@ const EmailInput = styled(Input)(() => ({
   marginBottom: 16,
 }));
 
-const PasswordInput = styled(SensitiveInput)(() => ({
-  marginBottom: 24,
-}));
-
 const AuthButton = styled(Button)(() => ({
-  marginBottom: 24,
+  margin: "24px 0",
 }));
 
 const ForgotPassword = styled("div")(() => ({
@@ -47,11 +51,30 @@ const Legal = styled("p")(({ theme }) => ({
   lineHeight: rem("18px"),
 }));
 
+const ErrorContainer = styled("div")(() => ({
+  fontSize: rem("12px"),
+  lineHeight: rem("16px"),
+  display: "flex",
+  alignItems: "center",
+  marginTop: 10,
+  padding: "0 40px 0 56px",
+}));
+
+const RequirementsContainer = styled("div")(() => ({
+  fontSize: rem("12px"),
+  lineHeight: rem("16px"),
+  marginTop: 10,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
 interface AuthFormProps {
   title: string;
   cta: string;
   onSubmit: (email: string, password: string) => void;
   subtitle?: string;
+  error?: string;
   validateEmail?: boolean;
   validatePassword?: boolean;
   showForgotPassword?: boolean;
@@ -63,41 +86,109 @@ const AuthForm = ({
   cta,
   onSubmit,
   subtitle = "",
-  // validateEmail = false,
-  // validatePassword = false,
+  error = "",
+  validateEmail = false,
+  validatePassword = false,
   showForgotPassword = false,
   showLegal = false,
 }: AuthFormProps) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const disabled = !email || !password;
+  const [errorMessage, setErrorMessage] = useState(error);
+  const [submitEnabled, setSubmitEnabled] = useState(false);
+  const [showPasswordInfo, setShowPasswordInfo] = useState(false);
+  const [passwordInfo, setPasswordInfo] = useState({
+    hasEightChars: false,
+    hasSpecialChar: false,
+    hasLetter: false,
+    hasDigit: false,
+  });
 
   const handleSubmit = useCallback(() => {
+    if (validateEmail && !EmailValidator.validate(email)) {
+      setErrorMessage("Invalid email address");
+      setShowPasswordInfo(false);
+      setSubmitEnabled(false);
+      return;
+    }
     onSubmit(email, password);
   }, [email, password]);
 
+  const checkInputs = ({ currentEmail, currentPassword }: { currentEmail: string; currentPassword: string }) => {
+    setErrorMessage("");
+    if (validatePassword && currentPassword) {
+      const newPasswordInfo = getPasswordInfo(currentPassword);
+      setPasswordInfo(newPasswordInfo);
+      setShowPasswordInfo(true);
+      if (currentEmail && passwordIsValid(newPasswordInfo)) {
+        setSubmitEnabled(true);
+      } else {
+        setSubmitEnabled(false);
+      }
+    } else if (currentEmail && currentPassword) {
+      setSubmitEnabled(true);
+    } else {
+      setSubmitEnabled(false);
+    }
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newEmail = e.target.value;
+    setEmail(newEmail);
+    checkInputs({ currentEmail: newEmail, currentPassword: password });
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newPassword = e.target.value;
+    setPassword(newPassword);
+    checkInputs({ currentPassword: newPassword, currentEmail: email });
+  };
+
   return (
     <FormContainer>
-      <TitleContainer>
-        <Typography variant="h2">{title}</Typography>
-        {subtitle && <Subtitle variant="subtitle2">{subtitle}</Subtitle>}
-      </TitleContainer>
-      <EmailInput placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-      <PasswordInput placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-      <AuthButton onClick={handleSubmit} disabled={disabled} fullWidth>
-        {cta}
-      </AuthButton>
-      {showForgotPassword && (
-        <ForgotPassword>
-          <Link to="/login/password">Forgot password?</Link>
-        </ForgotPassword>
+      <FormContent>
+        <TitleContainer>
+          <Typography variant="h2">{title}</Typography>
+          {subtitle && <Subtitle variant="subtitle2">{subtitle}</Subtitle>}
+        </TitleContainer>
+        <EmailInput placeholder="Email" onChange={handleEmailChange} />
+        <SensitiveInput placeholder="Password" onChange={handlePasswordChange} />
+      </FormContent>
+      {errorMessage && (
+        <ErrorContainer>
+          <Error>{errorMessage}</Error>
+        </ErrorContainer>
       )}
-      {showLegal && (
-        <Legal>
-          By clicking “{cta}” above, you are agreeing to Vivial’s <Link to="/terms">TOS</Link> and{" "}
-          <Link to="/privacy">Privacy Policy</Link>.
-        </Legal>
+      {showPasswordInfo && (
+        <RequirementsContainer>
+          <Requirement met={passwordInfo.hasEightChars}>8 characters</Requirement>
+          <Requirement met={passwordInfo.hasLetter && passwordInfo.hasDigit}>1 letter and 1 digit</Requirement>
+          <Requirement met={passwordInfo.hasSpecialChar}>1 special character</Requirement>
+        </RequirementsContainer>
       )}
+      <FormContent>
+        <AuthButton onClick={handleSubmit} disabled={!submitEnabled} fullWidth>
+          {cta}
+        </AuthButton>
+        {showForgotPassword && (
+          <ForgotPassword>
+            <Link to="/login/password">Forgot password?</Link>
+          </ForgotPassword>
+        )}
+        {showLegal && (
+          <Legal>
+            By clicking “{cta}” above, you are agreeing to Vivial’s{" "}
+            <Link to="/terms" underline>
+              TOS
+            </Link>{" "}
+            and{" "}
+            <Link to="/privacy" underline>
+              Privacy Policy
+            </Link>
+            .
+          </Legal>
+        )}
+      </FormContent>
     </FormContainer>
   );
 };
