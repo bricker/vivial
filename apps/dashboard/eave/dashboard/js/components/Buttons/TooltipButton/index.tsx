d9c2255430c102b079570635b4e79034ecf3e00a
leilenah <leilenah.mamea@gmail.com>
2024-12-01 16:52:09 -0800

    date picker progress

---
+++ apps/dashboard/eave/dashboard/js/components/Buttons/TooltipButton/index.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { rem } from "$eave-dashboard/js/theme/helpers/rem";
+import { colors } from "$eave-dashboard/js/theme/colors";
+import { styled } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import TooltipIcon from "../../Icons/TooltipIcon";
+
+interface TooltipButtonProps {
+  info: string;
+  iconColor?: string;
+}
+
+const Button = styled(IconButton)(() => ({
+  padding: 0,
+}));
+
+const tooltipBorderStyle = "1px solid #4A4A4A"; // one-off color
+const tooltipBgStyle = "#131313"; // one-off color
+const tooltipStyles = {
+  color: colors.whiteText,
+  padding: "24px",
+  borderRadius: "8px",
+  maxWidth: 260,
+  fontSize: rem("14px"),
+  lineHeight: rem("16px"),
+  backgroundColor: tooltipBgStyle,
+  border: tooltipBorderStyle,
+};
+const arrowStyles = {
+  color: tooltipBgStyle,
+  "&:before": {
+    border: tooltipBorderStyle,
+  },
+};
+
+const TooltipButton = ({ info, iconColor }: TooltipButtonProps) => {
+  return (
+    <Tooltip
+      slotProps={{ tooltip: { sx: tooltipStyles }, arrow: { sx: arrowStyles }}}
+      title={info}
+      arrow
+    >
+      <Button>
+        <TooltipIcon color={iconColor} />
+      </Button>
+    </Tooltip>
+  );
+}
+
+export default TooltipButton;
--- apps/dashboard/eave/dashboard/js/components/Selections/DateTimeSelections/index.tsx
+++ apps/dashboard/eave/dashboard/js/components/Selections/DateTimeSelections/index.tsx
@@ -1,5 +1,10 @@
 import React, { useState, useCallback } from "react";
-import { DateCalendar, TimePicker } from '@mui/x-date-pickers';
+import dayjs from "dayjs";
+import { rem } from "$eave-dashboard/js/theme/helpers/rem";
+import { styled  } from "@mui/material";
+import { DateCalendar as BaseDatePicker, TimePicker as BaseTimePicker } from '@mui/x-date-pickers';
+import Typography from "@mui/material/Typography";
+import TooltipButton from "../../Buttons/TooltipButton";
 
 interface DateTimeSelectionsProps {
   cta: string;
@@ -7,18 +12,83 @@ interface DateTimeSelectionsProps {
   startTime: Date;
 };
 
+const DateTimeContainer = styled("div")(() => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  width: "100%",
+}));
+
+const TimeRow = styled("div")(() => ({
+  display: "flex",
+  width: 320, // width of MUI calendar
+  alignItems: "center",
+  paddingBottom: 16,
+  borderBottom: "1.08px solid #595959", // one-off color
+}));
+
+const TimeTitle = styled(Typography)(() => ({
+  fontWeight: 500,
+  margin: "0 16px 0 4px",
+}));
+
+const TimePicker = styled(BaseTimePicker)(({ theme }) => ({
+  maxWidth: 124,
+  "& .MuiInputBase-root": {
+    paddingRight: 0,
+  },
+  "& .MuiOutlinedInput-notchedOutline": {
+    border: "none",
+  },
+  "& .MuiInputBase-input": {
+    color: theme.palette.common.white,
+    border: `1px solid ${theme.palette.accent[2]}`,
+    borderRadius: 100,
+    fontSize: rem("16px"),
+    lineHeight: rem("19px"),
+    fontWeight: 500,
+    padding: "10px 24px",
+  }
+}));
+
+const DatePicker = styled(BaseDatePicker)(() => ({
+  margin: 0,
+}));
+
 const DateTimeSelections = ({ cta, startTime, onSubmit}: DateTimeSelectionsProps) => {
-  const [selectedTime, setSelectedTime] = useState(startTime);
+  // split out time
+  const [selectedTime, setSelectedTime] = useState(dayjs(startTime));
+  const minTime = dayjs().hour(6).minute(0); // 6:00 AM
+  const maxTime = dayjs().hour(20).minute(30); // 8:30 PM
 
   const handleSumbit = useCallback(() => {
-    onSubmit(selectedTime);
+    // TODO: Combine time and date into one Date object
+    // TODO: Convert from days js to Date obj
   }, []);
 
   return (
-    <>
-      <TimePicker />
-      <DateCalendar />
-    </>
+    <DateTimeContainer>
+      <TimeRow>
+        <TooltipButton info="Recommended plans will be within roughly a 4 hour window from selected start time." />
+        <TimeTitle>Date Start Time:</TimeTitle>
+        {/* <TimePicker
+          slotProps={{
+            field: { shouldRespectLeadingZeros: true },
+            digitalClockSectionItem: { sx: { backgroundColor: "red" }}
+          }}
+
+          // thresholdToRenderTimeInASingleColumn={500}
+          defaultValue={selectedTime}
+          minTime={minTime}
+          maxTime={maxTime}
+          format="LT"
+          timeSteps={{ minutes: 30 }}
+          thresholdToRenderTimeInASingleColumn={500}
+          skipDisabled
+        /> */}
+      </TimeRow>
+      <DatePicker />
+    </DateTimeContainer>
   );
 };
 
