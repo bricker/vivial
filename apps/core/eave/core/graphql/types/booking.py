f60ca9afa58db7450e5131caf96a5ca4e7d1cc17
niehusst <liamniehusstaab@gmail.com>
2024-12-05 15:08:10 -0800

    chore: remove unneeded field from gql type

--- apps/core/eave/core/graphql/types/booking.py
+++ apps/core/eave/core/graphql/types/booking.py
@@ -10,14 +10,12 @@ from eave.core.orm.booking import BookingOrm
 @strawberry.type
 class Booking:
     id: UUID
-    account_id: UUID
     reserver_details_id: UUID
 
     @classmethod
     def from_orm(cls, orm: BookingOrm) -> "Booking":
         return Booking(
             id=orm.id,
-            account_id=orm.account_id,
             reserver_details_id=orm.reserver_details_id,
         )
 
