da3254533005b380ab797223f332d301fd560dda
niehusst <liamniehusstaab@gmail.com>
2024-12-11 12:36:37 -0800

    refactor: hoist edit state so we know when to use new/existing card data

--- apps/dashboard/eave/dashboard/js/components/CheckoutReservation/PaymentForm/index.tsx
+++ apps/dashboard/eave/dashboard/js/components/CheckoutReservation/PaymentForm/index.tsx
@@ -1,7 +1,7 @@
 import { colors } from "$eave-dashboard/js/theme/colors";
 import { Button as MuiButton, Typography, styled } from "@mui/material";
 import { PaymentElement } from "@stripe/react-stripe-js";
-import React, { useState } from "react";
+import React from "react";
 import EditIcon from "../../Icons/EditIcon";
 
 const PaymentContainer = styled("div")(() => ({}));
@@ -26,8 +26,15 @@ const CollapsingContainer = styled("div")<{ collapsed: boolean }>(({ collapsed }
   display: collapsed ? "none" : "auto",
 }));
 
-const PaymentForm = ({ paymentDetails }: { paymentDetails?: string }) => {
-  const [isEditing, setIsEditing] = useState(false);
+const PaymentForm = ({
+  paymentDetails,
+  isUsingNewCard,
+  setIsUsingNewCard,
+}: {
+  isUsingNewCard: boolean;
+  setIsUsingNewCard: (newValue: boolean) => void;
+  paymentDetails?: string;
+}) => {
   // Testing? See here: https://docs.stripe.com/testing#cards
   // TL;DR: Number: 4242 4242 4242 4242; Exp: 10/30; Code: 123; Zip: 12345
   const titleText = `Payment info${paymentDetails ? ":" : "rmation"}`;
@@ -38,15 +45,18 @@ const PaymentForm = ({ paymentDetails }: { paymentDetails?: string }) => {
           {titleText}
           {paymentDetails && <BoldText variant="subtitle2">{paymentDetails}</BoldText>}
         </Typography>
-        {!isEditing && paymentDetails && (
-          <TextButton onClick={() => setIsEditing(true)}>
+        {!isUsingNewCard && paymentDetails && (
+          <TextButton onClick={() => setIsUsingNewCard(true)}>
             Add new
             <EditIcon color={colors.mediumPurpleAccent} />
           </TextButton>
         )}
-        {isEditing && paymentDetails && <TextButton onClick={() => setIsEditing(false)}>Cancel</TextButton>}
+        {isUsingNewCard && paymentDetails && (
+          <TextButton onClick={() => setIsUsingNewCard(false)}>Use existing card</TextButton>
+        )}
       </HeaderContainer>
-      <CollapsingContainer collapsed={!!(paymentDetails && !isEditing)}>
+      <CollapsingContainer collapsed={!!(paymentDetails && !isUsingNewCard)}>
+        {isUsingNewCard && <Typography variant="subtitle2">New payment information</Typography>}
         <PaymentElement options={{}} />
       </CollapsingContainer>
     </PaymentContainer>
--- apps/dashboard/eave/dashboard/js/components/CheckoutReservation/index.tsx
+++ apps/dashboard/eave/dashboard/js/components/CheckoutReservation/index.tsx
@@ -18,7 +18,7 @@ import { storeReserverDetails } from "$eave-dashboard/js/store/slices/reserverDe
 import { rem } from "$eave-dashboard/js/theme/helpers/rem";
 import { styled } from "@mui/material";
 import { useElements, useStripe } from "@stripe/react-stripe-js";
-import React, { useCallback, useEffect, useState } from "react";
+import React, { FormEvent, useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import LoadingButton from "../Buttons/LoadingButton";
@@ -28,14 +28,16 @@ import PaymentForm from "./PaymentForm";
 import ReservationDetailsForm, { ReserverFormFields } from "./ReservationDetailsForm";
 
 const PaddedPrimaryButton = styled(LoadingButton)(() => ({
-  padding: "10px 14px",
-  minWidth: rem("76px"),
+  marginLeft: 16,
+  marginRight: 16,
+  marginBottom: 56,
 }));
 
 const FormContainer = styled("form")(() => ({
   display: "flex",
   flexDirection: "column",
   gap: 16,
+  padding: "24px 16px",
 }));
 
 const InputErrorContainer = styled("div")(() => ({
@@ -48,7 +50,7 @@ const InputErrorContainer = styled("div")(() => ({
   padding: "0 16px",
 }));
 
-function hasPaidActivity(outing?: Outing) {
+function hasPaidActivity(outing: Outing | null) {
   const costData = outing?.activity?.ticketInfo;
   return costData?.cost || costData?.fee || costData?.tax;
 }
@@ -59,11 +61,9 @@ const CheckoutForm = ({ outingId }: { outingId: string }) => {
   const stripeClient = useStripe();
   const stripeElements = useElements();
 
-  // const localOuting = useSelector((state: RootState) => state.)
+  const localOuting = useSelector((state: RootState) => state.outing.details);
   const localReserverDetails = useSelector((state: RootState) => state.reserverDetails.reserverDetails);
 
-  // TODO: load (or is in redux?) outing details we want to book
-  // then: determine which UI to present
   const {
     data: reserverDetailsData,
     isLoading: listDetailsIsLoading,
@@ -82,11 +82,11 @@ const CheckoutForm = ({ outingId }: { outingId: string }) => {
   const [externalReserverDetailError, setExternalReserverDetailError] = useState<string | undefined>(undefined);
   const [paymentError, setPaymentError] = useState<string | undefined>(undefined);
   const [bookingError, setBookingError] = useState<string | undefined>(undefined);
+  const [isUsingNewCard, setIsUsingNewCard] = useState(false);
   const [reserverDetails, setReserverDetails] = useState(
     localReserverDetails || { id: "", firstName: "", lastName: "", phoneNumber: "" },
   );
-  // TODO: fetch from redux
-  const [outing, setOuting] = useState<Outing | undefined>(undefined);
+  const [outing, setOuting] = useState<Outing | null>(localOuting);
 
   const submissionIsLoading = createBookingIsLoading || updateDetailsIsLoading || submitDetailsIsLoading;
   // only prevent submit on internalError since that can be fixed w/o another submit
@@ -165,174 +165,182 @@ const CheckoutForm = ({ outingId }: { outingId: string }) => {
     [reserverDetails],
   );
 
-  const handleSubmit = useCallback(async () => {
-    if (!stripeClient || !stripeElements) {
-      console.warn("stripe not loaded; must wrap in StripeElementsProvider");
-      return;
-    }
-    setInternalReserverDetailError(undefined);
+  const handleSubmit = useCallback(
+    async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (!stripeClient || !stripeElements) {
+        console.warn("stripe not loaded; must wrap in StripeElementsProvider");
+        return;
+      }
+      setInternalReserverDetailError(undefined);
 
-    let bookingDetails = { ...reserverDetails };
-    try {
-      if (outing?.restaurant) {
-        // if the reserver details dont have db ID yet, create a new entry
-        if (reserverDetails.id.length == 0) {
-          const submitDetailsResp = await submitReserverDetails({
-            input: reserverDetails,
-          });
-          switch (submitDetailsResp.data?.viewer.__typename) {
-            case "AuthenticatedViewerMutations": {
-              const createdData = submitDetailsResp.data.viewer.submitReserverDetails;
-              switch (createdData?.__typename) {
-                case "SubmitReserverDetailsSuccess": {
-                  bookingDetails.id = createdData.reserverDetails.id;
-                  dispatch(storeReserverDetails({ details: bookingDetails }));
-                  // allow success case to continue execution
-                  break;
-                }
-                case "SubmitReserverDetailsFailure":
-                  switch (createdData.failureReason) {
-                    case SubmitReserverDetailsFailureReason.ValidationErrors: {
-                      const invalidFields = createdData.validationErrors?.map((e) => e.field).join(", ");
-                      setExternalReserverDetailError(`The following fields are invalid: ${invalidFields}`);
-                      break;
-                    }
-                    default:
-                      console.error("Unhandled case for SubmitReserverDetailsFailure", createdData.failureReason);
-                      break;
+      let bookingDetails = { ...reserverDetails };
+      try {
+        if (outing?.restaurant) {
+          // if the reserver details dont have db ID yet, create a new entry
+          if (reserverDetails.id.length == 0) {
+            const submitDetailsResp = await submitReserverDetails({
+              input: reserverDetails,
+            });
+            switch (submitDetailsResp.data?.viewer.__typename) {
+              case "AuthenticatedViewerMutations": {
+                const createdData = submitDetailsResp.data.viewer.submitReserverDetails;
+                switch (createdData?.__typename) {
+                  case "SubmitReserverDetailsSuccess": {
+                    bookingDetails.id = createdData.reserverDetails.id;
+                    dispatch(storeReserverDetails({ details: bookingDetails }));
+                    // allow success case to continue execution
+                    break;
                   }
-                  return;
-                default:
-                  // 500 error
-                  setExternalReserverDetailError("Unable to book your outing. Please try again later.");
-                  return;
-              }
-              // allow success case to continue execution
-              break;
-            }
-            case "UnauthenticatedViewer":
-              dispatch(loggedOut());
-              window.location.assign(AppRoute.logout);
-              return;
-            default:
-              return;
-          }
-        }
-        // existing db entry needs to be updated
-        else {
-          const updateDetailsResp = await updateReserverDetails({
-            input: reserverDetails,
-          });
-          switch (updateDetailsResp.data?.viewer.__typename) {
-            case "AuthenticatedViewerMutations": {
-              const updatedData = updateDetailsResp.data.viewer.updateReserverDetails;
-              switch (updatedData?.__typename) {
-                case "UpdateReserverDetailsSuccess":
-                  dispatch(storeReserverDetails({ details: updatedData.reserverDetails }));
-                  // allow success case to continue execution
-                  break;
-                case "UpdateReserverDetailsFailure":
-                  switch (updatedData.failureReason) {
-                    case UpdateReserverDetailsFailureReason.ValidationErrors: {
-                      const invalidFields = updatedData.validationErrors?.map((e) => e.field).join(", ");
-                      setExternalReserverDetailError(`The following fields are invalid: ${invalidFields}`);
-                      break;
+                  case "SubmitReserverDetailsFailure":
+                    switch (createdData.failureReason) {
+                      case SubmitReserverDetailsFailureReason.ValidationErrors: {
+                        const invalidFields = createdData.validationErrors?.map((e) => e.field).join(", ");
+                        setExternalReserverDetailError(`The following fields are invalid: ${invalidFields}`);
+                        break;
+                      }
+                      default:
+                        console.error("Unhandled case for SubmitReserverDetailsFailure", createdData.failureReason);
+                        break;
                     }
-                    default:
-                      console.error("Unhandled case for UpdateReserverDetailsFailure", updatedData.failureReason);
-                      break;
-                  }
-                  return;
-                default:
-                  // 500 error
-                  setExternalReserverDetailError("Unable to book your outing. Please try again later.");
-                  return;
-              }
-              // allow success case to continue execution
-              break;
-            }
-            case "UnauthenticatedViewer":
-              dispatch(loggedOut());
-              window.location.assign(AppRoute.logout);
-              return;
-            default:
-              return;
-          }
-        }
-      }
-
-      if (hasPaidActivity(outing)) {
-        const serverHasNoPaymentDetails = false;
-        if (serverHasNoPaymentDetails) {
-          // TODO: create payment details
-        } else {
-          // TODO: update payment details
-        }
-      }
-
-      const createBookingResp = await createBooking({
-        input: {
-          reserverDetailsId: bookingDetails.id,
-          outingId,
-        },
-      });
-      switch (createBookingResp.data?.viewer.__typename) {
-        case "AuthenticatedViewerMutations": {
-          const createdData = createBookingResp.data?.viewer.createBooking;
-          switch (createdData?.__typename) {
-            case "CreateBookingSuccess":
-              navigate(AppRoute.checkoutComplete);
-              // allow success case to continue execution
-              break;
-            case "CreateBookingFailure":
-              switch (createdData.failureReason) {
-                case CreateBookingFailureReason.ValidationErrors: {
-                  // TODO: when would this happen????
-                  const invalidFields = createdData.validationErrors?.map((e) => e.field).join(", ");
-                  setBookingError(`The following fields are invalid: ${invalidFields}`);
-                  break;
+                    return;
+                  default:
+                    // 500 error
+                    setExternalReserverDetailError("Unable to book your outing. Please try again later.");
+                    return;
                 }
-                default:
-                  console.error("Unhandled case for CreateBookingFailure", createdData.failureReason);
-                  break;
+                // allow success case to continue execution
+                break;
               }
-              return;
-            default:
-              // 500 error
-              setBookingError("Unable to book your outing. Please try again later.");
-              return;
+              case "UnauthenticatedViewer":
+                dispatch(loggedOut());
+                window.location.assign(AppRoute.logout);
+                return;
+              default:
+                return;
+            }
           }
-          // allow success case to continue execution
-          break;
+          // existing db entry needs to be updated
+          else {
+            const updateDetailsResp = await updateReserverDetails({
+              input: reserverDetails,
+            });
+            switch (updateDetailsResp.data?.viewer.__typename) {
+              case "AuthenticatedViewerMutations": {
+                const updatedData = updateDetailsResp.data.viewer.updateReserverDetails;
+                switch (updatedData?.__typename) {
+                  case "UpdateReserverDetailsSuccess":
+                    dispatch(storeReserverDetails({ details: updatedData.reserverDetails }));
+                    // allow success case to continue execution
+                    break;
+                  case "UpdateReserverDetailsFailure":
+                    switch (updatedData.failureReason) {
+                      case UpdateReserverDetailsFailureReason.ValidationErrors: {
+                        const invalidFields = updatedData.validationErrors?.map((e) => e.field).join(", ");
+                        setExternalReserverDetailError(`The following fields are invalid: ${invalidFields}`);
+                        break;
+                      }
+                      default:
+                        console.error("Unhandled case for UpdateReserverDetailsFailure", updatedData.failureReason);
+                        break;
+                    }
+                    return;
+                  default:
+                    // 500 error
+                    setExternalReserverDetailError("Unable to book your outing. Please try again later.");
+                    return;
+                }
+                // allow success case to continue execution
+                break;
+              }
+              case "UnauthenticatedViewer":
+                dispatch(loggedOut());
+                window.location.assign(AppRoute.logout);
+                return;
+              default:
+                return;
+            }
+          }
         }
-        case "UnauthenticatedViewer":
-          dispatch(loggedOut());
-          window.location.assign(AppRoute.logout);
-          return;
-        default:
-          return;
-      }
 
-      // execute the payment
-      if (hasPaidActivity(outing)) {
-        const response = await stripeClient.confirmPayment({
-          elements: stripeElements,
-          clientSecret: "", // This property is required but already provided by stripeElements
-          confirmParams: {
-            return_url: `${window.location.origin}${AppRoute.checkoutComplete}`,
+        if (hasPaidActivity(outing)) {
+          const serverHasNoPaymentDetails = false;
+          if (serverHasNoPaymentDetails) {
+            // TODO: create payment details
+          } else {
+            // TODO: update payment details
+          }
+        }
+
+        const createBookingResp = await createBooking({
+          input: {
+            reserverDetailsId: bookingDetails.id,
+            outingId,
           },
         });
-
-        if (response.error) {
-          console.error(response.error);
-          setPaymentError(response.error.message);
+        switch (createBookingResp.data?.viewer.__typename) {
+          case "AuthenticatedViewerMutations": {
+            const createdData = createBookingResp.data?.viewer.createBooking;
+            switch (createdData?.__typename) {
+              case "CreateBookingSuccess":
+                navigate(AppRoute.checkoutComplete);
+                // allow success case to continue execution
+                break;
+              case "CreateBookingFailure":
+                switch (createdData.failureReason) {
+                  case CreateBookingFailureReason.ValidationErrors: {
+                    // TODO: when would this happen????
+                    const invalidFields = createdData.validationErrors?.map((e) => e.field).join(", ");
+                    setBookingError(`The following fields are invalid: ${invalidFields}`);
+                    break;
+                  }
+                  default:
+                    console.error("Unhandled case for CreateBookingFailure", createdData.failureReason);
+                    break;
+                }
+                return;
+              default:
+                // 500 error
+                setBookingError("Unable to book your outing. Please try again later.");
+                return;
+            }
+            // allow success case to continue execution
+            break;
+          }
+          case "UnauthenticatedViewer":
+            dispatch(loggedOut());
+            window.location.assign(AppRoute.logout);
+            return;
+          default:
+            return;
         }
+
+        // execute the payment
+        if (hasPaidActivity(outing)) {
+          // TODO: send w/ existing payment details when not using new card
+          if (isUsingNewCard) {
+            const response = await stripeClient.confirmPayment({
+              elements: stripeElements,
+              clientSecret: "", // This property is required but already provided by stripeElements
+              confirmParams: {
+                return_url: `${window.location.origin}${AppRoute.checkoutComplete}`,
+              },
+            });
+
+            if (response.error) {
+              console.error(response.error);
+              setPaymentError(response.error.message);
+              return;
+            }
+          }
+        }
+      } catch {
+        // network error
+        setInternalReserverDetailError("Unable to book your outing. Please try again later.");
       }
-    } catch {
-      // network error
-      setInternalReserverDetailError("Unable to book your outing. Please try again later.");
-    }
-  }, [reserverDetails, stripeClient, stripeElements]);
+    },
+    [reserverDetails, stripeClient, stripeElements, outing],
+  );
 
   // TODO: render cost header? opentable footer?
   return (
@@ -345,7 +353,13 @@ const CheckoutForm = ({ outingId }: { outingId: string }) => {
       />
 
       {/* TODO pass real payment data */}
-      {hasPaidActivity(outing) && <PaymentForm paymentDetails="Visa *1234" />}
+      {hasPaidActivity(outing) && (
+        <PaymentForm
+          paymentDetails="Visa *1234"
+          isUsingNewCard={isUsingNewCard}
+          setIsUsingNewCard={setIsUsingNewCard}
+        />
+      )}
 
       {error && (
         <InputErrorContainer>
