71bc7460f39bb7de7c3e7811d1b246ece4ef1264
Leilenah Mamea <leilenah@eave.fyi>
2024-12-11 16:34:05 -0800

    Itinerary Page Header (#550)
    Ticket link:
    <!-- here -->
    
    Did you run?:
    - [ ] unit tests
    - [x] lint

--- apps/dashboard/eave/dashboard/js/components/Global/GlobalHeader/Variants/ItineraryVariant/index.tsx
+++ apps/dashboard/eave/dashboard/js/components/Global/GlobalHeader/Variants/ItineraryVariant/index.tsx
@@ -1,12 +1,47 @@
 import VivialLogo from "$eave-dashboard/js/components/Logo";
-import React from "react";
+import { AppRoute } from "$eave-dashboard/js/routes";
+import { RootState } from "$eave-dashboard/js/store";
+
+import { styled } from "@mui/material";
+import React, { useCallback } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import { getTotalCost } from "$eave-dashboard/js/components/Pages/DateItineraryPage/helpers";
+
+import PrimaryButton from "$eave-dashboard/js/components/Buttons/PrimaryButton";
+import Typography from "@mui/material/Typography";
 import Header from "../../Shared/Header";
 
+const BookButton = styled(PrimaryButton)(() => ({
+  height: 35,
+  width: 84,
+}));
+
+const Cost = styled("span")(() => ({
+  fontWeight: 600,
+}));
+
 const ItineraryVariant = () => {
+  const outing = useSelector((state: RootState) => state.outing.details);
+  const cost = getTotalCost(outing);
+  const navigate = useNavigate();
+
+  const handleBook = useCallback(() => {
+    navigate(`${AppRoute.checkout}/${outing?.id}`);
+  }, [outing]);
+
   return (
     <Header>
-      <VivialLogo />
-      <button onClick={() => {}}>Book</button>
+      <VivialLogo hideText />
+      {cost && (
+        <Typography variant="subtitle1">
+          Total: <Cost>{cost}</Cost>
+        </Typography>
+      )}
+      <BookButton onClick={handleBook} disabled={!outing}>
+        Book
+      </BookButton>
     </Header>
   );
 };
