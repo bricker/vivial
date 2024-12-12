a254fbf11ca03e3b32ffa9933d3d23b2a109f010
niehusst <liamniehusstaab@gmail.com>
2024-12-12 13:02:54 -0800

    feat: protect xo route if path param is obviously bad

--- apps/dashboard/eave/dashboard/js/components/Pages/CheckoutReservationPage/index.tsx
+++ apps/dashboard/eave/dashboard/js/components/Pages/CheckoutReservationPage/index.tsx
@@ -1,13 +1,24 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import { AppRoute } from "$eave-dashboard/js/routes";
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import CheckoutReservation from "../../CheckoutReservation";
 
 const CheckoutReservationPage = () => {
+  const navigate = useNavigate();
   const params = useParams();
   const outingId = params["outingId"];
-  // TODO: do something if id is bad
 
-  return <CheckoutReservation outingId={outingId!} showStripeBadge showCostBreakdown />;
+  // validate path param is a UUID
+  useEffect(() => {
+    // regex from https://stackoverflow.com/a/13653180
+    const uuidRegex = new RegExp(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i);
+    const possibleUuid = outingId || "";
+    if (!outingId || !uuidRegex.test(possibleUuid)) {
+      navigate(AppRoute.root);
+    }
+  }, []);
+
+  return <CheckoutReservation outingId={outingId || ""} showStripeBadge showCostBreakdown />;
 };
 
 export default CheckoutReservationPage;
