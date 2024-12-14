04dd02867552f119615d1d1953bde1fe716e6c9a
Leilenah Mamea <leilenah@eave.fyi>
2024-12-13 16:05:33 -0800

    Itinerary - Image Carousel (#561)
    Ticket link:
    <!-- here -->
    
    Did you run?:
    - [ ] unit tests
    - [x] lint

--- apps/dashboard/eave/dashboard/js/components/Pages/DateItineraryPage/Sections/RestaurantSection/Views/ExpandedView/index.tsx
+++ apps/dashboard/eave/dashboard/js/components/Pages/DateItineraryPage/Sections/RestaurantSection/Views/ExpandedView/index.tsx
@@ -1,4 +1,41 @@
+import { RootState } from "$eave-dashboard/js/store";
+import { styled } from "@mui/material";
+import React from "react";
+import { useSelector } from "react-redux";
+
+import ImageCarousel from "$eave-dashboard/js/components/Carousels/ImageCarousel";
+import BaseRestaurantBadge from "../../RestaurantBadge";
+
+import { getRestaurantImgUrls } from "../../../../helpers";
+
+const ViewContainer = styled("div")(() => ({
+  position: "relative",
+}));
+
+const CarouselContainer = styled("div")(() => ({
+  marginBottom: 16,
+}));
+
+const RestaurantBadge = styled(BaseRestaurantBadge)(() => ({
+  position: "absolute",
+  top: 0,
+  left: 0,
+}));
+
 const ExpandedView = () => {
+  const outing = useSelector((state: RootState) => state.outing.details);
+  const restaurant = outing?.restaurant;
+
+  if (restaurant) {
+    return (
+      <ViewContainer>
+        <RestaurantBadge />
+        <CarouselContainer>
+          <ImageCarousel imgUrls={getRestaurantImgUrls(restaurant)} />
+        </CarouselContainer>
+      </ViewContainer>
+    );
+  }
   return null;
 };
 
