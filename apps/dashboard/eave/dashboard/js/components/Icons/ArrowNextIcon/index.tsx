e1c1184e26fd598021112ed602cc07290561987d
leilenah <leilenah.mamea@gmail.com>
2024-12-13 16:05:07 -0800

    fix

--- apps/dashboard/eave/dashboard/js/components/Icons/ArrowNextIcon/index.tsx
+++ apps/dashboard/eave/dashboard/js/components/Icons/ArrowNextIcon/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ArrowNext = () => {
+const ArrowNextIcon = () => {
   return (
     <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
       <circle cx="18" cy="18" r="18" fill="white" fillOpacity="0.25" />
@@ -15,4 +15,4 @@ const ArrowNext = () => {
   );
 };
 
-export default ArrowNext;
+export default ArrowNextIcon;
