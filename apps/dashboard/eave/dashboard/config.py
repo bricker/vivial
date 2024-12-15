26701fa0a4ca438eb1187cbedd176176706ee153
Bryan Ricker <978899+bricker@users.noreply.github.com>
2024-12-15 08:22:42 -0800

    Add Datadog client monitoring (#572)

--- apps/dashboard/eave/dashboard/config.py
+++ apps/dashboard/eave/dashboard/config.py
@@ -13,8 +13,17 @@ class _AppConfig(ConfigBase):
     def stripe_publishable_key(self) -> str:
         return get_required_env("STRIPE_PUBLISHABLE_KEY")
 
+    @property
     def apple_domain_verification_code(self) -> str:
         return os.getenv("EAVE_WWW_APPLE_DOMAIN_VERIFICATION_CODE", "")
+
+    @property
+    def datadog_application_id(self) -> str:
+        return os.getenv("EAVE_WWW_DATADOG_APPLICATION_ID", "")
+
+    @property
+    def datadog_client_token(self) -> str:
+        return os.getenv("EAVE_WWW_DATADOG_CLIENT_TOKEN", "")
 
 
 DASHBOARD_APP_CONFIG = _AppConfig()
