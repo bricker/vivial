2ace5f04cbad7325c3fcbe1d99282b6acf8aa0d8
leilenah <leilenah.mamea@gmail.com>
2024-12-15 18:50:55 -0800

    Fix header

--- apps/dashboard/eave/dashboard/js/components/Global/GlobalHeader/Variants/ItineraryVariant/index.tsx
+++ apps/dashboard/eave/dashboard/js/components/Global/GlobalHeader/Variants/ItineraryVariant/index.tsx
@@ -1,14 +1,14 @@
+import { AppRoute } from "$eave-dashboard/js/routes";
 import { RootState } from "$eave-dashboard/js/store";
 import { getTotalCost } from "$eave-dashboard/js/util/currency";
 import { styled } from "@mui/material";
-import React, { useCallback, useState } from "react";
+
+import React, { useCallback } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import PrimaryButton from "$eave-dashboard/js/components/Buttons/PrimaryButton";
-import CheckoutReservation from "$eave-dashboard/js/components/CheckoutReservation";
-import StripeBadge from "$eave-dashboard/js/components/CheckoutReservation/StripeBadge";
 import VivialLogo from "$eave-dashboard/js/components/Logo";
-import Modal from "$eave-dashboard/js/components/Modal";
 import Typography from "@mui/material/Typography";
 import Header from "../../Shared/Header";
 
@@ -22,13 +22,15 @@ const Cost = styled("span")(() => ({
 }));
 
 const ItineraryVariant = () => {
-  const [bookingOpen, setBookingOpen] = useState(false);
   const outing = useSelector((state: RootState) => state.outing.details);
+  const navigate = useNavigate();
   const hasCost = !!outing?.costBreakdown?.totalCostCents;
 
-  const toggleBookingOpen = useCallback(() => {
-    setBookingOpen(!bookingOpen);
-  }, [bookingOpen]);
+  const handleBook = useCallback(() => {
+    if (outing) {
+      navigate(`${AppRoute.checkout}/${outing.id}`);
+    }
+  }, [outing]);
 
   return (
     <Header>
@@ -38,16 +40,7 @@ const ItineraryVariant = () => {
           <Typography variant="subtitle1">
             Total: <Cost>{getTotalCost(outing)}</Cost>
           </Typography>
-          <BookButton onClick={toggleBookingOpen}>Book</BookButton>
-          <Modal
-            title="Booking Info"
-            onClose={toggleBookingOpen}
-            open={bookingOpen}
-            badge={<StripeBadge />}
-            padChildren={false}
-          >
-            <CheckoutReservation outingId={outing.id} />
-          </Modal>
+          <BookButton onClick={handleBook}>Book</BookButton>
         </>
       )}
     </Header>
