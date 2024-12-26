786ef20b2d1a6148dbcc277a0a1c6e2a46e4ec6d
Bryan Ricker <978899+bricker@users.noreply.github.com>
2024-12-26 12:49:30 -0800

    update start time validation to allow to end of day; Allow escape to close modal (#631)

--- apps/core/eave/core/graphql/validators/time_bounds_validator.py
+++ apps/core/eave/core/graphql/validators/time_bounds_validator.py
@@ -8,8 +8,23 @@ we can safely book events for a user.
 
 
 def start_time_too_soon(*, start_time: datetime, timezone: ZoneInfo) -> bool:
-    return datetime.now(tz=timezone) + timedelta(hours=24) > start_time.astimezone(timezone)
+    now = datetime.now(tz=timezone)
+    start_time = start_time.astimezone(timezone)
+    delta = timedelta(hours=24)
+
+    lower_bound_incl = now + delta
+    lower_bound_incl = lower_bound_incl.replace(minute=0, second=0)
+
+    return start_time >= lower_bound_incl
 
 
 def start_time_too_far_away(*, start_time: datetime, timezone: ZoneInfo) -> bool:
-    return datetime.now(tz=timezone) + timedelta(days=30) < start_time.astimezone(timezone)
+    now = datetime.now(tz=timezone)
+    start_time = start_time.astimezone(timezone)
+    delta = timedelta(days=30)
+
+    # For upper bound, we allow up to the end of the day
+    upper_bound_incl = now + delta
+    upper_bound_incl = upper_bound_incl.replace(hour=23, minute=59, second=59)
+
+    return start_time <= upper_bound_incl
