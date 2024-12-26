4be571d369b47c23741372d63e17154a9752b473
leilenah <leilenah.mamea@gmail.com>
2024-12-25 23:49:33 -0800

    Make LogoPill a fixed width.

--- apps/dashboard/eave/dashboard/js/components/LogoPill/index.tsx
+++ apps/dashboard/eave/dashboard/js/components/LogoPill/index.tsx
@@ -3,28 +3,27 @@ import { imageUrl } from "$eave-dashboard/js/util/asset";
 import { styled } from "@mui/material";
 import React from "react";
 
-const LogoImage = styled("img")(() => ({
-  height: "100%",
-  maxWidth: "100%",
+const LogoImage = styled("img")<{ width: number }>(({ width }) => ({
+  height: "auto",
+  width,
 }));
 
-const LogoContainer = styled("div")<{ padding: number; backgroundColor: string }>(({ padding, backgroundColor }) => ({
+const LogoContainer = styled("div")<{ backgroundColor: string }>(({ backgroundColor }) => ({
   borderRadius: 10,
-  width: "40%",
-  minWidth: "40%",
-  aspectRatio: 3.4,
+  width: 136,
+  minWidth: 136, // Ensures 136 width as flex item.
+  height: 40,
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
   backgroundColor,
-  padding,
 }));
 
 export interface LogoPillAttributes {
   backgroundColor: string;
-  padding: number;
   alt: string;
   logoUri: string;
+  logoWidth: number;
 }
 
 export const logos: { [key: string]: LogoPillAttributes } = {
@@ -32,26 +31,26 @@ export const logos: { [key: string]: LogoPillAttributes } = {
     logoUri: imageUrl("vivial-word-logo.png"),
     alt: "Vivial",
     backgroundColor: colors.vivialYellow,
-    padding: 11,
+    logoWidth: 65,
   },
   opentable: {
     logoUri: imageUrl("opentable-logo.png"),
     alt: "Opentable",
     backgroundColor: "#DA3644",
-    padding: 8,
+    logoWidth: 91.4,
   },
   eventbrite: {
     logoUri: imageUrl("eventbrite-logo.png"),
     alt: "Eventbrite",
     backgroundColor: "#F05537",
-    padding: 12,
+    logoWidth: 83.7,
   },
 };
 
-const LogoPill = ({ attrs: { backgroundColor, padding, alt, logoUri } }: { attrs: LogoPillAttributes }) => {
+const LogoPill = ({ attrs: { backgroundColor, alt, logoUri, logoWidth } }: { attrs: LogoPillAttributes }) => {
   return (
-    <LogoContainer backgroundColor={backgroundColor} padding={padding}>
-      <LogoImage alt={alt} src={logoUri} />
+    <LogoContainer backgroundColor={backgroundColor}>
+      <LogoImage alt={alt} src={logoUri} width={logoWidth} />
     </LogoContainer>
   );
 };
