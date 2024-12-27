a72c4afc07367c10b7387691b2cf5fe15f26b3c2
Bryan Ricker <git@bryanricker.com>
2024-12-26 21:49:09 -0800

    shared codegen config

--- apps/admin/codegen.ts
+++ apps/admin/codegen.ts
@@ -1,83 +1,9 @@
-import { type AddPluginConfig } from "@graphql-codegen/add";
-import type { CodegenConfig } from "@graphql-codegen/cli";
-import { addTypenameSelectionDocumentTransform, type ClientPresetConfig } from "@graphql-codegen/client-preset";
+import { buildCodegenConfig } from "@eave-fyi/develop/codegen";
+
 const schema = process.env["ADMIN_GRAPHQL_SCHEMA"] || "http://api.internal.eave.run:8080/internal/graphql";
 
-const config: CodegenConfig = {
+export default buildCodegenConfig({
   schema,
-  documents: ["eave/dashboard/**/*.graphql"],
-  ignoreNoDocuments: true,
-  generates: {
-    "./eave/dashboard/js/graphql/generated/": {
-      preset: "client",
-      presetConfig: <ClientPresetConfig>{
-        fragmentMasking: false,
-      },
-      documentTransforms: [addTypenameSelectionDocumentTransform],
-      config: {
-        // See here: https://the-guild.dev/graphql/codegen/plugins/presets/preset-client#config-api
-        // The options here are forwarded to other plugins and the exact properties aren't available in a type,
-        // so use the documentation to know which options are available.
-        documentMode: "string",
-        defaultScalarType: "string",
-        strictScalars: true,
-        useTypeImports: true,
-
-        scalars: {
-          // These scalars match what the server provided, including the scalars built-in to Strawberry:
-          // https://strawberry.rocks/docs/types/scalars
-          UUID: {
-            input: "string",
-            output: "string",
-          },
-          Void: {
-            input: "null",
-            output: "null",
-          },
-          DateTime: {
-            input: "string",
-            output: "string",
-          },
-          Date: {
-            input: "string",
-            output: "string",
-          },
-          Time: {
-            input: "string",
-            output: "string",
-          },
-          Decimal: {
-            input: "string",
-            output: "string",
-          },
-          JSON: {
-            input: "string",
-            output: "string",
-          },
-          Base16: {
-            input: "string",
-            output: "string",
-          },
-          Base32: {
-            input: "string",
-            output: "string",
-          },
-          Base64: {
-            input: "string",
-            output: "string",
-          },
-        },
-      },
-      plugins: [
-        {
-          add: <AddPluginConfig>{
-            placement: "prepend",
-            content: "// @ts-nocheck",
-          },
-        },
-      ],
-    },
-  },
-};
-
-export default config;
+  documents: ["eave/admin/**/*.graphql"],
+  destination: "./eave/admin/js/graphql/generated/",
+});
