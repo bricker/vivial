f388d4836cd3ef8a323daa52aa01d0a9173ccebe
Leilenah Mamea <leilenah@eave.fyi>
2024-12-27 13:54:15 -0800

    Mobile safari UI overflow in header of itinerary page (#637)
    Ticket link:
    <!-- here -->
    
    Did you run?:
    - [ ] unit tests
    - [ ] lint

--- apps/dashboard/eave/dashboard/js/components/Util/ScrollToTop/index.tsx
+++ apps/dashboard/eave/dashboard/js/components/Util/ScrollToTop/index.tsx
@@ -4,7 +4,11 @@ import { useLocation } from "react-router-dom";
 const ScrollToTop = () => {
   const { pathname } = useLocation();
   useEffect(() => {
-    setTimeout(() => window.scroll({ top: -1, left: 0, behavior: "instant" }), 10); // FIXME: sad race condition hack :(
+    /**
+     * setTimeout is used to mitigate race conditions.
+     * window.scrollTo(0, 1) suppresses the search bar on Safari mobile when scrolling to top.
+     */
+    setTimeout(() => window.scrollTo(0, 1), 10);
   }, [pathname]);
   return null;
 };
