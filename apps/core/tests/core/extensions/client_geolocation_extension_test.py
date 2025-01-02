06796367c7d001d698f593b296dab33b645e0044
Bryan Ricker <git@bryanricker.com>
2025-01-01 22:24:40 -0800

    more tests

---
+++ apps/core/tests/core/extensions/client_geolocation_extension_test.py
@@ -0,0 +1,8 @@
+from eave.stdlib.analytics import SEGMENT_ANONYMOUS_ID_COOKIE_NAME
+
+from ..base import BaseTestCase
+
+
+class TestClientGeolocationExtension(BaseTestCase):
+    async def test_client_geolocation_extension(self) -> None:
+        self.fail()
---
+++ apps/core/tests/core/extensions/log_context_extension_test.py
@@ -0,0 +1,8 @@
+from eave.stdlib.analytics import SEGMENT_ANONYMOUS_ID_COOKIE_NAME
+
+from ..base import BaseTestCase
+
+
+class TestLogContextExtension(BaseTestCase):
+    async def test_log_context_extension(self) -> None:
+        self.fail()
