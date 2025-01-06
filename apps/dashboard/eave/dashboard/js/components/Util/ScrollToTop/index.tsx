cc5403e56229a160da5cda877ad0c40e1abd29ae
Bryan Ricker <git@bryanricker.com>
2025-01-06 15:03:57 -0800

    more scroll tweaks

--- apps/dashboard/eave/dashboard/js/components/Util/ScrollToTop/index.tsx
+++ apps/dashboard/eave/dashboard/js/components/Util/ScrollToTop/index.tsx
@@ -1,14 +1,21 @@
+import { ITINERARY_PREFIX, type NavigationState } from "$eave-dashboard/js/routes";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const location = useLocation();
+  const { pathname } = location;
+  const state = location.state as NavigationState | undefined | null;
+
   useEffect(() => {
     /**
      * setTimeout is used to mitigate race conditions.
      * window.scrollTo(0, 1) suppresses the search bar on Safari mobile when scrolling to top.
      */
-    setTimeout(() => window.scrollTo(0, 1), 10);
+    const scrollBehavior = state?.scrollBehavior;
+    setTimeout(() => {
+      window.scrollTo({ top: 0, left: 0, behavior: scrollBehavior });
+    }, 10);
   }, [pathname]);
   return null;
 };
