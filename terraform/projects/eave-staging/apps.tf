965d6b3154bf96ad01021751e7828b2d2ff52c91
Bryan Ricker <git@bryanricker.com>
2025-01-07 09:08:37 -0800

    add jwt auds

--- terraform/projects/eave-staging/apps.tf
+++ terraform/projects/eave-staging/apps.tf
@@ -42,8 +42,8 @@ module "core_api_app" {
   iap_oauth_client_kube_secret_name = module.shared_kubernetes_resources.iap_oauth_client_kube_secret_name
 
   root_iap_enabled = true
-  root_iap_jwt_aud = "" # "/projects/${data.google_project.default.number}/global/backendServices/${data.google_compute_backend_service["core_api"].generated_id}"
-  iap_jwt_aud      = "" # "/projects/${data.google_project.default.number}/global/backendServices/${data.google_compute_backend_service["core_api_iap"].generated_id}"
+  root_iap_jwt_aud = "/projects/${data.google_project.default.number}/global/backendServices/${data.google_compute_backend_service.k8s_backend_services["core-api"].generated_id}"
+  iap_jwt_aud      = "/projects/${data.google_project.default.number}/global/backendServices/${data.google_compute_backend_service.k8s_backend_services["core-api-iap"].generated_id}"
 }
 
 module "dashboard_app" {
@@ -86,5 +86,5 @@ module "admin_app" {
 
   iap_oauth_client_id               = module.iap.google_iap_client.client_id
   iap_oauth_client_kube_secret_name = module.shared_kubernetes_resources.iap_oauth_client_kube_secret_name
-  iap_jwt_aud                       = null # "/projects/${data.google_project.default.number}/global/backendServices/${data.google_compute_backend_service["admin"].generated_id}"
+  iap_jwt_aud                       = "/projects/${data.google_project.default.number}/global/backendServices/${data.google_compute_backend_service.k8s_backend_services["admin"].generated_id}"
 }
