0ff9106d36cf993f87b402f3e54a1db03ca1ac58
Bryan Ricker <git@bryanricker.com>
2025-01-06 17:14:35 -0800

    Mon Jan  6 05:14:35 PM PST 2025

--- apps/dashboard/eave/dashboard/js/App.tsx
+++ apps/dashboard/eave/dashboard/js/App.tsx
@@ -1,6 +1,7 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import mixpanel from "mixpanel-browser";
 import React from "react";
 import { CookiesProvider, withCookies } from "react-cookie";
 import { Helmet } from "react-helmet";
