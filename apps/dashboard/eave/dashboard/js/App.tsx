b60315f6c15ec01a318d3521856e64e01bde2879
niehusst <liamniehusstaab@gmail.com>
2025-01-08 11:55:47 -0800

    fix: lint

--- apps/dashboard/eave/dashboard/js/App.tsx
+++ apps/dashboard/eave/dashboard/js/App.tsx
@@ -1,7 +1,6 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import mixpanel from "mixpanel-browser";
 import React from "react";
 import { CookiesProvider, withCookies } from "react-cookie";
 import { Helmet } from "react-helmet";
