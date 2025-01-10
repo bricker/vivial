890099481f6fc0610fd885f0153221a9817cb0f7
niehusst <liamniehusstaab@gmail.com>
2025-01-10 14:04:38 -0800

    feat: UI looks ok now

--- apps/dashboard/eave/dashboard/js/components/Pages/DateItineraryPage/Views/PreferencesBanner/index.tsx
+++ apps/dashboard/eave/dashboard/js/components/Pages/DateItineraryPage/Views/PreferencesBanner/index.tsx
@@ -1,8 +1,11 @@
 import CloseButton from "$eave-dashboard/js/components/Buttons/CloseButton";
+import ArrowRightIcon from "$eave-dashboard/js/components/Icons/ArrowRightIcon";
 import { ItineraryPageVariant, SearchParam } from "$eave-dashboard/js/routes";
 import { RootState } from "$eave-dashboard/js/store";
-import { Typography, styled } from "@mui/material";
-import { ArrowRightIcon } from "@mui/x-date-pickers";
+import { colors } from "$eave-dashboard/js/theme/colors";
+import { fontFamilies } from "$eave-dashboard/js/theme/fonts";
+import { rem } from "$eave-dashboard/js/theme/helpers/rem";
+import { Typography, keyframes, styled } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useSearchParams } from "react-router-dom";
@@ -12,10 +15,46 @@ const BannerContainer = styled("div")<{ open: boolean }>(({ theme, open }) => ({
   backgroundColor: theme.palette.accent[1],
   alignItems: "center",
   justifyContent: "flex-start",
+  padding: 16,
+}));
+
+const AlignedCloseButton = styled(CloseButton)(() => ({
+  alignSelf: "flex-start",
 }));
 
 const TextContainer = styled("div")(() => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: 8,
   padding: 16,
+}));
+
+const BannerText = styled(Typography)(({ theme }) => ({
+  fontFamily: fontFamilies.quicksand,
+  color: theme.palette.field.primary,
+}));
+
+const BannerTitle = styled(BannerText)(() => ({
+  fontSize: rem(24),
+}));
+
+const pulse = keyframes`
+0% {
+  transform: scale(1);
+}
+50% {
+  transform: scale(1.2);
+}
+100% {
+  transform: scale(1);
+}
+`;
+
+const AnimatedDiv = styled("div")(() => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  animation: `${pulse} 2s linear infinite`,
 }));
 
 const PreferencesBanner = () => {
@@ -31,12 +70,14 @@ const PreferencesBanner = () => {
   }, [searchParams, isLoggedIn]);
   return (
     <BannerContainer open={prefsBannerOpen}>
-      <CloseButton onClick={() => setPrefsBannerOpen(false)} />
+      <AlignedCloseButton iconColor={colors.grey[800]} onClick={() => setPrefsBannerOpen(false)} />
       <TextContainer>
-        <Typography variant="h2">🎯 Not quite right?</Typography>
-        <Typography variant="body1">Update your preferences for personalized recommendations.</Typography>
+        <BannerTitle variant="h4">🎯 Not quite right?</BannerTitle>
+        <BannerText variant="body1">Update your preferences for personalized recommendations.</BannerText>
       </TextContainer>
-      <ArrowRightIcon />
+      <AnimatedDiv>
+        <ArrowRightIcon />
+      </AnimatedDiv>
     </BannerContainer>
   );
 };
