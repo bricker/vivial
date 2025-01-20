4f88e0f54c3ee637bcd120cb12f9e764caada2a6
Bryan Ricker <git@bryanricker.com>
2025-01-20 15:05:01 -0800

    maxmemory config

--- terraform/modules/redis/main.tf
+++ terraform/modules/redis/main.tf
@@ -12,7 +12,7 @@ resource "google_redis_instance" "default" {
   transit_encryption_mode = "SERVER_AUTHENTICATION"
   redis_version           = "REDIS_7_0"
   redis_configs = {
-    "maxmemory"        = "2000mb" # 2gb, minus 48mb for LRU buffering
+    "maxmemory-gb"        = "1.8" # leaves headroom for LRU buffer
     "maxmemory-policy" = "allkeys-lru"
   }
 
